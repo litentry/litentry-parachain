f379a88b61c4ced551eb4b7cb2e4474f2c2e8045
Han Zhao <windzhaohan@gmail.com>
2021-02-05 23:17:26 +0100

    adapted to generate genesis head and wasm of parachain

--- ts-tests/scripts/start-alice-and-bob.sh
+++ ts-tests/scripts/start-alice-and-bob.sh
@@ -2,9 +2,15 @@
 
 # configure the relative paths of binary and chain spec
 GIT_ROOT=`git rev-parse --show-toplevel`
+LITENTRY_BIN=$GIT_ROOT/target/release/rococo-collator
 POLKADOT_BIN=$GIT_ROOT/polkadot/target/release/polkadot
 ROCOCO_CHAINSPEC=$GIT_ROOT/polkadot/rococo-local-cfde-real-overseer-new.json
 
+# generate genesis head and wasm validation files
+$LITENTRY_BIN export-genesis-state --parachain-id 1984 > para-1984-genesis
+$LITENTRY_BIN export-genesis-wasm > para-1984-wasm
+
 # run alice and bob as relay nodes
-$POLKADOT_BIN --chain $ROCOCO_CHAINSPEC --alice --tmp &
+$POLKADOT_BIN --chain $ROCOCO_CHAINSPEC --alice --tmp --port 30333 &
 $POLKADOT_BIN --chain $ROCOCO_CHAINSPEC --bob --tmp --port 30334 &
+
