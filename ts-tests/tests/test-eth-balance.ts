dc768d3114cf0742326e07b5e824f0dfa1b17180
Han Zhao <windzhaohan@gmail.com>
2021-04-01 16:38:01 +0200

    Revert "Increase waiting time of ocw claim call"
    This reverts commit 29e006ddbca73ebd4031febc828107d3fe509cca.

--- ts-tests/tests/test-eth-balance.ts
+++ ts-tests/tests/test-eth-balance.ts
@@ -161,8 +161,8 @@ describeLitentry("Test Ethereum Link and Balance Fetch", ``, (context) => {
       data: old_balance,
     } = await context.api.query.system.account(OCR_ACCOUNT);
 
-    // Wait for 240s to ensure ocw has finished the job
-    await new Promise((r) => setTimeout(r, 240000));
+    // Wait for 72s ~ 6 blocks
+    await new Promise((r) => setTimeout(r, 72000));
     const balances = await get_assets(context.api, context.alice);
 
     // TODO fetch real time balance and compare it here
