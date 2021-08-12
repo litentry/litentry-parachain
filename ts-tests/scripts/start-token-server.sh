181271ac8b8e185099e8f45b87a7181d77aa2a40
zongxiong.chen <czxczf@gmail.com>
2021-08-12 22:28:39 +0200

    Update token server startup script

--- ts-tests/scripts/start-token-server.sh
+++ ts-tests/scripts/start-token-server.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-# export etherscan="RF71W4Z2RDA7XQD6EN19NGB66C2QD9UPHB"
-# export infura="aa0a6af5f94549928307febe80612a2a"
-# export blockchain=""
+
 GIT_ROOT=`git rev-parse --show-toplevel`
 TOKEN_SERVER_BINARY=$GIT_ROOT/token-server/target/release/litentry-token-server
 
-etherscan="RF71W4Z2RDA7XQD6EN19NGB66C2QD9UPHB" infura="aa0a6af5f94549928307febe80612a2a" blockchain="" $TOKEN_SERVER_BINARY
+ETHERSCAN="RF71W4Z2RDA7XQD6EN19NGB66C2QD9UPHB"
+INFURA="aa0a6af5f94549928307febe80612a2a"
+BLOCKCHAIN=""
+
+etherscan=$ETHERSCAN infura=$INFURA blockchain=$BLOCKCHAIN $TOKEN_SERVER_BINARY &> /tmp/litentry-token-server.log &
