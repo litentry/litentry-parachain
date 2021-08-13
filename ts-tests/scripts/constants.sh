f5c5f4cabfbb2dbff096ad96a66eede16bfe683a
zongxiong.chen <czxczf@gmail.com>
2021-08-13 20:05:55 +0200

    Add some constants

---
+++ ts-tests/scripts/constants.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+TMP_DIR=$1
+
+# pid files
+RELAY_ALICE_PIDFILE="$TMP_DIR/relay.alice.pid"
+RELAY_BOB_PIDFILE="$TMP_DIR/relay.bob.pid"
+PARA_ALICE_PIDFILE="$TMP_DIR/para.alice.pid"
+PARA_BOB_PIDFILE="$TMP_DIR/para.bob.pid"
+TOKEN_SERVER_PIDFILE="$TMP_DIR/token-server.pid"
