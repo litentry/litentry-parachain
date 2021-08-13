cb7e73925b0fd0b05eb08d9e9889310636e08355
zongxiong.chen <czxczf@gmail.com>
2021-08-13 22:52:03 +0200

    Test

--- ts-tests/scripts/run-test.sh
+++ ts-tests/scripts/run-test.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# set -o pipefail
-
 basedir=$(dirname "$0")
 cd "$basedir"
 
@@ -9,12 +7,11 @@ cd ..
 
 tmpdir=$(mktemp -d /tmp/tmp.XXXXXX)
 tmpdir="${1:-$tmpdir}"
-echo "tmpdir: $tmpdir"
+
 echo "NODE_ENV=ci" > .env
-# ./scripts/start-relay-and-para-chain.sh "$tmpdir"
-# ./scripts/start-token-server.sh "$tmpdir"
-# yarn && yarn register-parachain 2>&1 | tee "$tmpdir/register-parachain.log"
-# yarn test 2>&1 | tee "$tmpdir/test.log"
-# ./scripts/clean-up.sh "$tmpdir" $?
-echo hello > "$tmpdir/world.txt"
-exit 0
+./scripts/start-relay-and-para-chain.sh "$tmpdir"
+./scripts/start-token-server.sh "$tmpdir"
+yarn && yarn register-parachain 2>&1 | tee "$tmpdir/register-parachain.log"
+yarn test 2>&1 | tee "$tmpdir/test.log"
+
+exit 1
