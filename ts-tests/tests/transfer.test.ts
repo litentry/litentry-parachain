2d1a72b10bd1757a713a558747ccc5e68bc4e756
Han Zhao <windzhaohan@gmail.com>
2021-08-27 15:56:07 +0200

    Formatting new file

--- ts-tests/tests/transfer.test.ts
+++ ts-tests/tests/transfer.test.ts
@@ -4,39 +4,52 @@ import { step } from 'mocha-steps';
 
 import { signAndSend, describeLitentry, loadConfig, sleep } from './utils';
 
-
 describeLitentry('Test Balance Transfer', ``, (context) => {
-
     console.log(`Test Balance Transfer`);
 
-    step("Transfer 1000 unit from Alice to Bob", async function () {
-		// Get the initial balance of Alice and Bob
-		const { nonce: aliceInitNonce, data: aliceInitBalance } = await context.api.query.system.account(context.alice.address);
-		const { nonce: bobInitNonce, data: bobInitBalance } = await context.api.query.system.account(context.bob.address);
+    step('Transfer 1000 unit from Alice to Bob', async function () {
+        // Get the initial balance of Alice and Bob
+        const { nonce: aliceInitNonce, data: aliceInitBalance } = await context.api.query.system.account(
+            context.alice.address
+        );
+        const { nonce: bobInitNonce, data: bobInitBalance } = await context.api.query.system.account(
+            context.bob.address
+        );
 
-		const tx = context.api.tx.balances.transfer(context.bob.address, 1000);
-		await signAndSend(tx, context.alice);
+        const tx = context.api.tx.balances.transfer(context.bob.address, 1000);
+        await signAndSend(tx, context.alice);
 
-		const { nonce: aliceCurrentNonce, data: aliceCurrentBalance } = await context.api.query.system.account(context.alice.address);
-		const { nonce: bobCurrentNonce, data: bobCurrentBalance } = await context.api.query.system.account(context.bob.address);
+        const { nonce: aliceCurrentNonce, data: aliceCurrentBalance } = await context.api.query.system.account(
+            context.alice.address
+        );
+        const { nonce: bobCurrentNonce, data: bobCurrentBalance } = await context.api.query.system.account(
+            context.bob.address
+        );
 
         expect(aliceCurrentNonce.toNumber()).to.equal(aliceInitNonce.toNumber() + 1);
         expect(bobCurrentBalance.free.toBigInt()).to.equal(bobInitBalance.free.toBigInt() + BigInt(1000));
     });
 
-    step("Transfer 1000 unit back to Alice from Bob", async function () {
-		// Get the initial balance of Alice and Bob
-		const { nonce: aliceInitNonce, data: aliceInitBalance } = await context.api.query.system.account(context.alice.address);
-		const { nonce: bobInitNonce, data: bobInitBalance } = await context.api.query.system.account(context.bob.address);
+    step('Transfer 1000 unit back to Alice from Bob', async function () {
+        // Get the initial balance of Alice and Bob
+        const { nonce: aliceInitNonce, data: aliceInitBalance } = await context.api.query.system.account(
+            context.alice.address
+        );
+        const { nonce: bobInitNonce, data: bobInitBalance } = await context.api.query.system.account(
+            context.bob.address
+        );
 
-		const tx = context.api.tx.balances.transfer(context.alice.address, 1000);
-		await signAndSend(tx, context.bob);
+        const tx = context.api.tx.balances.transfer(context.alice.address, 1000);
+        await signAndSend(tx, context.bob);
 
-		const { nonce: aliceCurrentNonce, data: aliceCurrentBalance } = await context.api.query.system.account(context.alice.address);
-		const { nonce: bobCurrentNonce, data: bobCurrentBalance } = await context.api.query.system.account(context.bob.address);
+        const { nonce: aliceCurrentNonce, data: aliceCurrentBalance } = await context.api.query.system.account(
+            context.alice.address
+        );
+        const { nonce: bobCurrentNonce, data: bobCurrentBalance } = await context.api.query.system.account(
+            context.bob.address
+        );
 
         expect(bobCurrentNonce.toNumber()).to.equal(bobInitNonce.toNumber() + 1);
         expect(aliceCurrentBalance.free.toBigInt()).to.equal(aliceInitBalance.free.toBigInt() + BigInt(1000));
     });
-
 });
