2a04052e0ec8ff6dc1213ac1459dcea636be4f69
Kailai Wang <Kailai.Wang@hotmail.com>
2021-09-30 18:45:15 +0200

    refactor the dir name and file names

--- ts-tests/scripts/clean-up.sh
+++ ts-tests/scripts/clean-up.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 basedir=$(dirname "$0")
-cd "$basedir/../../docker/generated"
+cd "$basedir/../../docker/generated-dev"
 
 docker images
 
@@ -9,7 +9,7 @@ echo "stop and remove docker containers..."
 docker-compose rm -f -s -v
 
 echo "remove docker volumes..."
-docker volume ls | grep generated_ | sed 's/local *//' | xargs docker volume rm
+docker volume ls | grep generated-dev | sed 's/local *//' | xargs docker volume rm
 
 echo "remove dangling docker images if any..."
 [ -z "$(docker images --filter=dangling=true -q)" ] || docker rmi -f $(docker images --filter=dangling=true -q)
@@ -18,6 +18,6 @@ echo "keep litentry/litentry-parachain:latest while removing other tags..."
 docker rmi -f $(docker images litentry/litentry-parachain --format "{{.Repository}}:{{.Tag}}" | grep -v latest)
 
 echo "remove generated images..."
-docker rmi -f $(docker images --filter=reference='generated_*' --format "{{.Repository}}:{{.Tag}}")
+docker rmi -f $(docker images --filter=reference='generated-dev*' --format "{{.Repository}}:{{.Tag}}")
 
 echo "cleaned up."
