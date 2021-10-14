a1435a23b322dcc878883e1a3ebe0623b8dd6800
Kailai Wang <Kailai.Wang@hotmail.com>
2021-10-14 22:22:19 +0200

    separate actions to modules

--- scripts/run-ci-test.sh
+++ scripts/run-ci-test.sh
@@ -5,7 +5,5 @@ set -eo pipefail
 ROOTDIR=$(git rev-parse --show-toplevel)
 cd "$ROOTDIR/ts-tests"
 
-tmpdir="${1:-/tmp}"
-
 echo "NODE_ENV=ci" > .env
-yarn && yarn test 2>&1 | tee "$tmpdir/run_test.log"
+yarn && yarn test 2>&1 | tee "/tmp/parachain_ci_test.log"
