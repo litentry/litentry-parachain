9b3c28f0eef00d4a48d6b16ce3c14b7f0197e14d
Kai <7630809+Kailai-Wang@users.noreply.github.com>
2021-10-28 14:40:56 +0200

    Revert to use of raw binaries in CI (#193)
    * add scripts to launch dev network with raw binaries
    
    * experimental: try to pull bin from litentry/litentry-parachain:collective
    
    * improve download and pipe
    
    * experimental: run actions on ubuntu-latest
    
    * Revert "experimental: run actions on ubuntu-latest"
    
    This reverts commit 38ebf342323a1928a846474d6a2ab80214addb34.
    
    * add comments to how staging keys are generated
    
    * update README
    
    * build parachain binary locally
    
    * disable building and pushing of docker image
    
    * install rust toolchain
    
    * use nightly rustc
    
    * Update README.md

---
+++ scripts/clean-local-binary.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# no `set -e` here as we allow commands to fail in this script
+
+TMPDIR=/tmp/parachain_dev
+
+# for f in $(ls $TMPDIR/*.pid 2>/dev/null); do
+#   echo "Killing $f ..."
+#   kill -9 $(cat "$f")
+# done
+
+# use killall here:
+# the previously written PID could change if any process restarted
+killall polkadot
+killall litentry-collator
+
+rm -rf "$TMPDIR"
+
+echo "cleaned up."
