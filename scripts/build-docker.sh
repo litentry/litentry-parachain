29eda14862b37468c03d30dbf10e3bfc4647554b
Kailai Wang <Kailai.Wang@hotmail.com>
2021-10-29 00:16:46 +0200

    update readme and metadata in scripts

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -4,38 +4,39 @@ set -eo pipefail
 ROOTDIR=$(git rev-parse --show-toplevel)
 cd "$ROOTDIR"
 
-VERSION="$1"
-FEATURES="$2"
+TAG="$1"
+ARGS="$2"
 
-if [ -z "$VERSION" ]; then
+if [ -z "$TAG" ]; then
     TAG_COMMIT=`git rev-list --tags --max-count=1`
     HEAD_COMMIT=`git rev-parse HEAD`
     echo "TAG  commit: $TAG_COMMIT"
     echo "HEAD commit: $HEAD_COMMIT"
 
     if [ "$TAG_COMMIT" == "$HEAD_COMMIT" ]; then
-        VERSION=`git describe --tags $TAG_COMMIT`
+        TAG=`git describe --tags $TAG_COMMIT`
     else
-        VERSION=`git rev-parse --short HEAD`
+        TAG=`git rev-parse --short HEAD`
     fi
 fi
 
-echo "VERSION: $VERSION"
-echo "FEATURES: $FEATURES"
+echo "TAG: $TAG"
+echo "ARGS: $ARGS"
 
 GITUSER=litentry
 GITREPO=litentry-parachain
 
 # Build the image
-echo "Building ${GITUSER}/${GITREPO}:${VERSION} docker image, hang on!"
+echo "------------------------------------------------------------"
+echo "Building ${GITUSER}/${GITREPO}:${TAG} docker image ..."
 docker build --rm --no-cache --pull -f ./docker/Dockerfile \
-    --build-arg PROFILE=release \
-    --build-arg FEATURES="$FEATURES" \
-    -t ${GITUSER}/${GITREPO}:${VERSION} .
+    --build-arg BUILD_ARGS="$ARGS" \
+    -t ${GITUSER}/${GITREPO}:${TAG} .
 
-# Tag it with latest if no arg was provided
-[ -z "$1" ] && docker tag ${GITUSER}/${GITREPO}:${VERSION} ${GITUSER}/${GITREPO}:latest
+# Tag it with latest if no tag parameter was provided
+[ -z "$1" ] && docker tag ${GITUSER}/${GITREPO}:${TAG} ${GITUSER}/${GITREPO}:latest
 
 # Show the list of available images for this repo
 echo "Image is ready"
+echo "------------------------------------------------------------"
 docker images | grep ${GITREPO}
