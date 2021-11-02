39836ecce2299584f89735477ff04e4e2aab3be7
Cupnfish <40173605+Cupnfish@users.noreply.github.com>
2021-11-03 00:22:44 +0800

    cargo fmt

--- runtime/src/constants.rs
+++ runtime/src/constants.rs
@@ -1,16 +1,16 @@
 // Copyright 2020-2021 Litentry Technologies GmbH.
 // This file is part of Litentry.
-// 
+//
 // Litentry is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // Litentry is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with Litentry.  If not, see <https://www.gnu.org/licenses/>.
 
@@ -20,7 +20,7 @@ pub mod currency {
 
 	pub const UNIT: Balance = 1_000_000_000_000;
 	pub const DOLLARS: Balance = UNIT; // 1_000_000_000_000
-	pub const CENTS: Balance = DOLLARS / 100;  // 10_000_000_000
+	pub const CENTS: Balance = DOLLARS / 100; // 10_000_000_000
 	pub const MILLICENTS: Balance = CENTS / 1_000; // 10_000_000
 
 	// Linear ratio of transaction fee distribution
