f62c15997581d9e6147006c2a86ea14646856dfd
Han <windzhaohan@gmail.com>
2021-11-02 07:04:29 +0000

    Changed to forceTransfer sudo call, and removed tx fee case

--- ts-tests/tests/base-filter.test.ts
+++ ts-tests/tests/base-filter.test.ts
@@ -31,7 +31,7 @@ describeLitentry('Test Base Filter', ``, (context) => {
         expect(bobCurrentBalance.free.toBigInt()).to.equal(bobInitBalance.free.toBigInt());
     });
 
-    step('Transfer 1000 unit from Alice to Bob with Sudo', async function () {
+    step('Transfer 1000 unit from Eve to Bob with Sudo', async function () {
         // Get the initial balance of Alice and Bob
         const { nonce: aliceInitNonce, data: aliceInitBalance } = await context.api.query.system.account(
             context.alice.address
@@ -40,8 +40,9 @@ describeLitentry('Test Base Filter', ``, (context) => {
             context.bob.address
         );
 
+		// Force transfer 1000 unit from Eve to Bob, signed by Alice(sudo)
         const tx = context.api.tx.sudo.sudo(
-			context.api.tx.balances.transfer(context.bob.address, 1000)
+			context.api.tx.balances.forceTransfer(context.eve.address,context.bob.address, 1000)
 			);
         await signAndSend(tx, context.alice);
 
@@ -57,51 +58,4 @@ describeLitentry('Test Base Filter', ``, (context) => {
         expect(bobCurrentBalance.free.toBigInt()).to.equal(bobInitBalance.free.toBigInt() + BigInt(1000));
     });
 
-    step('Verify transaction fee distribution', async function () {
-		// These are default proportions configured in runtime as constants
-		const TREASURY_PROPORTION = 40;
-		const AUTHOR_PROPORTION = 0;
-		const BURNED_PROPORTION = 60;
-
-		// Get Treasury account
-		const treasuryAccount = u8aConcat(
-			'modl',
-			context.api.consts.treasury && context.api.consts.treasury.palletId
-			  ? context.api.consts.treasury.palletId.toU8a(true)
-			  : 'py/trsry',
-			  new Uint8Array(32)
-		  ).subarray(0, 32);
-
-		// Get the initial balances of Alice and Treasury
-        const { data: aliceInitBalance } = await context.api.query.system.account(
-            context.alice.address
-        );
-        const { data: treasuryInitBalance } = await context.api.query.system.account(
-            treasuryAccount
-        );
-
-	    // Send a sudo transaction from Alice
-		const transferAmount = 1000;
-        const txTransfer = context.api.tx.sudo.sudo(
-			context.api.tx.balances.transfer(context.bob.address, transferAmount)
-			);
-        await signAndSend(txTransfer, context.alice);
-
-		// Get the current balances of Alice and Treasury
-        const { data: aliceCurrentBalance } = await context.api.query.system.account(
-            context.alice.address
-        );
-        const { data: treasuryCurrentBalance } = await context.api.query.system.account(
-            treasuryAccount
-        );
-
-		// Calculate transaction fee 
-		const txFee = aliceInitBalance.free.toNumber() - aliceCurrentBalance.free.toNumber() - transferAmount;
-		const treasuryBalanceIncrease = treasuryCurrentBalance.free.toNumber() - treasuryInitBalance.free.toNumber();
-
-		console.log(`The actual transaction fee is ${txFee}, and the Treasury pot has an balance increase of ${treasuryBalanceIncrease}`)
-
-		const totalProportion = TREASURY_PROPORTION + AUTHOR_PROPORTION + BURNED_PROPORTION;
-		expect(treasuryBalanceIncrease).to.approximately(txFee / totalProportion * TREASURY_PROPORTION, 10);
-    });
 });
