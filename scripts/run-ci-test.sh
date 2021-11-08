93a50b910458e6230af1f5e113b4823fde797468
Kai <7630809+Kailai-Wang@users.noreply.github.com>
2021-11-08 13:57:19 +0100

    register parachain for launch-local-binary (#223)

--- scripts/run-ci-test.sh
+++ scripts/run-ci-test.sh
@@ -8,9 +8,6 @@ cd "$ROOTDIR/ts-tests"
 TMPDIR=/tmp/parachain_dev
 [ -d "$TMPDIR" ] || mkdir -p "$TMPDIR"
 
-echo "NODE_ENV=ci" > .env
+[ -f .env ] || echo "NODE_ENV=ci" > .env
 yarn
-if [ "$1" != "docker" ]; then
-  yarn register-parachain 2>&1 | tee "$TMPDIR/register-parachain.log"
-fi
 yarn test 2>&1 | tee "$TMPDIR/parachain_ci_test.log"
