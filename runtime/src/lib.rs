a14714366877b91e0a977ebc0b905093c4fdbaf6
Kailai Wang <Kailai.Wang@hotmail.com>
2021-11-11 15:30:36 +0100

    cargo fmt

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -1,16 +1,16 @@
 // Copyright 2020-2021 Litentry Technologies GmbH.
 // This file is part of Litentry.
-// 
+//
 // Litentry is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // Litentry is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with Litentry.  If not, see <https://www.gnu.org/licenses/>.
 
@@ -22,8 +22,8 @@
 #[cfg(feature = "std")]
 include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
 
-pub mod weights;
 pub mod constants;
+pub mod weights;
 pub use constants::currency::*;
 
 pub use primitives::{opaque, Index, *};
@@ -898,14 +898,14 @@ construct_runtime! {
 
 pub struct BaseCallFilter;
 impl Contains<Call> for BaseCallFilter {
-    fn contains(call: &Call) -> bool {
-        matches!(
-            call,
-            Call::Sudo(_) |
+	fn contains(call: &Call) -> bool {
+		matches!(
+			call,
+			Call::Sudo(_) |
             // System
             Call::System(_) | Call::Timestamp(_) | Call::ParachainSystem(_)
-        )
-    }
+		)
+	}
 }
 
 impl_runtime_apis! {
