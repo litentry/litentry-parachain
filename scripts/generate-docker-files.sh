84b2e17b22afbb6523fcc813ef954a4ae6de378c
Kailai Wang <Kailai.Wang@hotmail.com>
2021-12-03 16:11:03 +0100

    remove parachain-id from docker-compose file

--- scripts/generate-docker-files.sh
+++ scripts/generate-docker-files.sh
@@ -31,5 +31,10 @@ print_divider
 
 "$PARCHAIN_LAUNCH_BIN" generate --config="parachain-launch-config-$CHAIN_TYPE.yml" --output="generated-$CHAIN_TYPE" --yes
 
+# workaround for polkadot-v0.9.13
+# the runCmd doesn't accept '--parachain-id=' parameter anymore
+# remove it manually before parachain-launch is aligned with upstream
+sed -i.bak '/parachain-id/d' "generated-$CHAIN_TYPE/docker-compose.yml"
+
 echo "Done, please check files under $ROOTDIR/docker/generated-$CHAIN_TYPE/"
 print_divider
