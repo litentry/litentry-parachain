e8919e9fb7e9bd7e253a26d9e4883a73b47413d4
suinuj <junius@litentry.com>
2022-05-25 20:43:30 +0800

    init identity

---
+++ pallets/identity-management/src/lib.rs
@@ -0,0 +1,61 @@
+// Copyright 2020-2022 Litentry Technologies GmbH.
+// This file is part of Litentry.
+//
+// Litentry is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Litentry is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Litentry.  If not, see <https://www.gnu.org/licenses/>.
+
+// Ensure we're `no_std` when compiling for Wasm.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+pub use pallet::*;
+
+#[frame_support::pallet]
+pub mod pallet {
+	use frame_support::{
+		pallet_prelude::*,
+	};
+	use frame_system::pallet_prelude::*;
+	
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
+
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+	}
+
+	#[pallet::event]
+	pub enum Event<T: Config> {}
+
+	#[pallet::error]
+	pub enum Error<T> {
+	}
+
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		/// The extrinsic used to verify the response from TEE side
+		#[pallet::weight(1_000)]
+		pub fn dummy(
+			origin: OriginFor<T>,
+		) -> DispatchResult {
+			let _ = ensure_signed(origin)?;
+			log::error!(
+				target: "identity management",
+				"dummy called."
+			);
+			Ok(())
+		}
+	}
+}
