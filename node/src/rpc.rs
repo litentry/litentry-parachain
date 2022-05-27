ea1b6af8a8cd27f4ba52d64c304c0f01ed2b3215
Yang <yang@litentry.com>
2022-05-27 21:28:01 +0200

    remove redundant_clone

--- node/src/rpc.rs
+++ node/src/rpc.rs
@@ -64,7 +64,7 @@ where
 	let FullDeps { client, pool, deny_unsafe } = deps;
 
 	module.merge(SystemRpc::new(client.clone(), pool, deny_unsafe).into_rpc())?;
-	module.merge(TransactionPaymentRpc::new(client.clone()).into_rpc())?;
+	module.merge(TransactionPaymentRpc::new(client).into_rpc())?;
 
 	Ok(module)
 }
