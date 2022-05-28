239e941ec8c57d57290e61ea5a1f87390dfcbf90
Yang <yang@litentry.com>
2022-05-28 22:03:39 +0200

    move back WeightInfo init

--- runtime/litmus/src/lib.rs
+++ runtime/litmus/src/lib.rs
@@ -442,7 +442,7 @@ impl pallet_utility::Config for Runtime {
 	type Event = Event;
 	type Call = Call;
 	type PalletsOrigin = OriginCaller;
-	type WeightInfo = ();
+	type WeightInfo = weights::pallet_utility::WeightInfo<Runtime>;
 }
 
 parameter_types! {
@@ -619,7 +619,7 @@ impl pallet_treasury::Config for Runtime {
 	type Burn = Burn;
 	type BurnDestination = ();
 	type SpendFunds = ();
-	type WeightInfo = ();
+	type WeightInfo = weights::pallet_treasury::WeightInfo<Runtime>;
 	type MaxApprovals = MaxApprovals;
 }
 
