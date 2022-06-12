ab49c20041a35f92d0f077aae04375e2de56709d
Kailai Wang <Kailai.Wang@hotmail.com>
2022-06-12 10:47:37 +0200

    fix into_account

--- runtime/rococo/src/tests/xcm_parachain/mod.rs
+++ runtime/rococo/src/tests/xcm_parachain/mod.rs
@@ -30,27 +30,26 @@ use frame_system::RawOrigin;
 use polkadot_parachain::primitives::{AccountIdConversion, RelayChainBlockNumber, Sibling};
 use sp_runtime::{traits::Convert, AccountId32};
 use xcm::prelude::*;
-// use xcm::latest::prelude::*;
 use xcm_simulator::TestExt;
 
 pub mod relay_sproof_builder;
 
 fn para_a_account() -> AccountId32 {
-	ParaId::from(1).into_account()
+	ParaId::from(1).into_account_truncating()
 }
 
 fn para_b_account() -> AccountId32 {
-	ParaId::from(2).into_account()
+	ParaId::from(2).into_account_truncating()
 }
 
 fn _sibling_a_account() -> AccountId32 {
 	use sp_runtime::traits::AccountIdConversion;
-	Sibling::from(1).into_account()
+	Sibling::from(1).into_account_truncating()
 }
 
 fn _sibling_b_account() -> AccountId32 {
 	use sp_runtime::traits::AccountIdConversion;
-	Sibling::from(2).into_account()
+	Sibling::from(2).into_account_truncating()
 }
 
 #[test]
