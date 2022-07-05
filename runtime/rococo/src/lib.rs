05ccc47a8a9be67a381c86793cc98c9a2a510132
Kailai Wang <Kailai.Wang@hotmail.com>
2022-07-05 11:41:08 +0200

    bump version for rococo and litmus

--- runtime/rococo/src/lib.rs
+++ runtime/rococo/src/lib.rs
@@ -151,7 +151,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	authoring_version: 1,
 	// same versioning-mechanism as polkadot:
 	// last digit is used for minor updates, like 9110 -> 9111 in polkadot
-	spec_version: 9080,
+	spec_version: 9081,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
