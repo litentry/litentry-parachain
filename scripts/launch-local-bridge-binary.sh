9ca168418de4f220ce1837dc066fb993bec38332
zhiming-zhong <zhiming.zhong@litentry.com>
2022-08-05 15:38:57 +0800

    build chainBridge

--- scripts/launch-local-bridge-binary.sh
+++ scripts/launch-local-bridge-binary.sh
@@ -2,7 +2,7 @@
 
 ROOTDIR=$(git rev-parse --show-toplevel)
 
-GOPATH=${HOME}/go go install github.com/litentry/ChainBridge@dev
+GOPATH=${HOME}/go go install github.com/litentry/ChainBridge/cmd/chainbridge@dev
 
 ${ROOTDIR}/scripts/geth/run_geth.sh
 
