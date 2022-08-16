3a11197335cc63320c0725c14c4890b72f63ea3e
zhiming-zhong <zhiming.zhong@litentry.com>
2022-08-16 21:51:08 +0800

    update scripts

--- scripts/launch-local-bridge-binary.sh
+++ scripts/launch-local-bridge-binary.sh
@@ -5,16 +5,9 @@ TMPDIR=/tmp/parachain_dev
 
 ROOTDIR=$(git rev-parse --show-toplevel)
 
-if [[ "$(docker image inspect litentry/chainbridge:latest 2>/dev/null)" == "[]" ]]; then
-    echo "litentry/chainbridge:latest image not found..."
-    ${ROOTDIR}/scripts/build-bridge-docker.sh
-fi
+GOPATH=${HOME}/go go install github.com/litentry/ChainBridge/cmd/chainbridge@dev
 
-echo "------------------------------------------------------------"
-
-docker run -d --rm --name chainbridge litentry/chainbridge bash -c 'ls /go/bin/ && sleep 5'
-docker cp chainbridge:/go/bin/chainbridge ${TMPDIR}/
-echo "copy binary:chainbridge to ${TMPDIR}"
+cp ${HOME}/go/bin/chainbridge $TMPDIR/chainbridge
 
 ${ROOTDIR}/scripts/geth/run_geth.sh
 
