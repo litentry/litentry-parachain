114fd0a709493aa968cd6e535f068a891b0c097b
Kailai Wang <Kailai.Wang@hotmail.com>
2022-09-16 14:39:19 +0200

    use sp-std vec

--- pallets/identity-management-mock/src/validation_data.rs
+++ pallets/identity-management-mock/src/validation_data.rs
@@ -20,21 +20,28 @@
 //
 // It's written in this IMP-mock crate just to give an idea how the
 // `validation_data` should look like, the decoding and usage of it
-// is done within TEE
+// is done within TEE, where std(or sgx_tstd) can be enabled.
+//
+// The validation data itself is always sent along with DID, so the
+// the struct doesn't include did.
 
 use codec::{Decode, Encode};
 use sp_runtime::MultiSignature;
+use sp_std::vec::Vec;
 
+// for now we use a simple link for web2 validation.
+// When it's not enough, we'll have to extend it and/or
+// write platform-specific struct
 #[derive(Encode, Decode, Clone, Debug, PartialEq, Eq)]
 #[allow(non_camel_case_types)]
 pub struct Web2ValidationData {
-	pub link: String,
+	pub link: Vec<u8>, // or String if under std
 }
 
 #[derive(Encode, Decode, Clone, Debug, PartialEq, Eq)]
 #[allow(non_camel_case_types)]
 pub struct Web3ValidationData {
-	pub message: String,
+	pub message: Vec<u8>, // or String if under std
 	pub signature: MultiSignature,
 }
 
