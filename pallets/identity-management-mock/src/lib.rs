9deaf05a00e2085115df1b4b9f0e15aa572a1788
Kailai Wang <Kailai.Wang@hotmail.com>
2022-09-22 18:43:20 +0200

    implement verification

--- pallets/identity-management-mock/src/lib.rs
+++ pallets/identity-management-mock/src/lib.rs
@@ -39,13 +39,18 @@ mod tests;
 use frame_support::{pallet_prelude::*, traits::ConstU32};
 pub use pallet::*;
 use pallet_identity_management::{MrenclaveType, ShardIdentifier};
-use sp_io::hashing::blake2_128;
+use sp_core::{ed25519, sr25519};
+use sp_io::{
+	crypto::{
+		ed25519_verify, secp256k1_ecdsa_recover, secp256k1_ecdsa_recover_compressed, sr25519_verify,
+	},
+	hashing::{blake2_128, blake2_256, keccak_256},
+};
 use sp_runtime::DispatchError;
 use sp_std::prelude::*;
 use tee_primitives::{
-	EthereumSignature, Identity, IdentityHandle, IdentityMultiSignature, IdentityString,
-	IdentityWebType, ValidationData, Web2Network, Web3CommonValidationData, Web3Network,
-	Web3ValidationData,
+	Identity, IdentityHandle, IdentityMultiSignature, IdentityWebType, ValidationData,
+	Web3CommonValidationData, Web3Network, Web3ValidationData,
 };
 
 mod identity_context;
@@ -67,6 +72,7 @@ pub mod pallet {
 	use super::*;
 	use frame_system::pallet_prelude::*;
 	use pallet_identity_management::UserShieldingKeyType;
+
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
 	pub struct Pallet<T>(_);
@@ -180,10 +186,26 @@ pub mod pallet {
 		VerificationRequestTooEarly,
 		/// a verification reqeust comes too late
 		VerificationRequestTooLate,
+		/// verify substrate signature failed
+		VerifySubstrateSignatureFailed,
+		/// recover substrate pubkey using ecdsa failed
+		RecoverSubstratePubkeyFailed,
+		/// verify evm signature failed
+		VerifyEvmSignatureFailed,
 		/// the linking request block is zero
 		LinkingRequestBlockZero,
 		/// the challenge code doesn't exist
 		ChallengeCodeNotExist,
+		/// compute evm message digest failed
+		ComputeEvmMessageDigestFailed,
+		/// wrong signature type
+		WrongSignatureType,
+		/// wrong web3 network type
+		WrongWeb3NetworkType,
+		/// wrong identity handle type
+		WrongIdentityHanldeType,
+		/// fail to recover evm pubkey
+		RecoverEvmPubkeyFailed,
 	}
 
 	#[pallet::storage]
@@ -191,9 +213,6 @@ pub mod pallet {
 	pub type WhitelistedCallers<T: Config> =
 		StorageMap<_, Twox64Concat, T::AccountId, (), OptionQuery>;
 
-	/// We use stf_primitives::AccountId instead of T::AcoundId as key for the TEE-releated storages
-	/// - for simplicity
-	/// - fo
 	/// user shielding key is per Litentry account
 	#[pallet::storage]
 	#[pallet::getter(fn user_shielding_keys)]
@@ -390,7 +409,8 @@ pub mod pallet {
 			let validation_data = ValidationData::decode(&mut decrypted_validation_data.as_slice())
 				.map_err(|_| Error::<T>::WrongDecodedType)?;
 
-			// do an actual verification for web3
+			// do an actual verification for web3, we can't do it for web2 as it requires offchain
+			// verification
 			if let ValidationData::Web3(web3_validation_data) = validation_data {
 				match web3_validation_data {
 					Web3ValidationData::Substrate(substrate_validation_data) => {
@@ -522,18 +542,68 @@ pub mod pallet {
 			identity: &Identity,
 			validation_data: &Web3CommonValidationData,
 		) -> DispatchResult {
-			let msg = get_expected_web3_message(who, identity)?;
+			let msg = Self::get_expected_web3_message(who, identity)?;
+
+			let substrate_address = match &identity.web_type {
+				IdentityWebType::Web3(Web3Network::Substrate(_)) => match &identity.handle {
+					IdentityHandle::Address32(handle) => handle,
+					_ => return Err(Error::<T>::WrongIdentityHanldeType.into()),
+				},
+				_ => return Err(Error::<T>::WrongWeb3NetworkType.into()),
+			};
+
+			match &validation_data.signature {
+				IdentityMultiSignature::Sr25519(sig) => {
+					ensure!(
+						sr25519_verify(sig, &msg, &sr25519::Public(*substrate_address)),
+						Error::<T>::VerifySubstrateSignatureFailed
+					);
+				},
+				IdentityMultiSignature::Ed25519(sig) => {
+					ensure!(
+						ed25519_verify(sig, &msg, &ed25519::Public(*substrate_address)),
+						Error::<T>::VerifySubstrateSignatureFailed
+					);
+				},
+				IdentityMultiSignature::Ecdsa(sig) => {
+					let digest = keccak_256(&msg);
+					let recovered_substrate_pubkey =
+						secp256k1_ecdsa_recover_compressed(&sig.0, &digest)
+							.map_err(|_| Error::<T>::RecoverSubstratePubkeyFailed)?;
+					ensure!(
+						&blake2_256(&recovered_substrate_pubkey) == substrate_address,
+						Error::<T>::VerifySubstrateSignatureFailed
+					);
+				},
+				_ => return Err(Error::<T>::WrongSignatureType.into()),
+			}
 			Ok(())
 		}
 
+		// mostly copied from the old account-linker, the msg digest is computed using EIP-191
+		// TODO: use external crates
 		fn verify_evm_signature(
 			who: &T::AccountId,
 			identity: &Identity,
 			validation_data: &Web3CommonValidationData,
 		) -> DispatchResult {
 			let msg = Self::get_expected_web3_message(who, identity)?;
-			let hash =
-				Self::get_evm_msg_hash(msg).map_err(|_| Error::<T>::UnexpectedEthMsgLength)?;
+			let digest = Self::compute_evm_msg_digest(msg)
+				.map_err(|_| Error::<T>::ComputeEvmMessageDigestFailed)?;
+			if let IdentityMultiSignature::Ethereum(sig) = &validation_data.signature {
+				let recovered_evm_pubkey = Self::recover_evm_address(&digest, sig.as_ref())
+					.map_err(|_| Error::<T>::RecoverEvmPubkeyFailed)?;
+				let evm_address = match &identity.web_type {
+					IdentityWebType::Web3(Web3Network::Evm(_)) => match &identity.handle {
+						IdentityHandle::Address20(handle) => handle,
+						_ => return Err(Error::<T>::WrongIdentityHanldeType.into()),
+					},
+					_ => return Err(Error::<T>::WrongWeb3NetworkType.into()),
+				};
+				ensure!(&recovered_evm_pubkey == evm_address, Error::<T>::VerifyEvmSignatureFailed);
+			} else {
+				return Err(Error::<T>::WrongSignatureType.into())
+			}
 			Ok(())
 		}
 
@@ -551,13 +621,46 @@ pub mod pallet {
 			Ok(msg)
 		}
 
-		pub fn get_evm_msg_hash(mut msg: Vec<u8>) -> Result<[u8; 32], &'static str> {
-			ensure!(msg.len() == 51, Err(format!("wrong msg length {}", msg.len)));
-			let mut length_bytes = usize_to_u8_array(data.len())?;
-			let mut eth_data = b"\x19Ethereum Signed Message:\n".encode();
-			eth_data.append(&mut length_bytes);
-			eth_data.append(&mut data);
-			Ok(sp_io::hashing::keccak_256(&eth_data))
+		fn compute_evm_msg_digest(mut msg: Vec<u8>) -> Result<[u8; 32], &'static str> {
+			let mut length_bytes = Self::usize_to_u8_array(msg.len())?;
+			let mut eth_bytes = b"\x19Ethereum Signed Message:\n".encode();
+			eth_bytes.append(&mut length_bytes);
+			eth_bytes.append(&mut msg);
+			Ok(keccak_256(&eth_bytes))
+		}
+
+		/// Convert a usize type to a u8 array.
+		/// The input is first converted as a string with decimal presentation,
+		/// and then this string is converted to a byte array with UTF8 encoding.
+		/// To avoid unnecessary complexity, the current function supports up to
+		/// 2 digits unsigned decimal (range 0 - 99)
+		fn usize_to_u8_array(length: usize) -> Result<Vec<u8>, &'static str> {
+			if length >= 100 {
+				Err("Unexpected ethereum message length!")
+			} else {
+				let digits = b"0123456789".encode();
+				let tens = length / 10;
+				let ones = length % 10;
+
+				let mut vec_res: Vec<u8> = Vec::new();
+				if tens != 0 {
+					vec_res.push(digits[tens]);
+				}
+				vec_res.push(digits[ones]);
+				Ok(vec_res)
+			}
+		}
+
+		fn recover_evm_address(
+			msg: &[u8; 32],
+			sig: &[u8; 65],
+		) -> Result<[u8; 20], sp_io::EcdsaVerifyError> {
+			let pubkey = secp256k1_ecdsa_recover(sig, msg)?;
+			let hashed_pk = keccak_256(&pubkey);
+
+			let mut addr = [0u8; 20];
+			addr[..20].copy_from_slice(&hashed_pk[12..32]);
+			Ok(addr)
 		}
 	}
 }
