784cc04e850914348a2c3ddacbae19753b2049a5
BillyWooo <yang@litentry.com>
2022-10-20 08:50:27 +0200

    rename ruleset -> assertion

--- pallets/vc-management/src/ruleset.rs
+++ pallets/vc-management/src/ruleset.rs
@@ -20,26 +20,26 @@
 // See: https://www.notion.so/litentry/Expected-parameters-in-predefined-rulesets-14f74928aa2b43509167da12a3e75507
 
 use codec::{Decode, Encode, MaxEncodedLen};
-use primitives::Balance;
 use scale_info::TypeInfo;
 use sp_runtime::{traits::ConstU32, BoundedVec};
 
+type Balance = u128;
 type MaxStringLength = ConstU32<64>;
 pub type ParameterString = BoundedVec<u8, MaxStringLength>;
 
 #[derive(Encode, Decode, Clone, Debug, PartialEq, Eq, TypeInfo, MaxEncodedLen)]
-pub enum Ruleset {
-	R1,
-	R2(ParameterString, ParameterString), // (channel_id, guild_id)
-	R3(ParameterString, ParameterString), // (channel_id, guild_id)
-	R4,
-	R5(ParameterString, ParameterString), // (twitter_account, tweet_id)
-	R6,
-	R7(Balance, u32), // (DOT_amount, year)
-	R8(u64),          // (tx_amount)
-	R9,
-	R10(Balance, u32), // (WBTC_amount, year)
-	R11(Balance, u32), // (ETH_amount, year)
-	R12(Balance, u32), // (LIT_amount, year)
-	R13(u32),          // (Karma_amount) - TODO: unsupported
+pub enum Assertion {
+	Assert1,
+	Assert2(ParameterString, ParameterString), // (guild_id, user_id)
+	Assert3(ParameterString, ParameterString), // (guild_id, user_id)
+	Assert4,
+	Assert5(ParameterString, ParameterString), // (twitter_account, tweet_id)
+	Assert6,
+	Assert7(Balance, u32), // (DOT_amount, year)
+	Assert8(u64),          // (tx_amount)
+	Assert9,
+	Assert10(Balance, u32), // (WBTC_amount, year)
+	Assert11(Balance, u32), // (ETH_amount, year)
+	Assert12(Balance, u32), // (LIT_amount, year)
+	Assert13(u32),          // (Karma_amount) - TODO: unsupported
 }
