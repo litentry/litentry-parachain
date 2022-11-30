f85a91be74b154ce126d316713d9ac06448c2670
Kai <7630809+Kailai-Wang@users.noreply.github.com>
2022-11-30 04:35:21 +0100

    Switch to local dependencies (#1037)
    * use local dependencies
    
    * fix docker path

--- tee-worker/scripts/litentry/build_parachain_docker.sh
+++ tee-worker/scripts/litentry/build_parachain_docker.sh
@@ -2,17 +2,5 @@
 set -euo pipefail
 
 ROOTDIR=$(git rev-parse --show-toplevel)
-ROOTDIR="$ROOTDIR/tee-worker"
-
 cd "$ROOTDIR"
-# with '#' so that it filters out the pallet dependency
-SHA=$(grep -F 'https://github.com/litentry/litentry-parachain.git?branch=tee-dev#' Cargo.lock | head -n1 | sed 's/.*#//;s/"$//')
-
-PARACHAIN_DIR=/tmp/litentry-parachain
-[ -d "$PARACHAIN_DIR" ] && rm -rf "$PARACHAIN_DIR"
-git clone https://github.com/litentry/litentry-parachain "$PARACHAIN_DIR"
-cd "$PARACHAIN_DIR"
-git checkout tee-dev
-git checkout "$SHA"
-
 ./scripts/build-docker.sh release tee-dev --features=tee-dev
