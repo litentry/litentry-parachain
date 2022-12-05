d9c059c8fb744c640451a0bd3b9adccbcfd0a8e0
Kailai Wang <Kailai.Wang@hotmail.com>
2022-12-05 19:26:20 +0100

    update script

--- scripts/fork-parachain-and-launch.sh
+++ scripts/fork-parachain-and-launch.sh
@@ -18,17 +18,16 @@ function print_divider() {
 
 function usage() {
   print_divider
-  echo "Usage: $0 [ws-rpc-endpoint] [orig-chain] [fork-chain] [binary]"
+  echo "Usage: $0 [ws-rpc-endpoint] [rococo|litmus|litentry] [binary]"
   echo 
   echo "default:"
   echo "ws-rpc-endpoint:   ws://127.0.0.1:9944"
-  echo "orig-chain:        rococo"
-  echo "fork-chain:        rococo-dev"
+  echo "chain:             rococo"
   echo "binary:            the binary copied from litentry/litentry-parachain:latest"
   print_divider
 }
 
-[ $# -gt 4 ] && (usage; exit 1)
+[ $# -gt 3 ] && (usage; exit 1)
 
 case "$1" in
   help|-h|--help)
@@ -41,16 +40,11 @@ esac
 
 ENDPOINT="${1:-ws://127.0.0.1:9944}"
 ORIG_CHAIN=${2:-rococo}
-FORK_CHAIN=${3:-rococo-dev}
-CHAIN_TYPE=
+FORK_CHAIN=${ORIG_CHAIN}-dev
 
-case "$FORK_CHAIN" in
-  litmus*)
-    CHAIN_TYPE=litmus ;;
-  litentry*)
-    CHAIN_TYPE=litentry ;;
-  rococo*)
-    CHAIN_TYPE=rococo ;;
+case "$ORIG_CHAIN" in
+  litmus|rococo|litentry)
+    ;;
   *)
     echo "unsupported chain type"
     exit 1 ;;
@@ -59,8 +53,8 @@ esac
 echo "TMPDIR is $TMPDIR"
 cd "$TMPDIR"
 git clone "$FORK_OFF_SUBSTRATE_REPO"
-git checkout wss-fork
 cd fork-off-substrate
+git checkout wss-fork
 npm i
 
 mkdir data && cd data
@@ -72,19 +66,6 @@ else
   cp "$4" binary
 fi
 chmod a+x binary
-
-# pop up a warning if on non-CI host
-if [ $(hostname) != "ubuntu-16gb-CI" ]; then
-  echo "WARNING: it seems you are not on the CI host"
-  echo "         please make sure the given HTTP-RPC endpoint accessible"
-else
-  # open the ssh port forwarding for a short time
-  ssh -f -L 9900:localhost:9933 litmus-sg-rpc0 sleep 120
-fi
-
-# retrieve the live wasm
-curl -s -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "state_getStorage", "params": [ "0x3a636f6465" ]}' "$ENDPOINT" | \
-jq .result | sed 's/"//g;s/^0x//' | xxd -r -p > runtime.wasm
 
 # write .env file
 cd ..
@@ -105,7 +86,7 @@ fi
 cp -f data/fork.json "$ROOTDIR/docker/"
 
 cd "$ROOTDIR"
-sed -i.bak "s;$FORK_CHAIN;fork.json;" docker/$CHAIN_TYPE-parachain-launch-config.yml
+sed -i.bak "s;$FORK_CHAIN;fork.json;" docker/$ORIG_CHAIN-parachain-launch-config.yml
 
 # start the network
-# make launch-docker-$CHAIN_TYPE
+make launch-docker-$ORIG_CHAIN
