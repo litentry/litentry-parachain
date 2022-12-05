084dbf3054265c1924a1deeba0b9b4146fe99886
Verin1005 <104152026+Verin1005@users.noreply.github.com>
2022-12-05 18:14:48 +0800

    Ts tests (#1053)
    * add cross-env&&dotenv
    
    * set env
    
    * random shard for testing
    
    * Change the versions of @polkadot
    
    * change the version of @polkadot/types
    
    * rm yarn.lock
    
    * change yarn.lock
    
    * generateChallengeCode
    
    * modify yarn run command
    
    * change defaultSinger
    
    * add getSinger
    
    * add Sign functions
    
    * add getSinger && generateChallengeCode
    
    * add ID_HUB_URL for sign message
    
    * modify defaultSigner
    
    * modify shard

---
+++ tee-worker/ts-tests/web3/setup.ts
@@ -0,0 +1,16 @@
+import { Keyring } from "@polkadot/api";
+import type { KeyringPair } from "@polkadot/keyring/types";
+const keyring = new Keyring({ type: "sr25519" });
+const crypto = require("crypto");
+export function getSinger(index: number): KeyringPair {
+    let Alice = keyring.addFromUri("//Alice", { name: "Alice" });
+    let Bob = keyring.addFromUri("//Bob", { name: "Bob" });
+    let Charlie = keyring.addFromUri("//Charlie", { name: "Charlie" });
+    let Eve = keyring.addFromUri("//Eve", { name: "Eve" });
+    const signers = [Alice, Bob, Charlie, Eve];
+    return signers[index];
+}
+
+export function generateChallengeCode(): String {
+    return crypto.randomBytes(16).toString("hex");
+}
