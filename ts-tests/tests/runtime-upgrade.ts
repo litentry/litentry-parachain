18d537e5f2301e00c59eb3fc7430ae18d23f86ad
Scc <chengcheng@litentry.com>
2022-12-09 22:05:17 +0800

    optimize ts-code & cli

--- ts-tests/tests/runtime-upgrade.ts
+++ ts-tests/tests/runtime-upgrade.ts
@@ -18,12 +18,11 @@ export function sleep(ms: number) {
 }
 
 async function authorizeUpgrade(admin: KeyringPair, codeHash: string) {
-    const config = loadConfig();
-    const provider = new WsProvider(config.parachain_ws);
+    const config = loadConfig()
+    const provider = new WsProvider(config.parachain_ws)
     const api = await ApiPromise.create({
         provider: provider,
     });
-
 
     await api.tx.sudo
         .sudo(api.tx.parachainSystem.authorizeUpgrade(codeHash))
@@ -43,33 +42,111 @@ async function authorizeUpgrade(admin: KeyringPair, codeHash: string) {
 
 async function upgrade(admin: KeyringPair, code: string) {
 
-    const config = loadConfig();
-    const provider = new WsProvider(config.parachain_ws);
+    const config = loadConfig()
+    const provider = new WsProvider(config.parachain_ws)
     const api = await ApiPromise.create({
         provider: provider,
     });
 
     const proposal = api.tx.parachainSystem.enactAuthorizedUpgrade(`0x${code}`)
 
+    const old_version =await runtime_version(api)
+
     console.log(`Upgrading from ${admin.address}, ${code.length / 2} bytes`)
     // Perform the actual chain upgrade via the sudo module
-    await api.tx.sudo
-        .sudo(proposal)
-        .signAndSend(admin, ({events, status}: ISubmittableResult) => {
-            console.log('Proposal status:', status.type)
-            if (status.isInBlock) {
-                console.error('You have just upgraded your chain')
+    const tx = api.tx.sudo.sudo(proposal)
+    await signAndSend(tx, admin)
 
-                console.log('Included at block hash', status.asInBlock.toHex())
-                console.log('Events:')
+    console.log("tx send successful ensure the new runtime version~~ please wait a later")
 
-                // @ts-ignore
-                console.log(JSON.stringify(events.toString(), null, 2))
-            } else if (status.isFinalized) {
-                console.log('Finalized block hash', status.asFinalized.toHex())
+    await sleep(72000)
+
+    const new_version = await runtime_version(api)
+
+    if (new_version.specVersion >old_version.specVersion){
+        console.log("version upgrade successful")
+    }else{
+        console.log(`old version:${old_version.toString()}  new version ${new_version.toString()},runtime upgrade failed `)
+    }
+
+    // send a tx verify the chain produce blocks
+    await transfer(api,admin)
+
+    process.exit(0)
+
+}
+async function runtime_version(api: ApiPromise) {
+
+    const runtime_version = await api.call.core.version()
+
+    return runtime_version
+}
+
+// event
+async function listenEvent(
+    api:ApiPromise,
+    filterObj: { module: string; method: string; event: string }
+) {
+    return new Promise(async (resolve, reject) => {
+        let startBlock = 0;
+        let timeout = 10; // 10 block number timeout
+        const unsubscribe = await api.rpc.chain.subscribeNewHeads(async (header) => {
+            const currentBlockNumber = header.number.toNumber();
+            if (startBlock == 0) startBlock = currentBlockNumber;
+            if (currentBlockNumber > startBlock + timeout) {
+                reject("timeout");
+                return;
             }
-        })
-    console.log(`parachain runtime upgrade ready`)
+            console.log(`Chain is at block: #${header.number}`);
+            const signedBlock = await api.rpc.chain.getBlock(header.hash);
+
+            const allEvents = (await api.query.system.events.at(
+                header.hash
+            ));
+            signedBlock.block.extrinsics.forEach((ex, index) => {
+                if (
+                    !(
+                        ex.method.section === filterObj.module &&
+                        ex.method.method === filterObj.method
+                    )
+                ) {
+                    return;
+                }
+                allEvents
+                    .filter(({ phase, event }) => {
+                        return (
+                            phase.isApplyExtrinsic &&
+                            phase.asApplyExtrinsic.eq(index) &&
+                            event.section == filterObj.module &&
+                            event.method == filterObj.event
+                        );
+                    })
+                    .forEach(({ event }) => {
+                        // const eventData = event.data as AESOutput;
+                        const data = event.data;
+                        const eventData:string[] = [];
+                        for (let i = 0; i < data.length; i++) {
+                            eventData.push();
+                        }
+                        resolve({ eventData });
+                        unsubscribe();
+                        return;
+                    });
+            });
+        });
+    });
+}
+
+//  transfer  function verify the runtime upgrade
+async function transfer(
+    api:ApiPromise,
+    admin: KeyringPair,
+) {
+    const bob = keyring.addFromUri('//Bob', {name: 'Bob'})
+    const tx = await api.tx.balances
+        .transfer(bob.address, 1000000000000)
+
+    await signAndSend(tx,admin)
 
 }
 
@@ -84,7 +161,7 @@ async function upgrade(admin: KeyringPair, code: string) {
 
     const admin = keyring.addFromUri('//Alice', {name: 'Alice'})
     await authorizeUpgrade(admin, codeHash)
-    // event
+
     await sleep(24000)
     await upgrade(admin, code)
 
