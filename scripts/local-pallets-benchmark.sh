1e1b9f05784e9814a967211d482bae163aba4882
Scc <chengcheng@litentry.com>
2022-12-13 10:18:01 +0800

    fix benmarking scripts

--- scripts/local-pallets-benchmark.sh
+++ scripts/local-pallets-benchmark.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-# This script can be used for running moonbeam's benchmarks.
+# This script can be used for running litentry's benchmarks.
 #
-# The moonbeam binary is required to be compiled with --features=runtime-benchmarks
+# The litentry binary is required to be compiled with make build-node-benchmarks
 # in release mode.
 
 set -e
@@ -12,7 +12,7 @@ BINARY="./target/release/litentry-collator"
 
 if [[ ! -f "${BINARY}" ]]; then
     echo "binary '${BINARY}' does not exist."
-    echo "ensure that the moonbeam binary is compiled with 'make build-node-benchmarks ' nd in release mode."
+    echo "ensure that the litentry binary is compiled with 'make build-node-benchmarks ' nd in release mode."
     exit 1
 fi
 
@@ -21,17 +21,21 @@ function help {
     echo "  ${0} litentry|litmuts|rococo pallet_name benchmark_method  [--check]"
     echo ""
     echo "EXAMPLES:"
-    echo "  ${0}                       " "list all benchmarks and provide a selection to choose from" 
-    echo "  ${0} --check               " "list all benchmarks and provide a selection to choose from, runs in 'check' mode (reduced steps and repetitions)" 
-    echo "  ${0} foo bar               " "run a benchmark for pallet 'foo' and benchmark 'bar'" 
-    echo "  ${0} foo bar --check       " "run a benchmark for pallet 'foo' and benchmark 'bar' in 'check' mode (reduced steps and repetitions)" 
-    echo "  ${0} foo bar --all         " "run a benchmark for all pallets" 
-    echo "  ${0} foo bar --all --check " "run a benchmark for all pallets in 'check' mode (reduced steps and repetitions)" 
+    echo "  ${0}                       " "list all benchmarks and provide a selection to choose from"
+    echo "  ${0} --check               " "list all benchmarks and provide a selection to choose from, runs in 'check' mode (reduced steps and repetitions)"
+    echo "  ${0} foo bar               " "run a benchmark for pallet 'foo' and benchmark 'bar'"
+    echo "  ${0} foo bar --all         " "run a benchmark for all pallets"
+    echo "  ${0} foo bar --all --check " "run a benchmark for all pallets in 'check' mode (reduced steps and repetitions)"
 }
 
-#echo "make output dir"
-#mkdir weights
-CHAIN_TYPE="--chain=$1-dev"
+WEIGHTS_PATH="$(pwd)/weights"
+if [ ! -d $WEIGHTS_PATH ];then
+  mkdir -p  weights/
+
+fi
+
+CHAIN_TYPE="--chain=${1}-dev"
+PALLET=${2//-/_}
 
 function choose_and_bench {
     readarray -t options < <(${BINARY} benchmark pallet --list $CHAIN_TYPE | sed 1d)
@@ -41,17 +45,16 @@ function choose_and_bench {
         IFS=', ' read -ra parts <<< "${opt}"
         [[ "${opt}" == 'EXIT' ]] && exit 0
 
-        echo "inputs args =====> ${parts[0]}" "${parts[1]}" "${2}"
-        bench "${parts[0]}" "${parts[1]}" "${1}"
+        bench "${parts[0]}" "${parts[1]}" "${2}"
         break
     done
 }
 
 function bench {
     echo "pallet  ${1}-weights.rs"
-    OUTPUT="--output=$(pwd)/weights/${1}-weights.rs"
+    OUTPUT="--output=$(pwd)/weights/${1}_weights.rs"
 
-    echo "benchmarking '${2}::${3}' --check=${4}, writing results to '${OUTPUT}'"
+    echo "benchmarking '${1}::${2}' --check=${3}, writing results to '${OUTPUT}'"
 
     if [[ $PALLET == *"parachain_staking"* ]]; then
         echo "will run ${1} benchmark code"
@@ -59,18 +62,16 @@ function bench {
         REPEAT=20
     else
         echo "will run other pallet (${1}) benchmark code"
-        STEPS=20
-        REPEAT=50
+        STEPS=2
+        REPEAT=5
     fi
-
-    echo "chain_type <====> $CHAIN_TYPE "
 
     WASMTIME_BACKTRACE_DETAILS=1 ${BINARY} benchmark pallet \
               $CHAIN_TYPE \
               --execution=wasm  \
               --db-cache=20 \
               --wasm-execution=compiled \
-              --pallet="${2}" \
+              --pallet="$PALLET" \
               --extrinsic="${3}" \
               --heap-pages=4096 \
               --steps="$STEPS" \
@@ -96,7 +97,6 @@ else
     fi
 
     if [[ "${ALL}" -eq 1 ]]; then
-        mkdir -p weights/
         bench '*' '*' "${CHECK}" "weights/"
     elif [[ $# -ne 2 ]]; then
         choose_and_bench "${CHECK}"
