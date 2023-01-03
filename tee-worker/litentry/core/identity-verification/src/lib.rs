3f6dc115653aa0e2e6a3022a292ea888591dd930
Kai <7630809+Kailai-Wang@users.noreply.github.com>
2023-01-03 10:21:22 +0100

    Support wrapped substrate signature verification (#1140)
    * support both versions of signatures
    
    * add mocked tests
    
    * fix some syntax
    
    * cargo update
    
    * add ts testcase
    
    * fix compile warnings
    
    Co-authored-by: zhizming-zhong <zhiming.zhong@litentry.com>

--- tee-worker/litentry/core/identity-verification/src/lib.rs
+++ tee-worker/litentry/core/identity-verification/src/lib.rs
@@ -49,9 +49,19 @@ pub mod error;
 
 // verification message format: <challeng-code> + <litentry-AccountId32> + <Identity>,
 // where <> means SCALE-encoded
-pub fn get_expected_payload(who: &AccountId, identity: &Identity, code: &ChallengeCode) -> Vec<u8> {
+pub fn get_expected_raw_message(
+	who: &AccountId,
+	identity: &Identity,
+	code: &ChallengeCode,
+) -> Vec<u8> {
 	let mut payload = code.encode();
 	payload.append(&mut who.encode());
 	payload.append(&mut identity.encode());
 	blake2_256(payload.as_slice()).to_vec()
+}
+
+// Get the wrapped version of the raw msg: <Bytes>raw_msg</Bytes>,
+// see https://github.com/litentry/litentry-parachain/issues/1137
+pub fn get_expected_wrapped_message(raw_msg: Vec<u8>) -> Vec<u8> {
+	["<Bytes>".as_bytes(), raw_msg.as_slice(), "</Bytes>".as_bytes()].concat()
 }
