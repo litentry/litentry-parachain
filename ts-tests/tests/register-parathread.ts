dd880cbd7354d175f0ba4875f2cd3aec920c814d
Chengcheng-S <87012234+Chengcheng-S@users.noreply.github.com>
2023-01-11 10:39:47 +0800

    optimize some ts code

--- ts-tests/tests/register-parathread.ts
+++ ts-tests/tests/register-parathread.ts
@@ -3,8 +3,7 @@ import '@polkadot/api-augment';
 import { ApiPromise, Keyring, WsProvider } from '@polkadot/api';
 import { TypeRegistry } from '@polkadot/types/create';
 import { Bytes } from '@polkadot/types';
-import type { ISubmittableResult } from "@polkadot/types/types";
-import {loadConfig, signAndSend, sleep} from './utils';
+import {loadConfig, signAndSend} from './utils';
 
 async function registerParathread(api: ApiPromise, config: any) {
     // Get keyring of Alice, who is also the sudo in dev chain spec
@@ -26,28 +25,6 @@ async function registerParathread(api: ApiPromise, config: any) {
 
     console.log(`Parachain registration tx Sent!`);
     return signAndSend(tx, alice);
-}
-
-async function onboarding_parachain(api:ApiPromise,config: any){
-    console.log("start onboarding parachain");
-    const keyring = new Keyring({ type: 'sr25519' });
-    const alice = keyring.addFromUri('//Alice');
-
-    // user can set the number
-    const Period_conut = 100;
-
-    return new Promise(async (resolvePromise, reject) => {
-        await api.tx.sudo
-            .sudo(api.tx.slots.forceLease(process.env.PARACHAIN_ID, alice.address, 0, 0, Period_conut))
-            .signAndSend(alice, ({ status }: ISubmittableResult) => {
-                console.log(`Current status is ${status}`);
-                if (status.isInBlock) {
-                    console.log(`Transaction included at blockHash ${status.asInBlock}`);
-                } else if (status.isFinalized) {
-                    resolvePromise(0)
-                }
-            });
-    })
 }
 
 (async () => {
