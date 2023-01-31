48ed052c15bc6e85c53a6e54d1af9f4140dbfdc5
zzz <zhiming.zhong@litentry.com>
2023-01-31 15:06:20 +0800

    add script pre-commit.sh

---
+++ scripts/pre-commit.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# It is recommended to execute the script before commit
+# which will help us to reduce test/fmt/clippy failures in CI
+
+start=$(date +%s)
+
+make fmt
+taplo fmt
+
+root_dir=$(git rev-parse --show-toplevel)
+CARGO_TARGET_DIR=${root_dir}/tmp
+
+cd "${root_dir}/tee-worker" || exit
+taplo fmt
+CARGO_TARGET_DIR=${CARGO_TARGET_DIR} cargo clippy -- -D warnings || exit
+
+cd "${root_dir}/tee-worker/enclave-runtime" || exit
+taplo fmt
+CARGO_TARGET_DIR=${CARGO_TARGET_DIR} cargo clippy -- -D warnings || exit
+
+cd "${root_dir}/tee-worker" || exit
+#RUST_LOG=info CARGO_TARGET_DIR=/root/work/tmp SKIP_WASM_BUILD=1 cargo test --release -- --show-output
+RUST_LOG=info CARGO_TARGET_DIR=${CARGO_TARGET_DIR} SKIP_WASM_BUILD=1 cargo test --release -- --show-output
+SGX_MODE=SW SKIP_WASM_BUILD=1 make
+
+cd "${root_dir}/tee-worker/bin" || exit
+./integritee-service test --all
+
+end=$(date +%s)
+echo "Elapsed Time: $((end-start)) seconds"
