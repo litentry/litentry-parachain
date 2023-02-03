4254aa036b173b01cbf2fd504410f31c53ae9ea4
zzz <zhiming.zhong@litentry.com>
2023-02-03 07:26:43 +0000

    try to implement

--- tee-worker/enclave-runtime/src/initialization/mod.rs
+++ tee-worker/enclave-runtime/src/initialization/mod.rs
@@ -276,6 +276,9 @@ pub(crate) fn migrate_shard(
 ) -> EnclaveResult<()> {
 	let state_handler = GLOBAL_STATE_HANDLER_COMPONENT.get()?;
 	let _ = state_handler.initialize_shard(new_shard)?;
+	if let Ok((state,_)) = state_handler.load_cloned(&old_shard){		
+		let _ = state_handler.reset(state, &new_shard)?;
+	}
 	Ok(())
 }
 
