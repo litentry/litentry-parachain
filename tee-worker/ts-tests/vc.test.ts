95baa6439c75087b063ba07c7b2f81dcaad37570
Verin1005 <daqingchong0809@gmail.com>
2023-02-07 12:01:26 +0000

    deleted comment code

--- tee-worker/ts-tests/vc.test.ts
+++ tee-worker/ts-tests/vc.test.ts
@@ -9,11 +9,8 @@ const assertion = <Assertion>{
     A2: ['A2', 'A2'],
     A3: ['A3', 'A3'],
     A4: [10, 'A4'],
-    // A5: ['A5', 'A5'],
-    // A6: 'A6',
     A7: [10, 10],
     A8: 'A8',
-    // A9: 'A9',
     A10: [10, 10],
 };
 describeLitentry('VC test', async (context) => {
