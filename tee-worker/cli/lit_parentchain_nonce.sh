2b88bafc908556c3cc0e3aacafb797549f020850
Kailai Wang <Kailai.Wang@hotmail.com>
2023-03-08 22:22:37 +0000

    try to add ci

--- tee-worker/cli/lit_parentchain_nonce.sh
+++ tee-worker/cli/lit_parentchain_nonce.sh
@@ -35,7 +35,7 @@ NODEURL=${NODEURL:-"ws://127.0.0.1"}
 WORKER1PORT=${WORKER1PORT:-2000}
 WORKER1URL=${WORKER1URL:-"wss://127.0.0.1"}
 
-CLIENT_BIN=${CLIENT_BIN:-"./bin/integritee-cli"}
+CLIENT_BIN=${CLIENT_BIN:-"./../bin/integritee-cli"}
 
 echo "Using client binary $CLIENT_BIN"
 echo "Using node uri $NODEURL:$NPORT"
