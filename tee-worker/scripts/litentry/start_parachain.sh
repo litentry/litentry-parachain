e48faf4aedc08bf76e55009f34b08c7f8d887b3e
Kai <7630809+Kailai-Wang@users.noreply.github.com>
2023-03-13 16:49:32 +0100

    Add port check prior to parachain launch (#1443)

--- tee-worker/scripts/litentry/start_parachain.sh
+++ tee-worker/scripts/litentry/start_parachain.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 set -euo pipefail
 
+# check the port before launching the parachain
+# this script is called in bothe launch.sh and launch.py
+#
+# please note this check doesn't apply to standalone integritee-node
+# as it's started without any pre-check script bound
+#
+# 9944: default ws port for parachain node
+# 30333: default p2p port for relaychain node
+# 4545: default untrusted-http-port for tee-worker (see config.json)
+for p in 9944 30333 4545; do
+  if [ ! -z "$(netstat -nat | grep $p)" ]; then
+    echo "port $p is in use, quit now"
+    exit 1
+  fi
+done
+
 ROOTDIR=$(git rev-parse --show-toplevel)
 cd "$ROOTDIR"
 make launch-docker-rococo
