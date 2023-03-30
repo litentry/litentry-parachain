3ce9882d87d0e04e23fbc6602cde7cc94fa5b048
Kai <7630809+Kailai-Wang@users.noreply.github.com>
2023-03-31 01:34:34 +0200

    Fix clippy (#1554)
    * Fix clippy
    
    * add comments
    
    * update comment space

--- tee-worker/litentry/core/assertion-build/src/a4.rs
+++ tee-worker/litentry/core/assertion-build/src/a4.rs
@@ -20,6 +20,49 @@ compile_error!("feature \"std\" and feature \"sgx\" cannot be enabled at the sam
 #[cfg(all(not(feature = "std"), feature = "sgx"))]
 extern crate sgx_tstd as std;
 
+/// Here's an example of different assertions in this VC:
+///
+/// Imagine:
+/// ALICE holds 100 LITs since 2018-03-02
+/// BOB   holds 100 LITs since 2023-03-07
+/// CAROL holds 0.1 LITs since 2020-02-22
+///
+/// min_amount is 1 LIT
+///
+/// If they all request A4, these are the received assertions:
+/// ALICE:
+/// [
+///    from_date: < 2019-01-01
+///    to_date: >= 2023-03-30 (now)
+///    value: true
+/// ]
+///
+/// BOB:
+/// [
+///    from_date: < 2017-01-01
+///    to_date: >= 2023-03-30 (now)
+///    value: false
+/// ]
+///
+/// CAROL:
+/// [
+///    from_date: < 2017-01-01
+///    to_date: >= 2023-03-30 (now)
+///    value: false
+/// ]
+///
+/// So just from the assertion results you can't distinguish between:
+/// BOB, who just started to hold recently,
+/// and CAROL, who has been holding for 3 years, but with too little amount
+///
+/// This is because the data provider doesn't provide more information, it only
+/// takes the query with from_date and min_ammount, and returns true or false.
+///
+/// Please note:
+/// the operators are mainly for IDHub's parsing, we will **NEVER** have:
+/// - `from_date` with >= op, nor
+/// - `value` is false but the `from_date` is something other than 2017-01-01.
+///  
 use crate::{Error, Result};
 use itp_stf_primitives::types::ShardIdentifier;
 use itp_types::AccountId;
