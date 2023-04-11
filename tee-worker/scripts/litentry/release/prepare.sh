d6fb9a51f19d1452967f25c65b27b56139e7b5eb
BillyWooo <thedreamofbilly@gmail.com>
2023-04-11 20:17:05 +0000

    adapt scripts with PR 1587

--- tee-worker/scripts/litentry/release/prepare.sh
+++ tee-worker/scripts/litentry/release/prepare.sh
@@ -42,7 +42,7 @@ echo "Enclave account is prepared inside enclave_account.txt"
 ./integritee_service shielding-key
 
 
-for Item in 'enclave.sign.so' 'integritee-service' 'aes_key_sealed.bin' 'ed25519_key_sealed.bin' 'enclave-shielding-pubkey.json' 'enclave-signing-pubkey.bin' 'rsa3072_key_sealed.bin' 'sidechain_db' 'ts-setup'; do
+for Item in 'enclave.sign.so' 'integritee-service' 'aes_key_sealed.bin' 'ed25519_key_sealed.bin' 'enclave-shielding-pubkey.json' 'enclave-signing-pubkey.bin' 'rsa3072_key_sealed.bin' 'sidechain_db'; do
     cp -r "${Item}" "${WORKER_DIR}"
 done
 
@@ -50,7 +50,8 @@ cp $RUNNING_MODE_CONFIG_PATH "${WORKER_DIR}/config.json"
 cp $INTEL_KEY_PATH "${WORKER_DIR}/key_production.txt"
 cp $INTEL_SPID_PATH "${WORKER_DIR}/spid_production.txt"
 
-cp $PRIVATE_ACCOUNT_JSON "${WORKER_DIR}/ts-setup/private_account.json"
-cp "enclave_account.txt" "${WORKER_DIR}/ts-setup/enclave_account.txt"
-cp "mrenclave.txt" "${WORKER_DIR}/ts-setup/mrenclave.txt"
+# Comment out for the moment. Need to adapt together with PR-1587 ts-utils.
+# cp $PRIVATE_ACCOUNT_JSON "${WORKER_DIR}/ts-setup/private_account.json"
+# cp "enclave_account.txt" "${WORKER_DIR}/ts-setup/enclave_account.txt"
+# cp "mrenclave.txt" "${WORKER_DIR}/ts-setup/mrenclave.txt"
 
