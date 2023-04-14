baf44d3227fa71b3166f034302844598d9043129
WMQ <wangminqi@aliyun.com>
2023-04-14 14:00:24 +0800

    debug: benchmark skip-ias-check

--- pallets/identity-management/src/mock.rs
+++ pallets/identity-management/src/mock.rs
@@ -56,7 +56,7 @@ impl EnsureOrigin<SystemOrigin> for EnsureEnclaveSigner {
 	fn try_successful_origin() -> Result<SystemOrigin, ()> {
 		use frame_support::assert_ok;
 		use test_utils::ias::consts::{TEST4_CERT, TEST4_SIGNER_PUB, URL};
-		let signer: SystemAccountId = test_utils::get_signer(TEST4_SIGNER_PUB);
+		let signer: SystemAccountId = frame_benchmarking::account::<SystemAccountId>("successful_origin", 0, 0);
 		assert_ok!(pallet_teerex::Pallet::<Test>::register_enclave(
 			RuntimeOrigin::signed(signer.clone()),
 			TEST4_CERT.to_vec(),
