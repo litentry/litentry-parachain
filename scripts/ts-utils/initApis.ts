66d6ad46a82262fd978e7db41229ace37a6b280a
Verin1005 <daqingchong0809@gmail.com>
2023-04-14 19:17:48 +0800

    add endpoint json

--- scripts/ts-utils/initApis.ts
+++ scripts/ts-utils/initApis.ts
@@ -1,11 +1,13 @@
 const { ApiPromise, WsProvider } = require("@polkadot/api");
 const { cryptoWaitReady } = require('@polkadot/util-crypto');
+const { fetchEndpoint, defaultEndpoint } = require('./endpoint.json');
+
 import colors from "colors";
 //fetchApi is used to fetch data from the chain
-const wsFetchProvider = new WsProvider("wss://tee-staging.litentry.io:443");
+const wsFetchProvider = new WsProvider(fetchEndpoint);
 
 //defaultAPI is used to send transactions to the chain
-const wsDefaultProvider = new WsProvider("wss://tee-staging.litentry.io/");
+const wsDefaultProvider = new WsProvider(defaultEndpoint);
 
 export const initApi = async () => {
     console.log(colors.green("init api..."))
