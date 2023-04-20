559ca20afc839202f00cf674991c9963b4b91bc2
BillyWooo <thedreamofbilly@gmail.com>
2023-04-20 10:15:47 +0000

    add dependency

--- pallets/identity-management/src/tests.rs
+++ pallets/identity-management/src/tests.rs
@@ -19,6 +19,9 @@ use core_primitives::{ErrorDetail, IMPError};
 use frame_support::{assert_noop, assert_ok};
 use sp_core::H256;
 
+#[cfg(feature = "skip-ias-check")]
+use test_utils::ias::consts::TEST8_CERT;
+
 use test_utils::ias::consts::TEST8_MRENCLAVE;
 type SystemAccountId = <Test as frame_system::Config>::AccountId;
 const ALICE_PUBKEY: &[u8; 32] = &[1u8; 32];
