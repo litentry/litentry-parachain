e127647a863e828456edb35b9ea4db6b72dc8233
zhouhuitian <zhouhui@litentry.com>
2023-05-05 15:03:55 +0800

    update holder description

--- tee-worker/litentry/core/assertion-build/src/a10.rs
+++ tee-worker/litentry/core/assertion-build/src/a10.rs
@@ -35,7 +35,7 @@ use std::{string::ToString, vec, vec::Vec};
 const WBTC_TOKEN_ADDRESS: &str = "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599";
 
 const VC_A10_SUBJECT_DESCRIPTION: &str =
-	"Since when has the user been consistently holding a min amount {x} of WBTC token";
+	"The user has been consistently holding at least {x} amount of tokens before 2023 Jan 1st 00:00:00 UTC on the supporting networks";
 const VC_A10_SUBJECT_TYPE: &str = "WBTC Holding Assertion";
 const VC_A10_SUBJECT_TAG: [&str; 1] = ["Ethereum"];
 
