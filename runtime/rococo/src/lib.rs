586077f28ae62ee94f37dc47b0c37d93c88ca100
Kailai Wang <Kailai.Wang@hotmail.com>
2023-05-14 13:10:39 +0000

    remove unneeded

--- runtime/rococo/src/lib.rs
+++ runtime/rococo/src/lib.rs
@@ -16,7 +16,6 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 #![allow(clippy::identity_op)]
-#![allow(clippy::items_after_test_module)]
 // `construct_runtime!` does a lot of recursion and requires us to increase the limit to 256.
 #![recursion_limit = "256"]
 
