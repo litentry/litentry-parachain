0f6b2ffaf41c2deb0aa9d068e681ded661499c5d
zhouhuitian <zhouhui@litentry.com>
2023-05-16 10:11:37 +0800

    align indentation

--- scripts/generate-upstream-patch.sh
+++ scripts/generate-upstream-patch.sh
@@ -12,8 +12,8 @@ function cleanup() {
 
 function usage() {
 	echo "Usage:"
-    echo "For pallets: $0 p pallets-new-commit"
-    echo "For worker : $0 w worker-new-commit"
+	echo "For pallets: $0 p pallets-new-commit"
+	echo "For worker : $0 w worker-new-commit"
 }
 
 # This function generates 9 patches for the diffs between commit-A and commit-B
@@ -25,8 +25,6 @@ function usage() {
 function generate_worker_patch() {
 	local TARGET="worker"
 	local TARGET_DIR="$ROOTDIR/$TARGET"
-    echo "TARGET: $1"
-    echo "TARGET_DIR: $TARGET_DIR"
 
 	if [[ $TARGET == "worker" ]]
 	then
@@ -34,7 +32,6 @@ function generate_worker_patch() {
 	fi
 
 	local UPSTREAM_URL="$UPSTREAM_URL_PREFIX/$TARGET"
-    echo "UPSTREAM_URL: $UPSTREAM_URL"
 
 	cd "$TARGET_DIR"
 
@@ -59,8 +56,8 @@ function generate_worker_patch() {
 	git diff "$OLD_COMMIT" HEAD > "$TARGET_DIR/upstream.patch"
 	git rev-parse --short HEAD > "$TARGET_DIR/upstream_commit"
 	 
-    # Clean up TMP DIR
-	cleanup $tmp_dir
+	# Clean up TMP DIR
+	cleanup "$tmp_dir"
 
 	echo
 }
@@ -72,11 +69,8 @@ function generate_worker_patch() {
 function generate_pallets_patch() {
 	local TARGET='pallets'
 	local TARGET_DIR="$ROOTDIR/$TARGET"
-    echo "TARGET: $TARGET"
-    echo "TARGET_DIR: $TARGET_DIR"
 
 	local UPSTREAM_URL="$UPSTREAM_URL_PREFIX/$TARGET"
-    echo "UPSTREAM_URL: $UPSTREAM_URL"
 
 	cd "$TARGET_DIR"
 
@@ -99,25 +93,26 @@ function generate_pallets_patch() {
 	[ "" != "$NEW_COMMIT" ] && git checkout "$NEW_COMMIT"
 	echo ">>> generating patch ..."
 
+	# pallets
 	local pallets=("parentchain" "sidechain" "teeracle" "teerex" "test-utils")
-    for p in "${pallets[@]}"; do
-        echo "generating $p.patch"
+	for p in "${pallets[@]}"; do
+		echo "generating $p.patch"
 		git diff "$OLD_COMMIT" HEAD -- "$p" > "$TARGET_DIR/pallets_$p.patch"
-    done
+	done
 
 	# primitives
-    local primitives=("sidechain" "teeracle" "teerex" "common")
-    for p in "${primitives[@]}"; do
-        echo "generating primitives_$p.patch"
+	local primitives=("sidechain" "teeracle" "teerex" "common")
+	for p in "${primitives[@]}"; do
+		echo "generating primitives_$p.patch"
 		git diff "$OLD_COMMIT" HEAD -- primitives/"$p" > "$TARGET_DIR/primitives_$p.patch"
-    done
+	done
 
 	echo ">>> generating patch done."
 
 	git rev-parse --short HEAD > "$TARGET_DIR/upstream_commit"
 	 
-    # Clean up TMP DIR
-    cleanup $tmp_dir
+	# Clean up TMP DIR
+	cleanup "$tmp_dir"
 
 	echo
 }
@@ -128,7 +123,7 @@ function apply_pallets_tips() {
 	echo "upstream.patch(s) are generated."
 	echo "To apply it, RUN FROM $ROOTDIR:"
 	echo " # Pallet patches:"
-    echo " git apply -p1 -3 --directory=pallets $ROOTDIR/pallets/pallets_xxx.patch"
+	echo " git apply -p1 -3 --directory=pallets $ROOTDIR/pallets/pallets_xxx.patch"
 	echo " # Primitive patches:"
 	echo " git apply -p1 -3 $ROOTDIR/pallets/primitives_xxx.patch"
 
@@ -145,36 +140,36 @@ function apply_woker_tips() {
 	echo "upstream_commit(s) are updated."
 	echo "upstream.patch(s) are generated."
 	echo "To apply it, RUN FROM $ROOTDIR:"
-    echo "  git am -3 --exclude=tee-worker/Cargo.lock --exclude=tee-worker/enclave-runtime/Cargo.lock --directory=tee-worker < tee-worker/upstream.patch"
+	echo "  git am -3 --exclude=tee-worker/Cargo.lock --exclude=tee-worker/enclave-runtime/Cargo.lock --directory=tee-worker < tee-worker/upstream.patch"
 
 	echo ""
 	echo "after that, please:"
 	echo "- pay special attention: "
-    echo "  * ALL changes/conflicts from tee-worker/upstream.patch patch should ONLY apply into:"
-    echo "    - tee-worker"
+	echo "  * ALL changes/conflicts from tee-worker/upstream.patch patch should ONLY apply into:"
+	echo "    - tee-worker"
 
-    echo "- resolve any conflicts"
+	echo "- resolve any conflicts"
 	echo "- optionally update Cargo.lock file"
-    echo "- apply the changes to $ROOTDIR/.github/workflows/tee-worker-ci.yml"
+	echo "- apply the changes to $ROOTDIR/.github/workflows/tee-worker-ci.yml"
 }
 
 if [ -z "$2" ]; then
-    usage; exit 1
+	usage; exit 1
 fi
 NEW_COMMIT=$2
 
 OPT="$1"
 case "$OPT" in
-    p)
-        generate_pallets_patch "$@"
+	p)
+		generate_pallets_patch "$@"
 		apply_pallets_tips
 		;;
-    w)
-        generate_worker_patch "$@"
+	w)
+		generate_worker_patch "$@"
 		apply_woker_tips
 		;;
-    *)
-        usage; exit 1 ;;
+	*)
+		usage; exit 1 ;;
 esac
 
 
