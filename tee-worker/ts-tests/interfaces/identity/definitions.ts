61f04fb46aa67443a00f18a273b68bc6db2a5297
Kailai Wang <Kailai.Wang@hotmail.com>
2023-05-19 20:18:21 +0000

    getter type

--- tee-worker/ts-tests/interfaces/identity/definitions.ts
+++ tee-worker/ts-tests/interfaces/identity/definitions.ts
@@ -38,6 +38,12 @@ export default {
         TrustedGetter: {
             _enum: {
                 free_balance: '(AccountId)',
+                reserved_balance: '(AccountId)',
+                nonce: '(AccountId)',
+                user_shielding_key: '(AccountId)',
+                id_graph: '(AccountId)',
+                challenge_code: '(AccountId, LitentryIdentity)',
+                id_graph_stats: '(AccountId)',
             },
         },
         //important
