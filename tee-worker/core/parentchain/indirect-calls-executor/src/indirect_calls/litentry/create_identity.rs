43bb95e0123af0dccb38712b2230deccfd5c11c1
Faisal Ahmed <faisalahmedfarooq46@gmail.com>
2023-05-23 00:20:33 +0530

    fix: comment out function body for create_identity

--- tee-worker/core/parentchain/indirect-calls-executor/src/indirect_calls/litentry/create_identity.rs
+++ tee-worker/core/parentchain/indirect-calls-executor/src/indirect_calls/litentry/create_identity.rs
@@ -56,40 +56,39 @@ impl<Executor: IndirectExecutor> IndirectDispatch<Executor> for CreateIdentityAr
 			bs58::encode(self.shard.encode()).into_string(),
 			self.account
 		);
-
-		let shielding_key = executor.shielding_key_repo.retrieve_key()?;
-		let identity: Identity =
-			Identity::decode(&mut shielding_key.decrypt(self.encrypted_identity)?.as_slice())?;
-		debug!(
-			"execute indirect call: CreateIdentity, who: {:?}, identity: {:?}",
-			account_id_to_string(self.account),
-			identity
-		);
-
-		let metadata = match self.encrypted_metadata {
-			None => None,
-			Some(m) => {
-				let decrypted_metadata = shielding_key.decrypt(m)?;
-				Some(MetadataOf::<Runtime>::decode(&mut decrypted_metadata.as_slice())?)
-			},
-		};
-
-		if extrinsic.signature.is_some() {
-			let enclave_account_id = executor.stf_enclave_signer.get_enclave_account()?;
-			let trusted_call = TrustedCall::create_identity_runtime(
-				enclave_account_id,
-				account.clone(),
-				identity,
-				metadata,
-				self.block_number,
-				hash_of(extrinsic),
-			);
-			let signed_trusted_call =
-				executor.stf_enclave_signer.sign_call_with_self(&trusted_call, shard)?;
-			let trusted_operation = TrustedOperation::indirect_call(signed_trusted_call);
-			let encrypted_trusted_call = shielding_key.encrypt(&trusted_operation.encode())?;
-			executor.submit_trusted_call(*shard, encrypted_trusted_call);
-		}
+		// let shielding_key = executor.shielding_key_repo.retrieve_key()?;
+		// let identity: Identity =
+		// 	Identity::decode(&mut shielding_key.decrypt(self.encrypted_identity)?.as_slice())?;
+		// debug!(
+		// 	"execute indirect call: CreateIdentity, who: {:?}, identity: {:?}",
+		// 	account_id_to_string(self.account),
+		// 	identity
+		// );
+		//
+		// let metadata = match self.encrypted_metadata {
+		// 	None => None,
+		// 	Some(m) => {
+		// 		let decrypted_metadata = shielding_key.decrypt(m)?;
+		// 		Some(MetadataOf::<Runtime>::decode(&mut decrypted_metadata.as_slice())?)
+		// 	},
+		// };
+		//
+		// if extrinsic.signature.is_some() {
+		// 	let enclave_account_id = executor.stf_enclave_signer.get_enclave_account()?;
+		// 	let trusted_call = TrustedCall::create_identity_runtime(
+		// 		enclave_account_id,
+		// 		account.clone(),
+		// 		identity,
+		// 		metadata,
+		// 		self.block_number,
+		// 		hash_of(extrinsic),
+		// 	);
+		// 	let signed_trusted_call =
+		// 		executor.stf_enclave_signer.sign_call_with_self(&trusted_call, shard)?;
+		// 	let trusted_operation = TrustedOperation::indirect_call(signed_trusted_call);
+		// 	let encrypted_trusted_call = shielding_key.encrypt(&trusted_operation.encode())?;
+		// 	executor.submit_trusted_call(*shard, encrypted_trusted_call);
+		// }
 		Ok(())
 	}
 }
