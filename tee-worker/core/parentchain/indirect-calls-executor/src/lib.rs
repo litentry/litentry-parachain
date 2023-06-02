c1faec0cc0837df8a1bfbba743b8f001fea17220
songzhouran <zhouran@litentry.com>
2023-06-02 14:10:46 +0200

    fix compact

--- tee-worker/core/parentchain/indirect-calls-executor/src/lib.rs
+++ tee-worker/core/parentchain/indirect-calls-executor/src/lib.rs
@@ -534,7 +534,7 @@ mod test {
 
 		let shield_funds_indexes = dummy_metadata.shield_funds_call_indexes().unwrap();
 		ParentchainUncheckedExtrinsic::<ShieldFundsFn>::new_signed(
-			(shield_funds_indexes, target_account, 1000u128, shard_id()),
+			(shield_funds_indexes, target_account, codec::Compact(1000u128), shard_id()),
 			GenericAddress::Address32([1u8; 32]),
 			MultiSignature::Ed25519(default_signature()),
 			default_extrinsic_params().signed_extra(),
