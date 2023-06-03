ccfef98e232f4203940a47e9bb4cdcbe56487d4f
songzhouran <zhouran@litentry.com>
2023-06-03 07:28:39 +0200

    add compact

--- tee-worker/core/parentchain/indirect-calls-executor/src/lib.rs
+++ tee-worker/core/parentchain/indirect-calls-executor/src/lib.rs
@@ -45,6 +45,209 @@ pub mod filter_calls;
 pub mod indirect_calls;
 pub mod parentchain_extrinsic_parser;
 
+<<<<<<< HEAD
 pub use error::{Error, Result};
 pub use executor::IndirectCallsExecutor;
 pub use traits::{ExecuteIndirectCalls, IndirectDispatch, IndirectExecutor};
+=======
+		let (indirect_calls_executor, top_pool_author, _) =
+			test_fixtures([0u8; 32], NodeMetadataMock::new());
+
+		let opaque_extrinsic =
+			OpaqueExtrinsic::from_bytes(call_worker_unchecked_extrinsic().encode().as_slice())
+				.unwrap();
+
+		let parentchain_block = ParentchainBlockBuilder::default()
+			.with_extrinsics(vec![
+				fill_opaque_extrinsic_with_status(opaque_extrinsic, true).unwrap()
+			])
+			.build();
+
+		indirect_calls_executor
+			.execute_indirect_calls_in_extrinsics(&parentchain_block)
+			.unwrap();
+
+		assert_eq!(1, top_pool_author.pending_tops(shard_id()).unwrap().len());
+	}
+
+	#[test]
+	fn failed_indirect_call_is_skipped() {
+		let _ = env_logger::builder().is_test(true).try_init();
+
+		let (indirect_calls_executor, top_pool_author, _) =
+			test_fixtures([0u8; 32], NodeMetadataMock::new());
+
+		let opaque_extrinsic =
+			OpaqueExtrinsic::from_bytes(call_worker_unchecked_extrinsic().encode().as_slice())
+				.unwrap();
+
+		let parentchain_block = ParentchainBlockBuilder::default()
+			.with_extrinsics(vec![
+				fill_opaque_extrinsic_with_status(opaque_extrinsic, false).unwrap()
+			])
+			.build();
+
+		indirect_calls_executor
+			.execute_indirect_calls_in_extrinsics(&parentchain_block)
+			.unwrap();
+
+		assert_eq!(0, top_pool_author.pending_tops(shard_id()).unwrap().len());
+	}
+
+	#[test]
+	fn shielding_call_can_be_added_to_pool_successfully() {
+		let _ = env_logger::builder().is_test(true).try_init();
+
+		let mr_enclave = [33u8; 32];
+		let (indirect_calls_executor, top_pool_author, shielding_key_repo) =
+			test_fixtures(mr_enclave.clone(), NodeMetadataMock::new());
+		let shielding_key = shielding_key_repo.retrieve_key().unwrap();
+
+		let opaque_extrinsic = OpaqueExtrinsic::from_bytes(
+			shield_funds_unchecked_extrinsic(&shielding_key).encode().as_slice(),
+		)
+		.unwrap();
+
+		let parentchain_block = ParentchainBlockBuilder::default()
+			.with_extrinsics(vec![
+				fill_opaque_extrinsic_with_status(opaque_extrinsic, true).unwrap()
+			])
+			.build();
+
+		indirect_calls_executor
+			.execute_indirect_calls_in_extrinsics(&parentchain_block)
+			.unwrap();
+
+		assert_eq!(1, top_pool_author.pending_tops(shard_id()).unwrap().len());
+		let submitted_extrinsic =
+			top_pool_author.pending_tops(shard_id()).unwrap().first().cloned().unwrap();
+		let decrypted_extrinsic = shielding_key.decrypt(&submitted_extrinsic).unwrap();
+		let decoded_operation =
+			TrustedOperation::decode(&mut decrypted_extrinsic.as_slice()).unwrap();
+		assert_matches!(decoded_operation, TrustedOperation::indirect_call(_));
+		let trusted_call_signed = decoded_operation.to_call().unwrap();
+		assert!(trusted_call_signed.verify_signature(&mr_enclave, &shard_id()));
+	}
+
+	#[test]
+	fn ensure_empty_extrinsic_vec_triggers_zero_filled_merkle_root() {
+		// given
+		let dummy_metadata = NodeMetadataMock::new();
+		let (indirect_calls_executor, _, _) = test_fixtures([38u8; 32], dummy_metadata.clone());
+
+		let block_hash = H256::from([1; 32]);
+		let extrinsics = Vec::new();
+		let confirm_processed_parentchain_block_indexes =
+			dummy_metadata.confirm_processed_parentchain_block_call_indexes().unwrap();
+		let expected_call =
+			(confirm_processed_parentchain_block_indexes, block_hash, 1, H256::default()).encode();
+
+		// when
+		let call = indirect_calls_executor
+			.create_processed_parentchain_block_call::<Block>(
+				block_hash,
+				extrinsics,
+				codec::Compact(1),
+			)
+			.unwrap();
+
+		// then
+		assert_eq!(call.0, expected_call);
+	}
+
+	#[test]
+	fn ensure_non_empty_extrinsic_vec_triggers_non_zero_merkle_root() {
+		// given
+		let dummy_metadata = NodeMetadataMock::new();
+		let (indirect_calls_executor, _, _) = test_fixtures([39u8; 32], dummy_metadata.clone());
+
+		let block_hash = H256::from([1; 32]);
+		let extrinsics = vec![H256::from([4; 32]), H256::from([9; 32])];
+		let confirm_processed_parentchain_block_indexes =
+			dummy_metadata.confirm_processed_parentchain_block_call_indexes().unwrap();
+
+		let zero_root_call =
+			(confirm_processed_parentchain_block_indexes, block_hash, 1, H256::default()).encode();
+
+		// when
+		let call = indirect_calls_executor
+			.create_processed_parentchain_block_call::<Block>(
+				block_hash,
+				extrinsics,
+				codec::Compact(1),
+			)
+			.unwrap();
+
+		// then
+		assert_ne!(call.0, zero_root_call);
+	}
+
+	fn shield_funds_unchecked_extrinsic(
+		shielding_key: &ShieldingCryptoMock,
+	) -> ParentchainUncheckedExtrinsic<ShieldFundsFn> {
+		let target_account = shielding_key.encrypt(&AccountId::new([2u8; 32]).encode()).unwrap();
+		let dummy_metadata = NodeMetadataMock::new();
+
+		let shield_funds_indexes = dummy_metadata.shield_funds_call_indexes().unwrap();
+		ParentchainUncheckedExtrinsic::<ShieldFundsFn>::new_signed(
+			(shield_funds_indexes, target_account, codec::Compact(1000u128), shard_id()),
+			GenericAddress::Address32([1u8; 32]),
+			MultiSignature::Ed25519(default_signature()),
+			default_extrinsic_params().signed_extra(),
+		)
+	}
+
+	fn call_worker_unchecked_extrinsic() -> ParentchainUncheckedExtrinsic<CallWorkerFn> {
+		let request = Request { shard: shard_id(), cyphertext: vec![1u8, 2u8] };
+		let dummy_metadata = NodeMetadataMock::new();
+		let call_worker_indexes = dummy_metadata.call_worker_call_indexes().unwrap();
+
+		ParentchainUncheckedExtrinsic::<CallWorkerFn>::new_signed(
+			(call_worker_indexes, request),
+			GenericAddress::Address32([1u8; 32]),
+			MultiSignature::Ed25519(default_signature()),
+			default_extrinsic_params().signed_extra(),
+		)
+	}
+
+	fn default_signature() -> ed25519::Signature {
+		signer().sign(&[0u8])
+	}
+
+	fn signer() -> ed25519::Pair {
+		ed25519::Pair::from_seed(&TEST_SEED)
+	}
+
+	fn shard_id() -> ShardIdentifier {
+		ShardIdentifier::default()
+	}
+
+	fn default_extrinsic_params() -> ParentchainExtrinsicParams {
+		ParentchainExtrinsicParams::new(
+			0,
+			0,
+			0,
+			H256::default(),
+			ParentchainExtrinsicParamsBuilder::default(),
+		)
+	}
+	fn test_fixtures(
+		mr_enclave: [u8; 32],
+		metadata: NodeMetadataMock,
+	) -> (TestIndirectCallExecutor, Arc<TestTopPoolAuthor>, Arc<TestShieldingKeyRepo>) {
+		let shielding_key_repo = Arc::new(TestShieldingKeyRepo::default());
+		let stf_enclave_signer = Arc::new(TestStfEnclaveSigner::new(mr_enclave));
+		let top_pool_author = Arc::new(TestTopPoolAuthor::default());
+		let node_metadata_repo = Arc::new(NodeMetadataRepository::new(metadata));
+
+		let executor = IndirectCallsExecutor::new(
+			shielding_key_repo.clone(),
+			stf_enclave_signer,
+			top_pool_author.clone(),
+			node_metadata_repo,
+		);
+
+		(executor, top_pool_author, shielding_key_repo)
+	}
+}
+>>>>>>> add compact
