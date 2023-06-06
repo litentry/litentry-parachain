79c86abd8f98e28bfa01e6d1f7be2ed3d9f42b85
felixfaisal <faisalahmedfarooq46@gmail.com>
2023-06-06 04:54:31 +0000

    fix: bash script to run setup_enclave script

---
+++ scripts/ts-utils/setup_enclave.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Also need to set the port
+
+
+yarn install
+yarn tsc
+yarn node setup-enclave.js  5HTABcy5JjPuvG2mnVPfESTJBFQAUwV5UuA1A9DX85Q8mbdC effe659463f2286b0cf3bf62cc719e48dc4d1e6c6aa74be17537b955f6b76cb6 0
