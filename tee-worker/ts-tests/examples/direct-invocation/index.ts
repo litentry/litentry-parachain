fdaf296a9ff9131ce187ef7f4395ccd249189ae6
Kasper Ziemianek <kasper.ziemianek@gmail.com>
2023-06-07 16:21:32 +0200

    fix di examples

--- tee-worker/ts-tests/examples/direct-invocation/index.ts
+++ tee-worker/ts-tests/examples/direct-invocation/index.ts
@@ -1,7 +1,7 @@
 import { cryptoWaitReady } from '@polkadot/util-crypto';
 import { KeyringPair } from '@polkadot/keyring/types';
 import { ApiPromise, Keyring, WsProvider } from '@polkadot/api';
-import { teeTypes } from '../../common/type-definitions';
+import { default as teeTypes } from '../../parachain-interfaces/identity/definitions';
 import { HexString } from '@polkadot/util/types';
 import {
     createSignedTrustedCallSetUserShieldingKey,
@@ -14,7 +14,7 @@ import {
     sendRequestFromPublicGetter,
     decodeNonce,
 } from './util';
-import { getEnclave, sleep, buildIdentityHelper } from '../../common/utils';
+import { getEnclave, sleep, buildIdentityHelper, initIntegrationTestContext } from '../../common/utils';
 import { Metadata, TypeRegistry } from '@polkadot/types';
 import sidechainMetaData from '../../litentry-sidechain-metadata.json';
 import { hexToU8a, compactStripLength, u8aToString } from '@polkadot/util';
@@ -35,12 +35,22 @@ async function runDirectCall() {
     const parachain_ws = new WsProvider(PARACHAIN_WS_ENDPINT);
     const sidechainRegistry = new TypeRegistry();
     const metaData = new Metadata(sidechainRegistry, sidechainMetaData.result as HexString);
-    sidechainRegistry.setMetadata(metaData);
+    const registry = new TypeRegistry();
+
     const { types } = teeTypes;
+
+    sidechainRegistry.setMetadata(metaData);
     const parachain_api = await ApiPromise.create({
         provider: parachain_ws,
         types,
     });
+
+    const context = await initIntegrationTestContext(
+        WORKER_TRUSTED_WS_ENDPOINT, // @fixme evil assertion; centralize env access
+        PARACHAIN_WS_ENDPINT, // @fixme evil assertion; centralize env access
+        0
+    );
+
     await cryptoWaitReady();
     const wsp = new WebSocketAsPromised(WORKER_TRUSTED_WS_ENDPOINT, {
         createWebSocket: (url: any) => new WebSocket(url),
@@ -90,7 +100,7 @@ async function runDirectCall() {
 
     nonce = parachain_api.createType('Index', NonceValue1);
     console.log('Send direct createIdentity call... hash:', hash);
-    const twitter_identity = await buildIdentityHelper('mock_user', 'Twitter', 'Web2');
+    const twitter_identity = await buildIdentityHelper('mock_user', 'Twitter', 'Web2', context);
     let createIdentityCall = createSignedTrustedCallCreateIdentity(
         parachain_api,
         mrenclave,
