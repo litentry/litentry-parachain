f54b91305699cdbcc95b4d37b2838dbec18c79bb
Ariel Birnbaum <ariel@litentry.com>
2023-06-16 16:53:06 +0200

    operationalize a little

--- tee-worker/ts-tests-with-workspaces/parachain-api/lib/src/index.d.ts
+++ tee-worker/ts-tests-with-workspaces/parachain-api/lib/src/index.d.ts
@@ -2,11 +2,6 @@ import "@polkadot/api/augment";
 import "@polkadot/types/augment";
 import { ApiOptions } from "@polkadot/api/types";
 import { ApiPromise } from "@polkadot/api";
-
-import { identity } from "parachain-api/interfaces/definitions";
-
 type ProviderInterface = Exclude<ApiOptions["provider"], undefined>;
-
-export async function create(provider: ProviderInterface): Promise<ApiPromise> {
-    return await ApiPromise.create({ provider, types: identity.types });
-}
+export declare function create(provider: ProviderInterface): Promise<ApiPromise>;
+export {};
---
+++ tee-worker/ts-tests-with-workspaces/parachain-api/lib/src/index.js
@@ -0,0 +1,7 @@
+import "@polkadot/api/augment";
+import "@polkadot/types/augment";
+import { ApiPromise } from "@polkadot/api";
+import { identity } from "parachain-api/interfaces/definitions";
+export async function create(provider) {
+	return await ApiPromise.create({ provider, types: identity.types });
+}
