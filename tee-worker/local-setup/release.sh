93df0fec07f8dd2a15ce0173d2af904cabc5d664
felixfaisal <faisalahmedfarooq46@gmail.com>
2023-06-26 02:05:09 +0000

    fix: upgrading worker to exit first

--- tee-worker/local-setup/release.sh
+++ tee-worker/local-setup/release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export LOG="/var/log"
-export TARGET_DIRECTORY="/usr/local/bin"
+export LOG="log/worker0.log"
+export TARGET_DIRECTORY="tmp/w0"
 
 # TODO: Set Pipe fail if any of the commands fail
 generate_service_file() {
@@ -32,13 +32,38 @@ WantedBy=multi-user.target
   echo "Service file \"${service_filename}\" generated successfully."
 }
 
+generate_service_file_for_worker() {
+ local service_name="$1"
+ local description="$2"
+ local command="$3"
+ local working_directory="$4"
+
+ local service_template="[Unit]
+ Description=${description}
+ After=network.target
+
+ [Service]
+ ExecStart=${command}
+ Restart=always
+ Environment='RUST_LOG=info,integritee_service=debug,ws=warn,sp_io=error,substrate_api_client=warn,itc_parentchain_light_client=info,jsonrpsee_ws_client=warn,jsonrpsee_ws_server=warn,enclave_runtime=debug,ita_stf=debug,its_rpc_handler=warn,itc_rpc_client=warn,its_consensus_common=debug,its_state=warn,its_consensus_aura=warn,aura*=warn,its_consensus_slots=warn,itp_attestation_handler=debug,http_req=debug,lc_mock_server=warn,itc_rest_client=debug,lc_credentials=debug,lc_identity_verification=debug,lc_stf_task_receiver=debug,lc_stf_task_sender=debug,lc_data_providers=debug,itp_top_pool=debug,itc_parentchain_indirect_calls_executor=debug'
+ StandardOutput=file:${WORKER_DIR}/${LOG}
+ StandardError=inherit
+
+ [Install]
+ WantedBy=default.target
+ "
+   local service_filename="${service_name}.service"
+   echo "$service_template" > "$service_filename"
+   echo "Service file \"${service_filename}\" generated successfully."
+}
+
 generate_worker_service_file() {
-  local command='./integritee-service --clean-reset -P 2000 -w 2001 -r 3443 -h 4545 --running-mode mock --enable-mock-server --parentchain-start-block 0 run --skip-ra --dev'
+  local command="/bin/bash -c  'cd ${WORKER_DIR}/tmp/w0 && ./integritee-service --clean-reset -P 2000 -w 2001 -r 3443 -h 4545 --running-mode mock --enable-mock-server --parentchain-start-block 0 run --skip-ra --dev'"
   local service_name='worker'
   local description='Worker Service for Litentry Side chain'
   local working_directory='/usr/local/bin'
 
-  generate_service_file "${service_name}" "${description}" "${command}" "${working_directory}"
+  generate_service_file_for_worker "${service_name}" "${description}" "${command}" "${working_directory}"
 }
 
 generate_upgrade_worker_service_file() {
@@ -47,11 +72,11 @@ generate_upgrade_worker_service_file() {
   local description='Worker Service for Litentry Side chain'
   local working_directory='/opt/worker/'
 
-  generate_service_file "${service_name}" "${description}" "${command}" "${working_directory}"
+  generate_service_file_for_worker "${service_name}" "${description}" "${command}" "${working_directory}"
 }
 
 generate_parachain_service_file() {
-  local command='/bin/bash -c "cd /home/faisal/litentry-parachain && scripts/launch-local-binary.sh rococo"'
+  local command="/bin/bash -c 'cd ${PARACHAIN_SOURCE} && scripts/launch-local-binary.sh rococo'"
   local service_name='litentry-parachain'
   local description='Parachain Setup for Litentry'
   local working_directory=$PARACHAIN_SOURCE
@@ -113,22 +138,22 @@ latest_sync_block(){
 }
 
 start_service_files(){
-  echo "Moving worker binaries to /usr/local/bin"
-  setup_working_dir "bin" "/usr/local/bin"
-  echo "Moving service files to /etc/systemd/system"
-  cp -r *.service /etc/systemd/system/
+  # echo "Moving worker binaries to /usr/local/bin"
+  # setup_working_dir "bin" "/usr/local/bin"
+  echo "Moving service files to ~/.config/systemd/user"
+  cp -r *.service ~/.config/systemd/user/
 
   echo "Performing Daemon Reload"
-  systemctl daemon-reload
+  systemctl --user daemon-reload
 
   echo "Starting Parachain Service"
-  systemctl start litentry-parachain.service
+  # systemctl --user start litentry-parachain.service
 
   echo "Sleep for 60s, Parachain can be started"
   sleep 60
   # TODO: Check for block finalization instead of 60s
   echo "Starting Working Service"
-  systemctl start worker.service
+  systemctl --user start worker.service
   # TODO: Check for block production via Logs
 
   echo "Parachain and Worker Service have started succesfully, You can check logs at /data/logs"
@@ -161,8 +186,8 @@ upgrade_worker(){
   if [ "$PRODUCTION" = "1" ]; then
       generate_upgrade_worker_service_file
       cp worker.service /etc/systemd/system/
-      systemctl daemon-reload
-      systemctl start worker.service
+      systemctl --user daemon-reload
+      systemctl --user start worker.service
   else
       export RUST_LOG='info,integritee_service=debug,ws=warn,sp_io=error,substrate_api_client=warn,
       itc_parentchain_light_client=info,
@@ -214,10 +239,18 @@ setup_working_dir() {
     done
 }
 
+if [ "$UPGRADE_WORKER" = "1" ]; then
+  # Assuming the worker has already stopped
+  echo "Preparing to Upgrade Worker"
+  upgrade_worker
+  exit 0
+fi
+
 # Example usage
 # generate_service_file "my_service" "My Service Description" "/path/to/my_script.sh" "/path/to/working_directory"
 if [ "$PRODUCTION" = "1" ]; then
     echo "Running in production mode."
+    setup_working_dir "bin" "tmp/w0"
     generate_worker_service_file
     generate_parachain_service_file
 else
@@ -232,12 +265,6 @@ if [ "$AUTO_START" = "true" ]; then
   else
     ./local-setup/launch.sh
   fi
-fi
-
-if [ "$UPGRADE_WORKER" = "true" ]; then
-  # Assuming the worker has already stopped
-  echo "Preparing to Upgrade Worker"
-  upgrade_worker
 fi
 
 # TODO: Need to also perform clean reset of the service
