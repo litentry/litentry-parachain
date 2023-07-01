2e9cf226dba020cb84fc55d1849fd43ccb122fd8
songzhouran <zhouran@litentry.com>
2023-07-01 10:58:35 +0200

    fix moc

--- tee-worker/cli/src/trusted_base_cli/commands/transfer.rs
+++ tee-worker/cli/src/trusted_base_cli/commands/transfer.rs
@@ -16,20 +16,15 @@
 */
 
 use crate::{
-	command_utils::get_worker_api_direct,
+	get_layer_two_nonce,
 	trusted_cli::TrustedCli,
 	trusted_command_utils::{get_accountid_from_str, get_identifiers, get_pair_from_str},
 	trusted_operation::perform_trusted_operation,
 	Cli,
 };
-use base58::ToBase58;
-use codec::{Decode, Encode};
-use ita_stf::{TrustedCall, TrustedOperation};
-use itc_rpc_client::direct_client::DirectApi;
-use itp_rpc::{RpcResponse, RpcReturnValue};
+use codec::Decode;
+use ita_stf::{Index, TrustedCall, TrustedOperation};
 use itp_stf_primitives::types::KeyPair;
-use itp_types::DirectRequestStatus;
-use itp_utils::{FromHexPrefixed, ToHexPrefixed};
 use litentry_primitives::ParentchainBalance as Balance;
 use log::*;
 use sp_core::{crypto::Ss58Codec, Pair};
@@ -51,28 +46,11 @@ impl TransferCommand {
 	pub(crate) fn run(&self, cli: &Cli, trusted_args: &TrustedCli) {
 		let from = get_pair_from_str(trusted_args, &self.from);
 		let to = get_accountid_from_str(&self.to);
-		info!("account_hex {}", to.to_hex());
 		info!("from ss58 is {}", from.public().to_ss58check());
 		info!("to ss58 is {}", to.to_ss58check());
 
 		let (mrenclave, shard) = get_identifiers(trusted_args);
-		let toclone = to.clone();
-		info!("shard base58 {} account_hex {}", shard.encode().to_base58(), toclone.to_hex());
-		let worker_api_direct = get_worker_api_direct(cli);
-		let nonce_ret = worker_api_direct.get_next_nonce(shard, toclone);
-		info!("nonce_ret {:?} ", nonce_ret);
-		let nonce_val = nonce_ret.unwrap();
-		info!("nonce_val {:?} ", nonce_val);
-		let rpc_response: RpcResponse = serde_json::from_str(&nonce_val).unwrap();
-		let rpc_return_value = RpcReturnValue::from_hex(&rpc_response.result).unwrap();
-		if rpc_return_value.status == DirectRequestStatus::Error {
-			println!("[Error] {}", String::decode(&mut rpc_return_value.value.as_slice()).unwrap());
-			worker_api_direct.close().unwrap();
-			return
-		}
-
-		worker_api_direct.close().unwrap();
-		let nonce: u32 = Decode::decode(&mut rpc_return_value.value.as_slice()).unwrap_or_default();
+		let nonce = get_layer_two_nonce!(from, cli, trusted_args);
 		println!(
 			"send trusted call transfer from {} to {}: {}, nonce: {}",
 			from.public(),
