bfed0a692043fd7e90f9f0080547d7fb13c92d0b
felixfaisal <faisalahmedfarooq46@gmail.com>
2023-07-03 04:31:54 +0000

    fix: move setup_enclave.sh to tee-worker

--- tee-worker/scripts/litentry/setup_enclave.sh
+++ tee-worker/scripts/litentry/setup_enclave.sh
@@ -2,6 +2,6 @@
 
 # Also need to set the port
 
-cd ../scripts/ts-utils/
+cd $ROOTDIR/scripts/ts-utils/
 yarn install
 npx ts-node setup-enclave.ts  $ENCLAVE_ACCOUNT $NEW_MRENCLAVE $SCHEDULE_UPDATE_BLOCK
