d27df31447a1e2707cc2dff6e99e7bb06ebc9a3b
Kasper Ziemianek <kasper.ziemianek@gmail.com>
2023-07-06 14:59:00 +0200

    add trusted cli nonce command test

--- tee-worker/cli/lit_nonce.sh
+++ tee-worker/cli/lit_nonce.sh
@@ -43,7 +43,6 @@ echo "Using trusted-worker uri $WORKER1URL:$WORKER1PORT"
 echo ""
 
 ACC=//Bob
-KEY="22fc82db5b606998ad45099b7978b5b4f9dd4ea6017e57370ac56141caaabd12"
 
 CLIENT="$CLIENT_BIN -p $NPORT -P $WORKER1PORT -u $NODEURL -U $WORKER1URL"
 echo "CLIENT is $CLIENT"
@@ -65,46 +64,12 @@ else
 fi
 [[ -z $MRENCLAVE ]] && { echo "MRENCLAVE is empty. cannot continue" ; exit 1; }
 
-ALICE=//Alice
-ALICE_KEY="8378193a4ce64180814bd60591d1054a04dbc4da02afde453799cd6888ee0c6c"
+echo "* Get $ACC 's nonce"
+nonce=$(${CLIENT} trusted --mrenclave $MRENCLAVE nonce $ACC)
 
-ALICE_IDENTITY='{"Substrate": {"network": "Polkadot", "address": [58, 215, 18, 154, 11, 223, 105, 185, 64, 123, 200, 233, 215, 156, 158, 88, 152, 249, 7, 217, 84, 219, 188, 4, 18, 50, 246, 243, 96, 91, 215, 11]}}'
-
-BOB=//Bob
-BOB_KEY="8378193a4ce64180814bd60591d1054a04dbc4da02afde453799cd6888ee0c6d"
-
-BOB_IDENTITY='{"Substrate": {"network": "Polkadot", "address": [58, 215, 18, 154, 11, 223, 105, 185, 64, 123, 200, 233, 215, 156, 158, 88, 152, 249, 7, 217, 84, 219, 188, 4, 18, 50, 246, 243, 96, 91, 215, 11]}}'
-
-sleep 10
-echo "* Set $ALICE 's shielding key to $ALICE_KEY"
-${CLIENT} set-user-shielding-key "$ALICE" "$ALICE_KEY" ${MRENCLAVE}
-echo ""
-
-sleep 10
-echo "* Create $ALICE 's Identity"
-${CLIENT} create-identity "$ALICE" "$ALICE_IDENTITY" ${MRENCLAVE}
-
-sleep 10
-echo "* Set $BOB 's shielding key to $BOB_KEY"
-${CLIENT} set-user-shielding-key "$BOB" "$BOB_KEY" ${MRENCLAVE}
-echo ""
-
-sleep 10
-echo "* Create $BOB 's Identity"
-${CLIENT} create-identity "$BOB" "$BOB_IDENTITY" ${MRENCLAVE}
-
-sleep 20
-echo "* Get IDGraph stats"
-IDGRAPH_STATS=$($CLIENT trusted --mrenclave $MRENCLAVE --direct id-graph-stats $ALICE)
-echo "${IDGRAPH_STATS}"
-echo ""
-
-read TOTALNUMBER <<< $(echo "$IDGRAPH_STATS" | awk '/Total number: / { print $3; exit }')
-echo "TOTALNUMBER: ${TOTALNUMBER}"
-
-if [ "$TOTALNUMBER" = "4" ]; then
-    echo "test id_graph_stats passed"
+if [ "$nonce" = "0" ]; then
+    echo "test nonce call passed"
 else
-    echo "KEY non-identical: expected: "4" actual: $TOTALNUMBER"
+    echo "nonce non-identical: expected: "0" actual: $nonce"
     exit 1
 fi
