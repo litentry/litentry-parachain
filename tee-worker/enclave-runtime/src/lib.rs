5839dfa39a451bbca22a1b400e45e83ee01d0f84
Kasper Ziemianek <kasper.ziemianek@gmail.com>
2023-07-21 01:46:51 +0200

    warn clippy::string_slice

--- tee-worker/enclave-runtime/src/lib.rs
+++ tee-worker/enclave-runtime/src/lib.rs
@@ -24,7 +24,7 @@
 #![cfg_attr(not(target_env = "sgx"), no_std)]
 #![cfg_attr(target_env = "sgx", feature(rustc_private))]
 #![allow(clippy::missing_safety_doc)]
-#![warn(clippy::unwrap_used, clippy::unreachable, clippy::unimplemented)]
+#![warn(clippy::unwrap_used, clippy::unreachable, clippy::unimplemented, clippy::string_slice)]
 
 #[cfg(not(target_env = "sgx"))]
 #[macro_use]
