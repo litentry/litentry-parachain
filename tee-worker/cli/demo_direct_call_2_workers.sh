6c34cebf879b219e7bfb13d2f2d179d217484439
Faisal Ahmed <faisalahmedfarooq46@gmail.com>
2023-07-25 00:14:46 +0530

    fix: failing two worker tests

--- tee-worker/cli/demo_direct_call_2_workers.sh
+++ tee-worker/cli/demo_direct_call_2_workers.sh
@@ -15,17 +15,11 @@ while getopts ":p:A:B:u:W:V:C:" opt; do
         A)
             WORKER1PORT=$OPTARG
             ;;
-        B)
-            WORKER2PORT=$OPTARG
-            ;;
         u)
             NODEURL=$OPTARG
             ;;
         V)
             WORKER1URL=$OPTARG
-            ;;
-        W)
-            WORKER2URL=$OPTARG
             ;;
         C)
             CLIENT_BIN=$OPTARG
@@ -40,14 +34,10 @@ NODEURL=${NODEURL:-"ws://127.0.0.1"}
 WORKER1PORT=${WORKER1PORT:-2000}
 WORKER1URL=${WORKER1URL:-"wss://127.0.0.1"}
 
-WORKER2PORT=${WORKER2PORT:-3000}
-WORKER2URL=${WORKER2URL:-"wss://127.0.0.1"}
-
 CLIENT_BIN=${CLIENT_BIN:-"./../bin/integritee-cli"}
 
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 "${SCRIPT_DIR}"/demo_direct_call.sh -p "${NPORT}" -u "${NODEURL}" -V "${WORKER1URL}" -P "${WORKER1PORT}" -C "${CLIENT_BIN}" -t first
-"${SCRIPT_DIR}"/demo_direct_call.sh -p "${NPORT}" -u "${NODEURL}" -V "${WORKER2URL}" -P "${WORKER2PORT}" -C "${CLIENT_BIN}" -t second
 
 exit 0
