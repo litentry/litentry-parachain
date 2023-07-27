3e3605bd0b4b369bddd8e34871e9f73409c511a8
Verin1005 <daqingchong0809@gmail.com>
2023-07-27 11:54:46 +0800

    merge

--- tee-worker/ts-tests/parachain-api/dist/src/index.js
+++ tee-worker/ts-tests/parachain-api/dist/src/index.js
@@ -7,11 +7,11 @@ import { identity } from "../../prepare-build/interfaces/definitions";
 export { ApiPromise, Keyring, WsProvider }; // @fixme don't export WsProvider :P
 export const definitions = identity; // @fixme don't export?
 export async function create(provider) {
-    const api = await ApiPromise.create({ provider, types: identity.types });
-    const foo = api.createType("LitentryIdentity"); // @fixme: temporary probe for typing sanity
-    api.events.identityManagement.LinkIdentityFailed.is;
-    return api;
+	const api = await ApiPromise.create({ provider, types: identity.types });
+	const foo = api.createType("LitentryIdentity"); // @fixme: temporary probe for typing sanity
+	api.events.identityManagement.LinkIdentityFailed.is;
+	return api;
 }
 export function filterEvents(eventType, events) {
-    return events.filter(eventType.is.bind(eventType));
+	return events.filter(eventType.is.bind(eventType));
 }
