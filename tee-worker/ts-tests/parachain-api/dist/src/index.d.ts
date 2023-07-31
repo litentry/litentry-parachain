25267d3812969ed3b6ac413c02c3554c7cc9ed33
Kasper Ziemianek <kasper.ziemianek@gmail.com>
2023-07-31 09:56:57 +0200

    missing ts build files

--- tee-worker/ts-tests/parachain-api/dist/src/index.d.ts
+++ tee-worker/ts-tests/parachain-api/dist/src/index.d.ts
@@ -6,7 +6,17 @@ import type { AnyTuple } from "@polkadot/types/types";
 export type { CorePrimitivesErrorErrorDetail } from "@polkadot/types/lookup";
 export type { FrameSystemEventRecord } from "@polkadot/types/lookup";
 export type { ApiTypes, SubmittableExtrinsic } from "@polkadot/api/types";
-export type { Assertion, LitentryIdentity, LitentryValidationData, Web3Network, WorkerRpcReturnValue, TrustedCallSigned, Getter, ErrorResponse, StfError, } from "../build/interfaces";
+export type {
+    Assertion,
+    LitentryIdentity,
+    LitentryValidationData,
+    Web3Network,
+    WorkerRpcReturnValue,
+    TrustedCallSigned,
+    Getter,
+    ErrorResponse,
+    StfError,
+} from "../build/interfaces";
 export type { Codec } from "@polkadot/types/types";
 export type { Bytes } from "@polkadot/types-codec";
 export { ApiPromise, Keyring, WsProvider };
@@ -276,5 +286,8 @@ type ProviderInterface = Exclude<ApiOptions["provider"], undefined>;
 export declare function create(provider: ProviderInterface): Promise<ApiPromise>;
 type GuardType<GuardFunction> = GuardFunction extends (x: any) => x is infer Type ? Type : never;
 type IEventLike = Parameters<AugmentedEvent<never>["is"]>[0];
-export declare function filterEvents<ApiType extends ApiTypes, T extends AnyTuple, N>(eventType: AugmentedEvent<ApiType, T, N>, events: IEventLike[]): GuardType<AugmentedEvent<ApiType, T, N>["is"]>[];
+export declare function filterEvents<ApiType extends ApiTypes, T extends AnyTuple, N>(
+    eventType: AugmentedEvent<ApiType, T, N>,
+    events: IEventLike[]
+): GuardType<AugmentedEvent<ApiType, T, N>["is"]>[];
 //# sourceMappingURL=index.d.ts.map
