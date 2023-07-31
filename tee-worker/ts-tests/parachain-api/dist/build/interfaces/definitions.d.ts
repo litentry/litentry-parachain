025c2aa74731f6d3d57b9e946d1c007a4d689a70
Kasper Ziemianek <kasper.ziemianek@gmail.com>
2023-07-31 22:38:53 +0200

    Revert "fix: dist/build files missing; breaks CI"
    This reverts commit 7154e81e2a396fba0f8e5e3e5cd6e55ce0244111.

--- tee-worker/ts-tests/parachain-api/dist/build/interfaces/definitions.d.ts
+++
@@ -1,2 +0,0 @@
-export { default as dummynocustom } from "./dummynocustom/definitions";
-//# sourceMappingURL=definitions.d.ts.map
--- tee-worker/ts-tests/parachain-api/dist/build/interfaces/types.d.ts
+++
@@ -1,2 +0,0 @@
-export * from "./dummynocustom/types.js";
-//# sourceMappingURL=types.d.ts.map
