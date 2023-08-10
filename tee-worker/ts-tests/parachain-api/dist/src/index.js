e5373074bbdf20799111d4558193d002d291364f
Kailai Wang <Kailai.Wang@hotmail.com>
2023-08-10 03:11:24 +0000

    try to use dev branch index.js

--- tee-worker/ts-tests/parachain-api/dist/src/index.js
+++ tee-worker/ts-tests/parachain-api/dist/src/index.js
@@ -1,15 +1,17 @@
 import "@polkadot/api/augment";
 import "@polkadot/types-augment";
 import { ApiPromise, Keyring, WsProvider } from "@polkadot/api";
-import { identity } from "../build/interfaces/definitions";
+
+// Before we finalize the complete submission of the 'dist' files, we'll temporarily use the 'prepare-build' path here. Otherwise, CI can't find the files.
+import { identity } from "../../prepare-build/interfaces/definitions";
 export { ApiPromise, Keyring, WsProvider }; // @fixme don't export WsProvider :P
 export const definitions = identity; // @fixme don't export?
 export async function create(provider) {
-    const api = await ApiPromise.create({ provider, types: identity.types });
-    const foo = api.createType("LitentryIdentity"); // @fixme: temporary probe for typing sanity
-    api.events.identityManagement.LinkIdentityFailed.is;
-    return api;
+	const api = await ApiPromise.create({ provider, types: identity.types });
+	const foo = api.createType("LitentryIdentity"); // @fixme: temporary probe for typing sanity
+	api.events.identityManagement.LinkIdentityFailed.is;
+	return api;
 }
 export function filterEvents(eventType, events) {
-    return events.filter(eventType.is.bind(eventType));
+	return events.filter(eventType.is.bind(eventType));
 }
