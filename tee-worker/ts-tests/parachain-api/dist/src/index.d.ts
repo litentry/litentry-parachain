d91423940b7d0223a8c0f96f89e0fe6b4deb537a
Ariel Birnbaum <ariel@litentry.com>
2023-08-12 13:34:07 +0000

    update rpc defs; rebuild parachain-api

--- tee-worker/ts-tests/parachain-api/dist/src/index.d.ts
+++ tee-worker/ts-tests/parachain-api/dist/src/index.d.ts
@@ -6,15 +6,7 @@ import type { AnyTuple } from "@polkadot/types/types";
 export type { CorePrimitivesErrorErrorDetail } from "@polkadot/types/lookup";
 export type { FrameSystemEventRecord } from "@polkadot/types/lookup";
 export type { ApiTypes, SubmittableExtrinsic } from "@polkadot/api/types";
-export type {
-    Assertion,
-    LitentryIdentity,
-    LitentryValidationData,
-    Web3Network,
-    WorkerRpcReturnValue,
-    TrustedCallSigned,
-    Getter,
-} from "../build/interfaces";
+export type { Assertion, LitentryIdentity, LitentryValidationData, Web3Network, WorkerRpcReturnValue, TrustedCallSigned, Getter, } from "../build/interfaces";
 export type { Codec } from "@polkadot/types/types";
 export type { Bytes } from "@polkadot/types-codec";
 export { ApiPromise, Keyring, WsProvider };
@@ -151,6 +143,7 @@ export declare const definitions: {
                 Sr25519: string;
                 Ecdsa: string;
                 Ethereum: string;
+                EthereumPrettified: string;
             };
         };
         EthereumSignature: string;
@@ -240,8 +233,5 @@ type ProviderInterface = Exclude<ApiOptions["provider"], undefined>;
 export declare function create(provider: ProviderInterface): Promise<ApiPromise>;
 type GuardType<GuardFunction> = GuardFunction extends (x: any) => x is infer Type ? Type : never;
 type IEventLike = Parameters<AugmentedEvent<never>["is"]>[0];
-export declare function filterEvents<ApiType extends ApiTypes, T extends AnyTuple, N>(
-    eventType: AugmentedEvent<ApiType, T, N>,
-    events: IEventLike[]
-): GuardType<AugmentedEvent<ApiType, T, N>["is"]>[];
+export declare function filterEvents<ApiType extends ApiTypes, T extends AnyTuple, N>(eventType: AugmentedEvent<ApiType, T, N>, events: IEventLike[]): GuardType<AugmentedEvent<ApiType, T, N>["is"]>[];
 //# sourceMappingURL=index.d.ts.map
