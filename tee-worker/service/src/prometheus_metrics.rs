ba7882c31657890ab18efcdd4bedacce4fd653ee
felixfaisal <faisalahmedfarooq46@gmail.com>
2023-08-28 12:57:38 +0000

    fix: match arm

--- tee-worker/service/src/prometheus_metrics.rs
+++ tee-worker/service/src/prometheus_metrics.rs
@@ -240,6 +240,7 @@ fn handle_stf_call_request(req: RequestType, time: f64) {
 				Assertion::A11(_) => "A11",
 				Assertion::A13(_) => "A13",
 				Assertion::A14 => "A14",
+				Assertion::A20 => "A20",
 				Assertion::Achainable(..) => "Achainable",
 			};
 			inc_stf_calls(category, label);
