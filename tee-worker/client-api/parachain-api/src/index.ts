08e03ca9995d80703e632b610196695e5066b9f4
Verin1005 <daqingchong0809@gmail.com>
2023-09-01 14:46:58 +0800

    extract parachain/sidechain api folder

--- tee-worker/client-api/parachain-api/src/index.ts
+++ tee-worker/client-api/parachain-api/src/index.ts
@@ -1,21 +1,53 @@
-import { ApiOptions } from "@polkadot/api/types";
-import { ApiPromise } from "@polkadot/api";
+import "@polkadot/api/augment";
+import "@polkadot/types-augment";
+import { ApiOptions, ApiTypes, AugmentedEvent } from "@polkadot/api/types";
+import { ApiPromise, Keyring, WsProvider } from "@polkadot/api";
 
-import rawMetadata from "sidechain-api/litentry-sidechain-metadata.json";
+import { identity } from "../build/interfaces/definitions";
+import { LitentryIdentity } from "../build/interfaces";
+import type { AnyTuple } from "@polkadot/types/types";
 
-export { Metadata, TypeRegistry } from "@polkadot/types";
-export type { Index } from "@polkadot/types/interfaces";
-export type { ApiTypes, SubmittableExtrinsic } from "@polkadot/api/types";
 export type {
-    LitentryPrimitivesIdentity,
-    PalletIdentityManagementTeeIdentityContext,
-    PalletIdentityManagementTeeError,
+    CorePrimitivesErrorErrorDetail,
+    PalletVcManagementVcContext,
+    TeerexPrimitivesEnclave,
 } from "@polkadot/types/lookup";
 
-export { rawMetadata };
+export type { FrameSystemEventRecord } from "@polkadot/types/lookup";
+export type { ApiTypes, SubmittableExtrinsic } from "@polkadot/api/types";
+export type {
+    Assertion,
+    LitentryIdentity,
+    LitentryValidationData,
+    Web3Network,
+    WorkerRpcReturnValue,
+    TrustedCallSigned,
+    Getter,
+    RequestVCResult,
+    TrustedOperationResponse,
+    StfError,
+    LinkIdentityResult,
+} from "../build/interfaces";
+export type { Codec } from "@polkadot/types/types";
+export type { Bytes } from "@polkadot/types-codec";
+export { ApiPromise, Keyring, WsProvider }; // @fixme don't export WsProvider :P
+
+export const definitions = identity; // @fixme don't export?
 
 type ProviderInterface = Exclude<ApiOptions["provider"], undefined>;
-
 export async function create(provider: ProviderInterface): Promise<ApiPromise> {
-    return await ApiPromise.create({ provider });
+    const api = await ApiPromise.create({ provider, types: identity.types });
+    const foo: LitentryIdentity = api.createType("LitentryIdentity"); // @fixme: temporary probe for typing sanity
+    api.events.identityManagement.LinkIdentityFailed.is;
+    return api;
+}
+
+type GuardType<GuardFunction> = GuardFunction extends (x: any) => x is infer Type ? Type : never;
+type IEventLike = Parameters<AugmentedEvent<never>["is"]>[0];
+
+export function filterEvents<ApiType extends ApiTypes, T extends AnyTuple, N>(
+    eventType: AugmentedEvent<ApiType, T, N>,
+    events: IEventLike[]
+): GuardType<AugmentedEvent<ApiType, T, N>["is"]>[] {
+    return events.filter(eventType.is.bind(eventType));
 }
