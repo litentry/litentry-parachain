dde49dbc2da8ebe632f706a60ce32b89649fa2c2
Kailai Wang <Kailai.Wang@hotmail.com>
2023-09-02 17:37:08 +0000

    fail early

--- tee-worker/scripts/litentry/generate_parachain_artefacts.sh
+++ tee-worker/scripts/litentry/generate_parachain_artefacts.sh
@@ -7,6 +7,12 @@ DESTDIR="$ROOTDIR/tee-worker/docker/litentry"
 # generate files
 cd "$ROOTDIR"
 make generate-docker-compose-rococo
+
+if [ $(stat -c %s docker/generated-rococo/rococo-local.json) -ne 5040588 ]; then
+    echo "unexpected rococo-local.json size"
+    exit 1
+fi
+
 # copy files over to `DESTDIR`
 mkdir -p "$DESTDIR"
 cp docker/generated-rococo/* "$DESTDIR/"
