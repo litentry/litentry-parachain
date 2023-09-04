f67a1e88da0ad9e6bc8ff91ddc83e682b9107f6a
Verin1005 <daqingchong0809@gmail.com>
2023-09-04 23:45:42 +0800

    format

--- tee-worker/client-api/sidechain-api/dist/src/index.d.ts
+++ tee-worker/client-api/sidechain-api/dist/src/index.d.ts
@@ -2,6 +2,22 @@ export * from "@polkadot/api/types";
 export * from "@polkadot/api";
 export * from "@polkadot/types/lookup";
 export * from "../build/interfaces";
-import { PalletIdentityManagementTeeCall, LitentryPrimitivesIdentity, PalletIdentityManagementTeeIdentityContext, PalletIdentityManagementTeeIdentityContextIdentityStatus, CorePrimitivesNetworkWeb3Network, SpRuntimeMultiSignature, PalletIdentityManagementTeeError } from "@polkadot/types/lookup";
-export type { PalletIdentityManagementTeeCall, LitentryPrimitivesIdentity, PalletIdentityManagementTeeIdentityContext, PalletIdentityManagementTeeIdentityContextIdentityStatus, CorePrimitivesNetworkWeb3Network, SpRuntimeMultiSignature, PalletIdentityManagementTeeError, };
+import {
+    PalletIdentityManagementTeeCall,
+    LitentryPrimitivesIdentity,
+    PalletIdentityManagementTeeIdentityContext,
+    PalletIdentityManagementTeeIdentityContextIdentityStatus,
+    CorePrimitivesNetworkWeb3Network,
+    SpRuntimeMultiSignature,
+    PalletIdentityManagementTeeError,
+} from "@polkadot/types/lookup";
+export type {
+    PalletIdentityManagementTeeCall,
+    LitentryPrimitivesIdentity,
+    PalletIdentityManagementTeeIdentityContext,
+    PalletIdentityManagementTeeIdentityContextIdentityStatus,
+    CorePrimitivesNetworkWeb3Network,
+    SpRuntimeMultiSignature,
+    PalletIdentityManagementTeeError,
+};
 //# sourceMappingURL=index.d.ts.map
