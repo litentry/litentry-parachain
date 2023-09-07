c7ea994d84be9df6949f889581e2030ba9ea0067
zhouhuitian <zhouhui@litentry.com>
2023-09-07 21:43:18 +0800

    update assertion result

--- tee-worker/litentry/core/assertion-build/src/oneblock/mod.rs
+++ tee-worker/litentry/core/assertion-build/src/oneblock/mod.rs
@@ -67,71 +67,110 @@ fn fetch_data_from_notion(course_type: &OneBlockCourseType) -> Result<OneBlockRe
 	})
 }
 
-fn oneblock_course_result(
-	data: &serde_json::Value,
-	course_type: &OneBlockCourseType,
-	addresses: Vec<String>,
-) -> Result<bool> {
-	let get_results = |data: &serde_json::Value| -> Option<Vec<serde_json::Value>> {
-		data.get("results").and_then(|results| results.as_array()).cloned()
-	};
+const ONEBLOCK_TABLE_COL_NUM: usize = 6;
+const ONEBLOCK_COURSES_CONTENT: [&str; 6] =
+	["第一课", "第二课", "第三课", "第四课", "第五课", "第六课"];
+const ONEBLOCK_GRADUATE_CONTENT: [&str; 2] = ["YES", "NO"];
+const ONEBLOCK_OUTSTANDING_CONTENT: [&str; 2] = ["YES", "NO"];
 
-	// A object contains 6 rows of content
-	let get_table_row = |object: &serde_json::Value| -> Option<Vec<serde_json::Value>> {
-		object
-			.get("table_row")
+#[derive(Debug)]
+pub struct OneBlockData {
+	// COLs info in one ROW
+	// 学号	 | 姓名	 | substrate地址 | 课程观看进度 | 是否毕业 | 是否优秀毕业
+	rows: Vec<serde_json::Value>,
+}
+
+impl OneBlockData {
+	pub fn new(rows: Vec<serde_json::Value>) -> Self {
+		Self { rows }
+	}
+
+	// As shown in the table, the FIRST column is the student NO.
+	fn check_student_number(&self, columns: &[serde_json::Value]) -> bool {
+		self.get_column_text(&columns[0]).parse::<u32>().is_ok()
+	}
+
+	pub fn get_address(&self, columns: &[serde_json::Value]) -> String {
+		self.get_column_text(&columns[2])
+	}
+
+	pub fn get_watch_progress(&self, columns: &[serde_json::Value]) -> String {
+		self.get_column_text(&columns[3])
+	}
+
+	pub fn get_graduate(&self, columns: &[serde_json::Value]) -> String {
+		self.get_column_text(&columns[4]).to_ascii_uppercase()
+	}
+
+	pub fn get_outstanding(&self, columns: &[serde_json::Value]) -> String {
+		self.get_column_text(&columns[5]).to_ascii_uppercase()
+	}
+
+	pub fn check(&self, course_type: &OneBlockCourseType, addresses: Vec<String>) -> bool {
+		for row in self.rows.iter() {
+			if let Some(columns) = self.collect_columns(row) {
+				if self.check_student_number(&columns)
+					&& addresses.contains(&self.get_address(&columns))
+				{
+					return self.qualify(&columns, course_type)
+				}
+			}
+		}
+
+		false
+	}
+}
+
+pub trait OneBlockTableDataHandler {
+	fn collect_columns(&self, row: &serde_json::Value) -> Option<Vec<serde_json::Value>>;
+	fn get_column_text(&self, column: &serde_json::Value) -> String;
+}
+
+impl OneBlockTableDataHandler for OneBlockData {
+	fn collect_columns(&self, row: &serde_json::Value) -> Option<Vec<serde_json::Value>> {
+		row.get("table_row")
 			.and_then(|data| data.get("cells"))
 			.and_then(|data| data.as_array())
-			.filter(|data| data.len() == 6)
+			.filter(|data| data.len() == ONEBLOCK_TABLE_COL_NUM)
 			.cloned()
-	};
+	}
 
-	let get_cell_text = |cells: &serde_json::Value| -> String {
-		cells
+	fn get_column_text(&self, column: &serde_json::Value) -> String {
+		column
 			.as_array()
 			.filter(|cells| cells.len() == 1)
 			.and_then(|cell_data| {
 				cell_data[0].get("text").and_then(|data| {
-					data.get("content").and_then(|data| data.as_str()).map(|data| data.to_string())
+					data.get("content")
+						.and_then(|data| data.as_str())
+						.map(|data| data.trim().to_string())
 				})
 			})
 			.unwrap_or_default()
-	};
+	}
+}
 
-	// If the first colume content of an table cell is not a numeric digit, then this is a invalid cell, no need to parse data.
-	// Using this method to differentiate student data.
-	let is_student_info_cell = |cells: &Vec<serde_json::Value>| -> bool {
-		get_cell_text(&cells[0]).parse::<u32>().is_ok()
-	};
+pub trait OneBlockAssertionQualify {
+	fn qualify(&self, columns: &[serde_json::Value], course_type: &OneBlockCourseType) -> bool;
+}
 
-	let get_student_address =
-		|cells: &Vec<serde_json::Value>| -> String { get_cell_text(&cells[2]) };
-
-	let qualify = |cells: &Vec<serde_json::Value>, course_type: &OneBlockCourseType| -> bool {
-		let cell_index: usize = match course_type {
-			OneBlockCourseType::CourseCompletion => 4,
-			OneBlockCourseType::CourseExcellenceCompletion => 5,
-			OneBlockCourseType::CourseParticipation => 3,
-		};
-		let text = get_cell_text(&cells[cell_index]);
-
-		// TODO: HOW TO CHECK IF THIS TYPE OF COURSE VC IS VALID???
-		// JUST CHECK IF THIS BLANK IS EMPTY OR NOT???
-		!text.is_empty()
-	};
-
-	if let Some(results) = get_results(data) {
-		for object in results.iter() {
-			if let Some(cells) = get_table_row(object) {
-				if is_student_info_cell(&cells) && addresses.contains(&get_student_address(&cells))
-				{
-					return Ok(qualify(&cells, course_type))
-				}
-			}
+impl OneBlockAssertionQualify for OneBlockData {
+	fn qualify(&self, columns: &[serde_json::Value], course_type: &OneBlockCourseType) -> bool {
+		match course_type {
+			OneBlockCourseType::CourseCompletion => {
+				let text = self.get_graduate(columns);
+				ONEBLOCK_GRADUATE_CONTENT[0] == text
+			},
+			OneBlockCourseType::CourseExcellenceCompletion => {
+				let text = self.get_outstanding(columns);
+				ONEBLOCK_OUTSTANDING_CONTENT[0] == text
+			},
+			OneBlockCourseType::CourseParticipation => {
+				let text = self.get_watch_progress(columns);
+				ONEBLOCK_COURSES_CONTENT[5] == text
+			},
 		}
 	}
-
-	Ok(false)
 }
 
 pub fn query_oneblock_status(
@@ -141,7 +180,25 @@ pub fn query_oneblock_status(
 	let oneblock_response = fetch_data_from_notion(course_type)?;
 	debug!("OneBlock Assertion Response: {oneblock_response:?}");
 
-	oneblock_course_result(&oneblock_response.data, course_type, addresses)
+	Ok(check_oneblock_data(&oneblock_response, course_type, addresses))
+}
+
+pub fn check_oneblock_data(
+	oneblock_response: &OneBlockResponse,
+	course_type: &OneBlockCourseType,
+	addresses: Vec<String>,
+) -> bool {
+	if let Some(rows) = oneblock_response
+		.data
+		.get("results")
+		.and_then(|results| results.as_array())
+		.cloned()
+	{
+		let data = OneBlockData::new(rows);
+		return data.check(course_type, addresses)
+	}
+
+	false
 }
 
 #[cfg(test)]
@@ -149,20 +206,33 @@ mod tests {
 	use super::*;
 
 	const RESPONSE_ONEBLOCK: &str = r#"
-{"object":"list","results":[{"object":"block","id":"8b371d31-1e60-4060-9f63-3527fa8cfe0c","parent":{"type":"block_id","block_id":"e4068e6a-3262-4346-8f35-dcdc0c43f111"},"created_time":"2023-09-05T03:21:00.000Z","last_edited_time":"2023-09-05T04:11:00.000Z","created_by":{"object":"user","id":"03bab8a4-8794-44d8-8843-961ee4c33485"},"last_edited_by":{"object":"user","id":"c208e40c-306a-4b31-a249-49139ff24411"},"has_children":false,"archived":false,"type":"table_row","table_row":{"cells":[[{"type":"text","text":{"content":"学号","link":null},"annotations":{"bold":false,"italic":false,"strikethrough":false,"underline":false,"code":false,"color":"default"},"plain_text":"学号","href":null}],[{"type":"text","text":{"content":"姓名","link":null},"annotations":{"bold":false,"italic":false,"strikethrough":false,"underline":false,"code":false,"color":"default"},"plain_text":"姓名","href":null}],[{"type":"text","text":{"content":"substrate地址","link":null},"annotations":{"bold":false,"italic":false,"strikethrough":false,"underline":false,"code":false,"color":"default"},"plain_text":"substrate地址","href":null}],[{"type":"text","text":{"content":"课程观看进度","link":null},"annotations":{"bold":false,"italic":false,"strikethrough":false,"underline":false,"code":false,"color":"default"},"plain_text":"课程观看进度","href":null}],[{"type":"text","text":{"content":"是否毕业","link":null},"annotations":{"bold":false,"italic":false,"strikethrough":false,"underline":false,"code":false,"color":"default"},"plain_text":"是否毕业","href":null}],[{"type":"text","text":{"content":"是否优秀毕业","link":null},"annotations":{"bold":false,"italic":false,"strikethrough":false,"underline":false,"code":false,"color":"default"},"plain_text":"是否优秀毕业","href":null}]]}},{"object":"block","id":"00d4a220-db1f-4d24-8b4f-4d3815dab3c5","parent":{"type":"block_id","block_id":"e4068e6a-3262-4346-8f35-dcdc0c43f111"},"created_time":"2023-09-05T03:21:00.000Z","last_edited_time":"2023-09-05T04:11:00.000Z","created_by":{"object":"user","id":"03bab8a4-8794-44d8-8843-961ee4c33485"},"last_edited_by":{"object":"user","id":"c208e40c-306a-4b31-a249-49139ff24411"},"has_children":false,"archived":false,"type":"table_row","table_row":{"cells":[[{"type":"text","text":{"content":"Team1","link":null},"annotations":{"bold":false,"italic":false,"strikethrough":false,"underline":false,"code":false,"color":"default"},"plain_text":"Team1","href":null}],[],[],[],[],[]]}},{"object":"block","id":"71130f91-0326-468a-8c42-c64c17e779af","parent":{"type":"block_id","block_id":"e4068e6a-3262-4346-8f35-dcdc0c43f111"},"created_time":"2023-09-05T03:21:00.000Z","last_edited_time":"2023-09-05T06:25:00.000Z","created_by":{"object":"user","id":"03bab8a4-8794-44d8-8843-961ee4c33485"},"last_edited_by":{"object":"user","id":"c208e40c-306a-4b31-a249-49139ff24411"},"has_children":false,"archived":false,"type":"table_row","table_row":{"cells":[[{"type":"text","text":{"content":"1264","link":null},"annotations":{"bold":false,"italic":false,"strikethrough":false,"underline":false,"code":false,"color":"default"},"plain_text":"1264","href":null}],[{"type":"text","text":{"content":"Student1","link":null},"annotations":{"bold":false,"italic":false,"strikethrough":false,"underline":false,"code":false,"color":"default"},"plain_text":"Student1","href":null}],[{"type":"text","text":{"content":"5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQQ","link":null},"annotations":{"bold":false,"italic":false,"strikethrough":false,"underline":false,"code":false,"color":"default"},"plain_text":"5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQQ","href":null}],[{"type":"text","text":{"content":"♥","link":null},"annotations":{"bold":false,"italic":false,"strikethrough":false,"underline":false,"code":false,"color":"default"},"plain_text":"♥","href":null}],[],[]]}},{"object":"block","id":"72cdaf64-9dc5-4e71-8285-bba8d03abd99","parent":{"type":"block_id","block_id":"e4068e6a-3262-4346-8f35-dcdc0c43f111"},"created_time":"2023-09-05T03:21:00.000Z","last_edited_time":"2023-09-05T04:11:00.000Z","created_by":{"object":"user","id":"03bab8a4-8794-44d8-8843-961ee4c33485"},"last_edited_by":{"object":"user","id":"c208e40c-306a-4b31-a249-49139ff24411"},"has_children":false,"archived":false,"type":"table_row","table_row":{"cells":[[{"type":"text","text":{"content":"1263","link":null},"annotations":{"bold":false,"italic":false,"strikethrough":false,"underline":false,"code":false,"color":"default"},"plain_text":"1263","href":null}],[{"type":"text","text":{"content":"Clement Tam","link":null},"annotations":{"bold":false,"italic":false,"strikethrough":false,"underline":false,"code":false,"color":"default"},"plain_text":"Clement Tam","href":null}],[{"type":"text","text":{"content":"5HYaWcUJvX1xjNnduouJnD3F5q5X7uCpGxHV2yCRZEurymEE","link":null},"annotations":{"bold":false,"italic":false,"strikethrough":false,"underline":false,"code":false,"color":"default"},"plain_text":"5HYaWcUJvX1xjNnduouJnD3F5q5X7uCpGxHV2yCRZEurymEE","href":null}],[],[],[]]}},{"object":"block","id":"42d0ada7-1e3d-493a-bd2f-17821d2e54a2","parent":{"type":"block_id","block_id":"e4068e6a-3262-4346-8f35-dcdc0c43f111"},"created_time":"2023-09-05T03:21:00.000Z","last_edited_time":"2023-09-05T04:11:00.000Z","created_by":{"object":"user","id":"03bab8a4-8794-44d8-8843-961ee4c33485"},"last_edited_by":{"object":"user","id":"c208e40c-306a-4b31-a249-49139ff24411"},"has_children":false,"archived":false,"type":"table_row","table_row":{"cells":[[{"type":"text","text":{"content":"1262","link":null},"annotations":{"bold":false,"italic":false,"strikethrough":false,"underline":false,"code":false,"color":"default"},"plain_text":"1262","href":null}],[{"type":"text","text":{"content":"Student2","link":null},"annotations":{"bold":false,"italic":false,"strikethrough":false,"underline":false,"code":false,"color":"default"},"plain_text":"Student2","href":null}],[{"type":"text","text":{"content":"12zh1QyBrqddzgLbxBHjmoCnna8XsT9pTTMPCUt1f7WtC1f5","link":null},"annotations":{"bold":false,"italic":false,"strikethrough":false,"underline":false,"code":false,"color":"default"},"plain_text":"12zh1QyBrqddzgLbxBHjmoCnna8XsT9pTTMPCUt1f7WtC1f5","href":null}],[],[],[]]}}],"next_cursor":null,"has_more":false,"type":"block","block":{}}
+{"object":"list","results":[{"object":"block","id":"8b371d31-1e60-4060-9f63-3527fa8cfe0c","parent":{"type":"block_id","block_id":"e4068e6a-3262-4346-8f35-dcdc0c43f111"},"created_time":"2023-09-05T03:21:00.000Z","last_edited_time":"2023-09-05T04:11:00.000Z","created_by":{"object":"user","id":"03bab8a4-8794-44d8-8843-961ee4c33485"},"last_edited_by":{"object":"user","id":"c208e40c-306a-4b31-a249-49139ff24411"},"has_children":false,"archived":false,"type":"table_row","table_row":{"cells":[[{"type":"text","text":{"content":"学号","link":null},"annotations":{"bold":false,"italic":false,"strikethrough":false,"underline":false,"code":false,"color":"default"},"plain_text":"学号","href":null}],[{"type":"text","text":{"content":"姓名","link":null},"annotations":{"bold":false,"italic":false,"strikethrough":false,"underline":false,"code":false,"color":"default"},"plain_text":"姓名","href":null}],[{"type":"text","text":{"content":"substrate地址","link":null},"annotations":{"bold":false,"italic":false,"strikethrough":false,"underline":false,"code":false,"color":"default"},"plain_text":"substrate地址","href":null}],[{"type":"text","text":{"content":"课程观看进度","link":null},"annotations":{"bold":false,"italic":false,"strikethrough":false,"underline":false,"code":false,"color":"default"},"plain_text":"课程观看进度","href":null}],[{"type":"text","text":{"content":"是否毕业","link":null},"annotations":{"bold":false,"italic":false,"strikethrough":false,"underline":false,"code":false,"color":"default"},"plain_text":"是否毕业","href":null}],[{"type":"text","text":{"content":"是否优秀毕业","link":null},"annotations":{"bold":false,"italic":false,"strikethrough":false,"underline":false,"code":false,"color":"default"},"plain_text":"是否优秀毕业","href":null}]]}},{"object":"block","id":"00d4a220-db1f-4d24-8b4f-4d3815dab3c5","parent":{"type":"block_id","block_id":"e4068e6a-3262-4346-8f35-dcdc0c43f111"},"created_time":"2023-09-05T03:21:00.000Z","last_edited_time":"2023-09-05T04:11:00.000Z","created_by":{"object":"user","id":"03bab8a4-8794-44d8-8843-961ee4c33485"},"last_edited_by":{"object":"user","id":"c208e40c-306a-4b31-a249-49139ff24411"},"has_children":false,"archived":false,"type":"table_row","table_row":{"cells":[[{"type":"text","text":{"content":"Team1","link":null},"annotations":{"bold":false,"italic":false,"strikethrough":false,"underline":false,"code":false,"color":"default"},"plain_text":"Team1","href":null}],[],[],[],[],[]]}},{"object":"block","id":"71130f91-0326-468a-8c42-c64c17e779af","parent":{"type":"block_id","block_id":"e4068e6a-3262-4346-8f35-dcdc0c43f111"},"created_time":"2023-09-05T03:21:00.000Z","last_edited_time":"2023-09-05T06:25:00.000Z","created_by":{"object":"user","id":"03bab8a4-8794-44d8-8843-961ee4c33485"},"last_edited_by":{"object":"user","id":"c208e40c-306a-4b31-a249-49139ff24411"},"has_children":false,"archived":false,"type":"table_row","table_row":{"cells":[[{"type":"text","text":{"content":"1264","link":null},"annotations":{"bold":false,"italic":false,"strikethrough":false,"underline":false,"code":false,"color":"default"},"plain_text":"1264","href":null}],[{"type":"text","text":{"content":"Student1","link":null},"annotations":{"bold":false,"italic":false,"strikethrough":false,"underline":false,"code":false,"color":"default"},"plain_text":"Student1","href":null}],[{"type":"text","text":{"content":"5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQQ","link":null},"annotations":{"bold":false,"italic":false,"strikethrough":false,"underline":false,"code":false,"color":"default"},"plain_text":"5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQQ","href":null}],[{"type":"text","text":{"content":"第一课","link":null},"annotations":{"bold":false,"italic":false,"strikethrough":false,"underline":false,"code":false,"color":"default"},"plain_text":"第一课","href":null}],[{"type":"text","text":{"content":"yes","link":null},"annotations":{"bold":false,"italic":false,"strikethrough":false,"underline":false,"code":false,"color":"default"},"plain_text":"yes","href":null}],[{"type":"text","text":{"content":"yes","link":null},"annotations":{"bold":false,"italic":false,"strikethrough":false,"underline":false,"code":false,"color":"default"},"plain_text":"yes","href":null}]]}},{"object":"block","id":"72cdaf64-9dc5-4e71-8285-bba8d03abd99","parent":{"type":"block_id","block_id":"e4068e6a-3262-4346-8f35-dcdc0c43f111"},"created_time":"2023-09-05T03:21:00.000Z","last_edited_time":"2023-09-05T04:11:00.000Z","created_by":{"object":"user","id":"03bab8a4-8794-44d8-8843-961ee4c33485"},"last_edited_by":{"object":"user","id":"c208e40c-306a-4b31-a249-49139ff24411"},"has_children":false,"archived":false,"type":"table_row","table_row":{"cells":[[{"type":"text","text":{"content":"1263","link":null},"annotations":{"bold":false,"italic":false,"strikethrough":false,"underline":false,"code":false,"color":"default"},"plain_text":"1263","href":null}],[{"type":"text","text":{"content":"Clement Tam","link":null},"annotations":{"bold":false,"italic":false,"strikethrough":false,"underline":false,"code":false,"color":"default"},"plain_text":"Clement Tam","href":null}],[{"type":"text","text":{"content":"5HYaWcUJvX1xjNnduouJnD3F5q5X7uCpGxHV2yCRZEurymEE","link":null},"annotations":{"bold":false,"italic":false,"strikethrough":false,"underline":false,"code":false,"color":"default"},"plain_text":"5HYaWcUJvX1xjNnduouJnD3F5q5X7uCpGxHV2yCRZEurymEE","href":null}],[],[],[]]}},{"object":"block","id":"42d0ada7-1e3d-493a-bd2f-17821d2e54a2","parent":{"type":"block_id","block_id":"e4068e6a-3262-4346-8f35-dcdc0c43f111"},"created_time":"2023-09-05T03:21:00.000Z","last_edited_time":"2023-09-05T04:11:00.000Z","created_by":{"object":"user","id":"03bab8a4-8794-44d8-8843-961ee4c33485"},"last_edited_by":{"object":"user","id":"c208e40c-306a-4b31-a249-49139ff24411"},"has_children":false,"archived":false,"type":"table_row","table_row":{"cells":[[{"type":"text","text":{"content":"1262","link":null},"annotations":{"bold":false,"italic":false,"strikethrough":false,"underline":false,"code":false,"color":"default"},"plain_text":"1262","href":null}],[{"type":"text","text":{"content":"Student2","link":null},"annotations":{"bold":false,"italic":false,"strikethrough":false,"underline":false,"code":false,"color":"default"},"plain_text":"Student2","href":null}],[{"type":"text","text":{"content":"12zh1QyBrqddzgLbxBHjmoCnna8XsT9pTTMPCUt1f7WtC1f5","link":null},"annotations":{"bold":false,"italic":false,"strikethrough":false,"underline":false,"code":false,"color":"default"},"plain_text":"12zh1QyBrqddzgLbxBHjmoCnna8XsT9pTTMPCUt1f7WtC1f5","href":null}],[],[],[]]}}],"next_cursor":null,"has_more":false,"type":"block","block":{}}
 "#;
 
 	#[test]
-	fn oneblock_course_result_works() {
+	fn check_oneblock_data_works() {
 		let address = "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQQ".to_string();
-		let oneblock_response: serde_json::Value = serde_json::from_str(RESPONSE_ONEBLOCK).unwrap();
+		let oneblock_response: OneBlockResponse = serde_json::from_str(RESPONSE_ONEBLOCK).unwrap();
 
-		let value = oneblock_course_result(
+		let participation = check_oneblock_data(
 			&oneblock_response,
 			&OneBlockCourseType::CourseParticipation,
-			vec![address],
+			vec![address.clone()],
 		);
-		assert!(value.is_ok());
-		assert_eq!(value.unwrap(), true);
+		assert!(!participation);
+
+		let completion = check_oneblock_data(
+			&oneblock_response,
+			&OneBlockCourseType::CourseCompletion,
+			vec![address.clone()],
+		);
+		assert!(completion);
+
+		let outstanding = check_oneblock_data(
+			&oneblock_response,
+			&OneBlockCourseType::CourseExcellenceCompletion,
+			vec![address.clone()],
+		);
+		assert!(outstanding);
 	}
 }
