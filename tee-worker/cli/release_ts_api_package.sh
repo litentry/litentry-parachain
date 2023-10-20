b1f3bd3fa061946e3cc748a5ff1a72246b85589d
Verin1005 <daqingchong0809@gmail.com>
2023-10-20 17:26:45 +0800

    debug: sidechain metadata

--- tee-worker/cli/release_ts_api_package.sh
+++ tee-worker/cli/release_ts_api_package.sh
@@ -14,15 +14,18 @@ TEST=$1
 
 cd /client-api
 pnpm install
+
 cd /client-api/parachain-api
 curl -s -H \"Content-Type: application/json\" -d '{\"id\":\"1\", \"jsonrpc\":\"2.0\", \"method\": \"state_getMetadata\", \"params\":[]}' http://litentry-node:9912 > prepare-build/litentry-parachain-metadata.json
 echo "update parachain metadata"
 
 cd /
 ls
+/bin/litentry-cli print-sgx-metadata-raw
 
 cd /client-api/sidechain-api
-curl -s -H \"Content-Type: application/json\" -d '{\"id\":\"1\", \"jsonrpc\":\"2.0\", \"method\": \"state_getMetadata\", \"params\":[]}' https://litentry-worker-1:2011 > prepare-build/litentry-sidechain-metadata.json
+/bin/litentry-cli print-sgx-metadata-raw > prepare-build/litentry-sidechain-metadata.json
+
 echo "update sidechain metadata"
 
 git status
