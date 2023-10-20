972cfaee4b471d33d54c2b73c3627fd947f42277
Verin1005 <daqingchong0809@gmail.com>
2023-10-20 20:28:06 +0800

    debug: update volumes

--- tee-worker/cli/release_ts_api_package.sh
+++ tee-worker/cli/release_ts_api_package.sh
@@ -10,7 +10,6 @@ function usage() {
 }
 
 [ $# -ne 1 ] && (usage; exit 1)
-TEST=$1
 
 cd /client-api
 pnpm install
@@ -20,12 +19,11 @@ cd /client-api/parachain-api
 curl -s -H "Content-Type: application/json" -d '{"id": "1", "jsonrpc": "2.0", "method": "state_getMetadata", "params": []}' http://litentry-node:9912 > prepare-build/litentry-parachain-metadata.json
 echo "update parachain metadata"
 
+ls /usr/local/worker-bin
 
-cd /
-ls
-cd ./bin
-ls
-./bin/litentry-cli print-sgx-metadata-raw
+cd  /client-api/sidechain-api
+
+/usr/local/worker-bin/litentry-cli print-sgx-metadata-raw
 
 echo "update sidechain metadata"
 
