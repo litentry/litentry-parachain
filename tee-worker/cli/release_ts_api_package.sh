d80d19285626ebf2de4b44048c2686b12f2d57a9
Verin1005 <daqingchong0809@gmail.com>
2023-10-24 23:53:22 +0800

    debug: build metadata

--- tee-worker/cli/release_ts_api_package.sh
+++ tee-worker/cli/release_ts_api_package.sh
@@ -14,7 +14,6 @@ apt-get install sudo
 sudo apt-get install wget
 sudo wget -qO /usr/local/bin/websocat https://github.com/vi/websocat/releases/latest/download/websocat.x86_64-unknown-linux-musl
 sudo chmod a+x /usr/local/bin/websocat
-whereis websocat
 websocat --version
 
 
@@ -22,17 +21,12 @@ cd /client-api
 pnpm install
 
 cd /client-api/parachain-api
-
 curl -s -H "Content-Type: application/json" -d '{"id": "1", "jsonrpc": "2.0", "method": "state_getMetadata", "params": []}' http://litentry-node:9912 > prepare-build/litentry-parachain-metadata.json
 echo "update parachain metadata"
 
 cd  /client-api/sidechain-api
 echo '{"id":1,"jsonrpc":"2.0","method":"state_getMetadata","params":[]}' | /usr/local/bin/websocat -n1 -k -B 99999999 wss://litentry-worker-1:2011 > prepare-build/litentry-sidechain-metadata.json
-
 echo "update sidechain metadata"
 
-sudo apt install git
-git status
-
-# pnpm run build
+pnpm run build
 
