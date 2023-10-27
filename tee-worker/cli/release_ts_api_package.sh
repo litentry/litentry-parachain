2c6613626018b0ba7d97e8e14113ffd675300f36
Verin1005 <daqingchong0809@gmail.com>
2023-10-27 12:04:44 +0800

    complete dev version

--- tee-worker/cli/release_ts_api_package.sh
+++ tee-worker/cli/release_ts_api_package.sh
@@ -38,16 +38,7 @@ echo "Using node uri $NODEURL:$NPORT"
 echo "Using trusted-worker uri $WORKER1URL:$WORKER1PORT"
 echo ""
 
-echo "fetch sidechian metadata"
-${CLIENT} print-sgx-metadata-raw
-echo ""
 
-# function usage() {
-#     echo "Usage: $0 <Options>"
-#     echo "updating metadata"
-# }
-
-# [ $# -ne 1 ] && (usage; exit 1)
 apt-get install sudo
 sudo apt-get install wget
 sudo wget -qO /usr/local/bin/websocat https://github.com/vi/websocat/releases/latest/download/websocat.x86_64-unknown-linux-musl
@@ -58,16 +49,13 @@ websocat --version
 cd /client-api
 pnpm install
 
-# cd /client-api/parachain-api
-# echo '{"id":1,"jsonrpc":"2.0","method":"state_getMetadata","params":[]}' | /usr/local/bin/websocat -n1 -k -B 99999999 ws://litentry-node:9912
-# # echo '{"id":1,"jsonrpc":"2.0","method":"state_getMetadata","params":[]}' | /usr/local/bin/websocat -n1 -k -B 99999999 ws://litentry-node:9912 > prepare-build/litentry-parachain-metadata.json
-
-# echo "update parachain metadata"
+cd /client-api/parachain-api
+echo '{"id":1,"jsonrpc":"2.0","method":"state_getMetadata","params":[]}' | /usr/local/bin/websocat -n1 -k -B 99999999 ws://litentry-node:9912 > prepare-build/litentry-parachain-metadata.json
+echo "update parachain metadata"
 
 cd  /client-api/sidechain-api
-echo '{"id":1,"jsonrpc":"2.0","method":"state_getMetadata","params":[]}' | /usr/local/bin/websocat -n1 -k -B 99999999 wss://litentry-worker-1:2011
-# # echo '{"id":1,"jsonrpc":"2.0","method":"state_getMetadata","params":[]}' | /usr/local/bin/websocat -n1 -k -B 99999999 wss://litentry-worker-1:2011 > prepare-build/litentry-sidechain-metadata.json
-# echo "update sidechain metadata"
+${CLIENT} print-sgx-metadata-raw > prepare-build/litentry-sidechain-metadata.json
+echo "update sidechain metadata"
 
-# pnpm run build
+pnpm run build
 
