9544245ccd3a8a59e9b3039b458fc15302d17480
Verin1005 <daqingchong0809@gmail.com>
2023-10-31 17:36:43 +0800

    update tee-test shell

--- tee-worker/cli/lit_ts_test.sh
+++ tee-worker/cli/lit_ts_test.sh
@@ -2,25 +2,65 @@
 
 # Copyright 2020-2023 Trust Computing GmbH.
 
-set -euo pipefail
+while getopts ":p:A:u:V:C:T:" opt; do
+    case $opt in
+        p)
+            NPORT=$OPTARG
+            ;;
+        A)
+            WORKER1PORT=$OPTARG
+            ;;
+        u)
+            NODEURL=$OPTARG
+            ;;
+        V)
+            WORKER1URL=$OPTARG
+            ;;
+        C)
+            CLIENT_BIN=$OPTARG
+            ;;
+        T)
+            INTEGRATION_TEST=$OPTARG
+            ;;
+    esac
+done
 
-function usage() {
-    echo "Usage: $0 <Options>"
-    echo ""
-    echo "This is a script for tee-worker ts-test. Current available Options:"
-    echo "  test-ii-identity: "
-    echo "  test-ii-vc: "
-    echo "  test-resuming-worker: "
-    echo ""
-    echo "Please try to extend the above list when adding new ts-test."
-}
+# Using default port if none given as arguments.
+NPORT=${NPORT:-9944}
+NODEURL=${NODEURL:-"ws://127.0.0.1"}
 
-[ $# -ne 1 ] && (usage; exit 1)
-TEST=$1
+WORKER1PORT=${WORKER1PORT:-2000}
+WORKER1URL=${WORKER1URL:-"wss://127.0.0.1"}
+
+CLIENT_BIN=${CLIENT_BIN:-"./../bin/litentry-cli"}
+
+CLIENT="${CLIENT_BIN} -p ${NPORT} -P ${WORKER1PORT} -u ${NODEURL} -U ${WORKER1URL}"
+
+echo "Using client binary $CLIENT_BIN"
+echo "Using node uri $NODEURL:$NPORT"
+echo "Using trusted-worker uri $WORKER1URL:$WORKER1PORT"
+echo ""
+
+# apt-get install sudo
+# sudo apt-get install wget
+# sudo wget -qO /usr/local/bin/websocat https://github.com/vi/websocat/releases/latest/download/websocat.x86_64-unknown-linux-musl
+# sudo chmod a+x /usr/local/bin/websocat
+# websocat --version
+
+cd /client-api/parachain-api
+curl -s -H "Content-Type: application/json" -d '{"id": "1", "jsonrpc": "2.0", "method": "state_getMetadata", "params": []}' $NODEURL:$NPORT > prepare-build/litentry-parachain-metadata.json
+echo "update parachain metadata"
+
+# cd  /client-api/sidechain-api
+# ${CLIENT} print-sgx-metadata-raw > prepare-build/litentry-sidechain-metadata.json
+# echo "update sidechain metadata"
 
 cd /client-api
 pnpm install
 pnpm run build
+
+
+TEST=$T
 
 cd /ts-tests
 
