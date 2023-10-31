4710b2deead1d614ac90605f6d767533dfca3270
BillyWooo <thedreamofbilly@gmail.com>
2023-10-31 07:53:12 +0000

    change attribute name

--- tee-worker/cli/src/lib.rs
+++ tee-worker/cli/src/lib.rs
@@ -60,12 +60,12 @@ pub(crate) const ED25519_KEY_TYPE: KeyTypeId = KeyTypeId(*b"ed25");
 #[clap(name = "litentry-cli")]
 #[clap(version = VERSION)]
 #[clap(
-	author = "Trust Computing GmbH <info@litentry.com>, Integritee AG <hello@integritee.network>"
+	author = "Trust Computing GmbH <info@litentry.com>"
 )]
 #[clap(about = "cli tool to interact with litentry-parachain and workers", long_about = None)]
-#[cfg_attr(feature = "teeracle", clap(about = "interact with integritee-node and teeracle", long_about = None))]
-#[cfg_attr(feature = "sidechain", clap(about = "interact with integritee-node and sidechain", long_about = None))]
-#[cfg_attr(feature = "offchain-worker", clap(about = "interact with integritee-node and offchain-worker", long_about = None))]
+#[cfg_attr(feature = "teeracle", clap(about = "interact with litentry-parachain and teeracle", long_about = None))]
+#[cfg_attr(feature = "sidechain", clap(about = "interact with litentry-parachain and sidechain", long_about = None))]
+#[cfg_attr(feature = "offchain-worker", clap(about = "interact with litentry-parachain and offchain-worker", long_about = None))]
 #[clap(after_help = "stf subcommands depend on the stf crate this has been built against")]
 pub struct Cli {
 	/// node url
