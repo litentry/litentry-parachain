9a292be186c2f6bf6f1e75a9dc27cdc66c7f3189
Verin1005 <daqingchong0809@gmail.com>
2023-11-06 20:38:55 +0800

    test update-build

--- tee-worker/cli/lit_ts_api_package_build.sh
+++ tee-worker/cli/lit_ts_api_package_build.sh
@@ -6,48 +6,49 @@ set -euo pipefail
 
 # /usr/local/bin/litentry-cli print-sgx-metadata-raw
 
-# cd /client-api
-# pnpm install
-# pnpm run update-build:ci
-# echo "Client-api build is complete"
-# echo ""
-
-
-while getopts ":p:A:u:V:C:" opt; do
-    case $opt in
-        p)
-            NPORT=$OPTARG
-            ;;
-        A)
-            WORKER1PORT=$OPTARG
-            ;;
-        u)
-            NODEURL=$OPTARG
-            ;;
-        V)
-            WORKER1URL=$OPTARG
-            ;;
-        C)
-            CLIENT_BIN=$OPTARG
-            ;;
-    esac
-done
-
-# Using default port if none given as arguments.
-NPORT=${NPORT:-9944}
-NODEURL=${NODEURL:-"ws://127.0.0.1"}
-
-WORKER1PORT=${WORKER1PORT:-2000}
-WORKER1URL=${WORKER1URL:-"wss://127.0.0.1"}
-
-CLIENT_BIN=${CLIENT_BIN:-"./../bin/litentry-cli"}
-
-CLIENT="${CLIENT_BIN} -p ${NPORT} -P ${WORKER1PORT} -u ${NODEURL} -U ${WORKER1URL}"
-
-echo "Using client binary $CLIENT_BIN"
-echo "Using node uri $NODEURL:$NPORT"
-echo "Using trusted-worker uri $WORKER1URL:$WORKER1PORT"
+cd /client-api
+pnpm install
+pnpm run update-build
+echo "Client-api build is complete"
 echo ""
+
+
+
+# while getopts ":p:A:u:V:C:" opt; do
+#     case $opt in
+#         p)
+#             NPORT=$OPTARG
+#             ;;
+#         A)
+#             WORKER1PORT=$OPTARG
+#             ;;
+#         u)
+#             NODEURL=$OPTARG
+#             ;;
+#         V)
+#             WORKER1URL=$OPTARG
+#             ;;
+#         C)
+#             CLIENT_BIN=$OPTARG
+#             ;;
+#     esac
+# done
+
+# # Using default port if none given as arguments.
+# NPORT=${NPORT:-9944}
+# NODEURL=${NODEURL:-"ws://127.0.0.1"}
+
+# WORKER1PORT=${WORKER1PORT:-2000}
+# WORKER1URL=${WORKER1URL:-"wss://127.0.0.1"}
+
+# CLIENT_BIN=${CLIENT_BIN:-"./../bin/litentry-cli"}
+
+# CLIENT="${CLIENT_BIN} -p ${NPORT} -P ${WORKER1PORT} -u ${NODEURL} -U ${WORKER1URL}"
+
+# echo "Using client binary $CLIENT_BIN"
+# echo "Using node uri $NODEURL:$NPORT"
+# echo "Using trusted-worker uri $WORKER1URL:$WORKER1PORT"
+# echo ""
 
 # apt-get install sudo
 # sudo apt-get install wget
@@ -59,10 +60,9 @@ echo ""
 # echo '{"id":1,"jsonrpc":"2.0","method":"state_getMetadata","params":[]}' | /usr/local/bin/websocat -n1 -k -B 99999999 $NODEURL:$NPORT > prepare-build/litentry-parachain-metadata.json
 # echo "update parachain metadata"
 
-cd  /client-api/sidechain-api
-${CLIENT} print-sgx-metadata-raw > prepare-build/litentry-sidechain-metadata.json
+# cd  /client-api/sidechain-api
+# ${CLIENT} print-sgx-metadata-raw > prepare-build/litentry-sidechain-metadata.json
 echo "update sidechain metadata"
 
-cd /client-api
-pnpm install
+
 pnpm run build
