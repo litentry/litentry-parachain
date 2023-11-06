046ee5d80157acfdc97619fdd7861867b7632b3a
Verin1005 <daqingchong0809@gmail.com>
2023-11-06 21:07:07 +0800

    update shell

--- tee-worker/cli/lit_ts_api_package_build.sh
+++ tee-worker/cli/lit_ts_api_package_build.sh
@@ -4,9 +4,49 @@
 
 set -euo pipefail
 
+while getopts ":p:A:u:V:C:" opt; do
+    case $opt in
+        p)
+            NPORT=$OPTARG
+            ;;
+        A)
+            WORKER1PORT=$OPTARG
+            ;;
+        u)
+            NODEURL=$OPTARG
+            ;;
+        V)
+            WORKER1URL=$OPTARG
+            ;;
+        C)
+            CLIENT_BIN=$OPTARG
+            ;;
+    esac
+done
 
-cd /client-api
-pnpm install
-pnpm run update-build:ci
-echo "Client-api build is complete"
+# Using default port if none given as arguments.
+NPORT=${NPORT:-9944}
+NODEURL=${NODEURL:-"ws://127.0.0.1"}
+
+WORKER1PORT=${WORKER1PORT:-2000}
+WORKER1URL=${WORKER1URL:-"wss://127.0.0.1"}
+
+CLIENT_BIN=${CLIENT_BIN:-"./../bin/litentry-cli"}
+
+CLIENT="${CLIENT_BIN} -p ${NPORT} -P ${WORKER1PORT} -u ${NODEURL} -U ${WORKER1URL}"
+
+echo "Using client binary $CLIENT_BIN"
+echo "Using node uri $NODEURL:$NPORT"
+echo "Using trusted-worker uri $WORKER1URL:$WORKER1PORT"
 echo ""
+
+cd /client-api/parachain-api
+curl -s -H "Content-Type: application/json" -d '{"id": "1", "jsonrpc": "2.0", "method": "state_getMetadata", "params": []}' http://litentry-node:9912 > prepare-build/litentry-parachain-metadata.json
+echo "update parachain metadata"
+
+cd  /client-api/sidechain-api
+${CLIENT} print-sgx-metadata-raw > prepare-build/litentry-sidechain-metadata.json
+echo "update sidechain metadata"
+
+pnpm install
+pnpm run build
