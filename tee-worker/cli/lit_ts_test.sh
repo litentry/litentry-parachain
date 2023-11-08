555857876f1e0908bd1dde129e1194cedd325832
Verin1005 <daqingchong0809@gmail.com>
2023-11-08 14:57:27 +0800

    remove ls

--- tee-worker/cli/lit_ts_test.sh
+++ tee-worker/cli/lit_ts_test.sh
@@ -5,25 +5,15 @@
 set -euo pipefail
 
 function usage() {
-    echo "Usage: $0 <Options>"
     echo ""
-    echo "This is a script for tee-worker ts-test. Current available Options:"
-    echo "  test-ii-identity: "
-    echo "  test-ii-vc: "
-    echo "  test-resuming-worker: "
+    echo "This is a script for tee-worker ts-test.Preparing to test: $0"
     echo ""
-    echo "Please try to extend the above list when adding new ts-test."
+
 }
 
 [ $# -ne 1 ] && (usage; exit 1)
 TEST=$1
-ls
+
 cd /ts-tests
-
 pnpm install
-ls integration-tests/node_modules
-ls integration-tests/node_modules/parachain-api
-ls integration-tests/node_modules/sidechain-api
-
-
 pnpm --filter integration-tests run $TEST:staging
