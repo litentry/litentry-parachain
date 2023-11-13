4a55af950b1224b8614d43cb62e895e7e12c20bd
Verin1005 <daqingchong0809@gmail.com>
2023-11-13 14:59:47 +0800

    fix rename

--- tee-worker/client-api/parachain-api/src/index.ts
+++ tee-worker/client-api/parachain-api/src/index.ts
@@ -14,6 +14,6 @@ export * from "@polkadot/api";
 export * from "@polkadot/api/types";
 import { default as identity } from "../build/interfaces/identity/definitions";
 import { default as vc } from "../build/interfaces/vc/definitions";
-import { default as trustedCalls } from "../build/interfaces/trustedCalls/definitions";
+import { default as trustedCalls } from "../build/interfaces/trusted-calls/definitions";
 import { default as sidechain } from "../build/interfaces/sidechain/definitions";
 export { identity, vc, trustedCalls, sidechain };
