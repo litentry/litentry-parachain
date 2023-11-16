08ad856920b2204ff47f81a9b71c5a18471bca6e
Verin1005 <104152026+Verin1005@users.noreply.github.com>
2023-11-16 11:25:59 +0800

    Handling minor issues (#2244)
    * separate type-definitios
    
    * remove examples
    
    * change types import
    
    * fix import path
    
    * Rename `ethereum...` to `evm`
    
    * fix types import
    
    * fix comments
    
    * fix rename
    
    * fix renaming issue
    
    * organize merged types
    
    * delete shieldingKey-related types
    
    * fix comments
    
    * move ShardIdentifier

---
+++ tee-worker/client-api/parachain-api/prepare-build/interfaces/vc/definitions.ts
@@ -0,0 +1,33 @@
+export default {
+    types: {
+        VCRequested: {
+            account: "AccountId",
+            mrEnclave: "ShardIdentifier",
+            assertion: "Assertion",
+        },
+        Assertion: {
+            _enum: {
+                A1: "Null",
+                A2: "Bytes",
+                A3: "(Bytes,Bytes,Bytes)",
+                A4: "Bytes",
+                A6: "Null",
+                A7: "Bytes",
+                A8: "Vec<AssertionSupportedNetwork>",
+                A10: "Bytes",
+                A11: "Bytes",
+                A12: "Bytes",
+                A13: "AccountId32",
+                A14: "Null",
+            },
+        },
+        AssertionSupportedNetwork: {
+            _enum: ["Litentry", "Litmus", "LitentryRococo", "Polkadot", "Kusama", "Khala", "Ethereum", "TestNet"],
+        },
+        RequestVCResult: {
+            vc_index: "H256",
+            vc_hash: "H256",
+            vc_payload: "AesOutput",
+        },
+    },
+};
