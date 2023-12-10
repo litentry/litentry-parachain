0299002bcf7f486bada4befc3d4e41f206e66804
WMQ <wangminqi@aliyun.com>
2023-12-10 16:56:51 +0800

    debug: license

--- precompiles/utils/src/tests.rs
+++ precompiles/utils/src/tests.rs
@@ -1,24 +1,20 @@
-// This file is part of Astar.
-
-// Copyright 2019-2022 PureStake Inc.
-// Copyright (C) 2022-2023 Stake Technologies Pte.Ltd.
-// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright 2020-2023 Trust Computing GmbH.
+// This file is part of Litentry.
 //
-// This file is part of Utils package, originally developed by Purestake Inc.
-// Utils package used in Astar Network in terms of GPLv3.
-//
-// Utils is free software: you can redistribute it and/or modify
+// Litentry is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-
-// Utils is distributed in the hope that it will be useful,
+//
+// Litentry is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-
+//
 // You should have received a copy of the GNU General Public License
-// along with Utils.  If not, see <http://www.gnu.org/licenses/>.
+// along with Litentry.  If not, see <https://www.gnu.org/licenses/>.
+//
+// This file is part of Utils package, originally developed by Purestake/Astar Inc.
 
 use super::*;
 use hex_literal::hex;
