2429d7ad74d9f9556f076cf41d2139ce8c86a2d6
Igor Trofimov <igor@kawagarbo-tech.io>
2023-12-15 13:12:35 +0000

    cleaup

--- ts-tests/tests/precompile-contract.test.ts
+++ ts-tests/tests/precompile-contract.test.ts
@@ -2,13 +2,10 @@ import { assert, expect } from 'chai';
 import { step } from 'mocha-steps';
 import { AbiItem } from 'web3-utils';
 import { signAndSend, describeLitentry, loadConfig, sleep } from './utils';
-import { hexToU8a, hexToString, u8aToString, u8aToHex } from '@polkadot/util';
-import Keyring, { createPair, encodeAddress } from '@polkadot/keyring';
 import Web3 from 'web3';
-import ethers from 'ethers';
-import { compiled } from './compile';
 import precompileContractAbi from '../precompile/contracts/staking.json';
 import { mnemonicGenerate, mnemonicToMiniSecret, decodeAddress, evmToAddress } from '@polkadot/util-crypto';
+import { KeyringPair } from '@polkadot/keyring/types';
 
 const toBigNumber = (int: number) => int * 1e12;
 
@@ -20,6 +17,7 @@ describeLitentry('Test Parachain Precompile Contract', ``, (context) => {
         privateKey: '0x01ab6e801c06e59ca97a14fc0a1978b27fa366fc87450e0b65459dd3515b7391',
         address: '0xaaafB3972B05630fCceE866eC69CdADd9baC2771',
         mappedAddress: evmToAddress('0xaaafB3972B05630fCceE866eC69CdADd9baC2771', 31),
+        publicKey: '0x93eac2793cb6d9e837b0f8da1a63dbc0db2ca848c05cbe66db139157922f78f9',
     };
 
     // candidate: collator address: 5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY
@@ -30,7 +28,7 @@ describeLitentry('Test Parachain Precompile Contract', ``, (context) => {
     const precompileContract = new web3.eth.Contract(precompileContractAbi as AbiItem[], precompileContractAddress);
 
     const executeTransaction = async (delegateTransaction: any, label = '') => {
-        console.log('Executing transaction ', label);
+        console.log(`=== Executing ${label} ===`);
 
         // estimate gas doesn't work
         // const gas = await delegateTransaction.estimateGas();
@@ -44,43 +42,28 @@ describeLitentry('Test Parachain Precompile Contract', ``, (context) => {
             },
             evmAccountRaw.privateKey
         );
-        const result = await web3.eth.sendSignedTransaction(transaction.rawTransaction!);
-        console.log('Result: ', result);
-        return result;
+
+        return await web3.eth.sendSignedTransaction(transaction.rawTransaction!);
     };
 
     const printBalance = (label: string, bl: any) => {
-        console.group("====", label, "====");
-        console.log('free', bl.free.toNumber() / 1e12, 'reserved', bl.reserved.toNumber() / 1e12);
-        console.groupEnd();
+        console.log(label, 'free', bl.free.toNumber() / 1e12, 'reserved', bl.reserved.toNumber() / 1e12);
     };
 
-    const transferTokens = async (from: typeof context.alice, to: any) => {
-        const filterMode = (await context.api.query.extrinsicFilter.mode()).toHuman();
-        if ('Test' !== filterMode) {
-            let extrinsic = context.api.tx.sudo.sudo(context.api.tx.extrinsicFilter.setMode('Test'));
-            let temp = await context.api.rpc.chain.getBlock();
-            console.log(`setMode await Before: ${temp.block.header.number}`);
-            await signAndSend(extrinsic, context.alice);
-            temp = await context.api.rpc.chain.getBlock();
-            console.log(`setMode await end: ${temp.block.header.number}`);
-        }
-
+    // this function makes two transactions first one from token owner substrate account to owner evm mapped account
+    // and then it transfers to recepient
+    const transferTokens = async (from: KeyringPair, to: any) => {
         const aliceEVMMappedAccount = from.publicKey.slice(0, 20); // pretend to be evm
         console.log(`alice address: ${from.publicKey}`);
         console.log(`aliceEVMMappedAccount: ${aliceEVMMappedAccount}`);
 
         let aliceMappedSustrateAccount = evmToAddress(aliceEVMMappedAccount, 31);
 
-        console.log("transfer from Alice to alice EMV");
-        
-        // Deposit money into substrate account's truncated EVM address's mapping substrate account
-        const tx_init = context.api.tx.balances.transfer(aliceMappedSustrateAccount, 65 * 1e12);
-        await signAndSend(tx_init, context.alice);
-        console.log("transfered from Alice to alice EMV");
+        console.log('transfer from Alice to alice EMV');
 
-        // 1 - substrate Alice has 70 tokens
-        // 2 - alice need to turn to own evm address account
+        // Deposit money into substrate account's truncated EVM address's mapping substrate account
+        const tx_init = context.api.tx.balances.transfer(aliceMappedSustrateAccount, 70 * 1e12);
+        await signAndSend(tx_init, context.alice);
 
         // 25000 is min_gas_price setup
         const tx = context.api.tx.evm.call(
@@ -99,6 +82,15 @@ describeLitentry('Test Parachain Precompile Contract', ``, (context) => {
         await signAndSend(tx, from);
         let temp = await context.api.rpc.chain.getBlock();
         console.log(`evm call await end: ${temp.block.header.number}`);
+    };
+
+    const isPendingRequest = async () =>
+        await precompileContract.methods.delegationRequestIsPending(evmAccountRaw.publicKey, collatorPublicKey).call();
+
+    const collatorDetails = async () => {
+        const response = await context.api.query.parachainStaking.autoCompoundingDelegations(collatorPublicKey);
+        const collators = response.toJSON() as { address: string; value: number }[];
+        return collators[0];
     };
 
     step('Address with not sufficient amount of tokens', async function () {
@@ -129,106 +121,113 @@ describeLitentry('Test Parachain Precompile Contract', ``, (context) => {
 
     // To see full params types for the interfaces, check notion page: https://web3builders.notion.site/Parachain-Precompile-Contract-0c34929e5f16408084446dcf3dd36006
     step('Test precompile contract', async function () {
-        const { data } = await context.api.query.system.account(evmAccountRaw.mappedAddress);
-        printBalance('initial balance', data);
-        // top up LITs if not sufficient amount for staking (require: 50 LITs minimum)
-        if (data.free.toNumber() < toBigNumber(60)) {
+        const filterMode = (await context.api.query.extrinsicFilter.mode()).toHuman();
+        if ('Test' !== filterMode) {
+            let extrinsic = context.api.tx.sudo.sudo(context.api.tx.extrinsicFilter.setMode('Test'));
+            let temp = await context.api.rpc.chain.getBlock();
+            console.log(`setMode await Before: ${temp.block.header.number}`);
+            await signAndSend(extrinsic, context.alice);
+            temp = await context.api.rpc.chain.getBlock();
+            console.log(`setMode await end: ${temp.block.header.number}`);
+        }
+
+        let balance = (await context.api.query.system.account(evmAccountRaw.mappedAddress)).data;
+        printBalance('initial balance', balance);
+
+        // top up LITs if not sufficient amount for staking or they are not reserved (require: 50 LITs minimum)
+        if (balance.free.toNumber() < toBigNumber(60) && balance.reserved.toNumber() === 0) {
             console.log('transferring more tokens');
 
             await transferTokens(context.alice, evmAccountRaw);
-            printBalance('after balance balance', (await context.api.query.system.account(evmAccountRaw.mappedAddress)).data);
+
+            balance = (await context.api.query.system.account(evmAccountRaw.mappedAddress)).data;
+            printBalance('balance after transferring', balance);
         }
 
         //// TESTS
+        const autoCompoundPercent = 20;
 
-        // const isPendingRequest = await precompileContract.methods
-        //     .delegationRequestIsPending(evmAccountRaw.mappedAddress, collatorPublicKey)
-        //     .call();
-
-        // console.log('pending staking', isPendingRequest);
-
-
-        // // cancelDelegationRequest(collator)
-        // const cancelDelegationRequest = precompileContract.methods.cancelDelegationRequest(collatorPublicKey);
-        // await executeTransaction(cancelDelegationRequest, 'cancelDelegationRequest');
-        // const { data: balanceAfterCancel } = await context.api.query.system.account(evmAccountRaw.mappedAddress);
-        // printBalance('balanceAfterCancel', balanceAfterCancel);
-
-
-        // const shouldBeFalse = await precompileContract.methods
-        //     .delegationRequestIsPending(evmAccountRaw.mappedAddress, collatorPublicKey)
-        //     .call();
-
-        // console.log('should be false', shouldBeFalse);
         // delegateWithAutoCompound(collator, amount, percent)
         const delegateWithAutoCompound = precompileContract.methods.delegateWithAutoCompound(
             collatorPublicKey,
             toBigNumber(60),
-            20
+            autoCompoundPercent
         );
-        // construct transaction
-        await executeTransaction(delegateWithAutoCompound, 'delegateWithAutoCompound');
-        const { data: balance } = await context.api.query.system.account(evmAccountRaw.mappedAddress);
-        printBalance('balance', balance);
-        // expect(balance.free.toNumber()).to.lt(balance.free.toNumber() - toBigNumber(60));
-        // expect(balance.reserved.toNumber()).to.eq(toBigNumber(60));
+
+        // skip test if already delegated
+        if (balance.reserved.toNumber() === 0) {
+            await executeTransaction(delegateWithAutoCompound, 'delegateWithAutoCompound');
+            const { data: afterDelegateBalance } = await context.api.query.system.account(evmAccountRaw.mappedAddress);
+
+            expect(balance.free.toNumber() - toBigNumber(60)).to.closeTo(
+                afterDelegateBalance.free.toNumber(),
+                toBigNumber(1)
+            );
+            expect(afterDelegateBalance.reserved.toNumber()).to.eq(toBigNumber(60));
+            const collator = await collatorDetails();
+            expect(collator.value).to.eq(autoCompoundPercent);
+
+            balance = afterDelegateBalance;
+        }
 
         // delegatorBondMore(collator, amount)
         const delegatorBondMore = precompileContract.methods.delegatorBondMore(collatorPublicKey, toBigNumber(1));
         await executeTransaction(delegatorBondMore, 'delegatorBondMore');
 
         const { data: balanceAfterBondMore } = await context.api.query.system.account(evmAccountRaw.mappedAddress);
-        // expect(balanceAfterBondMore.free.toNumber()).to.eq(balanceAfterBondMore.free.toNumber() - toBigNumber(1));
-        // expect(balanceAfterBondMore.reserved.toNumber()).to.eq(toBigNumber(61));
-        printBalance('balanceAfterBondMore', balanceAfterBondMore);
+        expect(balanceAfterBondMore.free.toNumber()).to.closeTo(
+            balanceAfterBondMore.free.toNumber() - toBigNumber(1),
+            toBigNumber(1)
+        );
+        expect(balanceAfterBondMore.reserved.toNumber()).to.eq(balance.reserved.toNumber() + toBigNumber(1));
 
-        // Ask minqi should it be triggered after execute
+        // setAutoCompound(collator, percent);
+        const setAutoCompound = precompileContract.methods.setAutoCompound(collatorPublicKey, autoCompoundPercent + 5);
+        await executeTransaction(setAutoCompound, 'setAutoCompound');
+        const collatorAfterCompound = await collatorDetails();
+        expect(collatorAfterCompound.value).to.eq(autoCompoundPercent + 5);
+
         // scheduleDelegatorBondLess(collator, amount)
+        expect(await isPendingRequest()).to.be.false;
         const scheduleDelegatorBondLess = precompileContract.methods.scheduleDelegatorBondLess(
             collatorPublicKey,
             toBigNumber(5)
         );
         await executeTransaction(scheduleDelegatorBondLess, 'scheduleDelegatorBondLess');
-        const { data: balanceAfterBondLess } = await context.api.query.system.account(evmAccountRaw.mappedAddress);
-        printBalance('balanceAfterBondLess', balanceAfterBondLess);
+        expect(await isPendingRequest()).to.be.true;
 
         // cancelDelegationRequest(collator)
         const cancelDelegationRequest = precompileContract.methods.cancelDelegationRequest(collatorPublicKey);
+        expect(await isPendingRequest()).to.be.true;
         await executeTransaction(cancelDelegationRequest, 'cancelDelegationRequest');
-        const { data: balanceAfterCancel } = await context.api.query.system.account(evmAccountRaw.mappedAddress);
-        printBalance('balanceAfterCancel', balanceAfterCancel);
+        expect(await isPendingRequest()).to.be.false;
 
-        // delegate(collator, amount)
-        const delegate = precompileContract.methods.delegate(collatorPublicKey, toBigNumber(55));
-        await executeTransaction(delegate, 'delegate');
-        const { data: balanceAfterDelegate } = await context.api.query.system.account(evmAccountRaw.mappedAddress);
-        printBalance('balanceAfterDelegate', balanceAfterDelegate);
-
-        // setAutoCompound(collator, percent)
-        const setAutoCompound = precompileContract.methods.setAutoCompound(collatorPublicKey);
-        await executeTransaction(setAutoCompound, 'setAutoCompound');
-        
-        // scheduleRevokeDelegation(collator)
-        const scheduleRevokeDelegation = precompileContract.methods.scheduleRevokeDelegation(collatorPublicKey);
-        await executeTransaction(scheduleRevokeDelegation, 'scheduleRevokeDelegation');
-        const { data: balanceAfterRevoke } = await context.api.query.system.account(evmAccountRaw.mappedAddress);
-        printBalance('balanceAfterRevoke', balanceAfterRevoke);
-
-        // executeDelegationRequest(delegator, collator)
-        const executeDelegationRequest = precompileContract.methods.executeDelegationRequest(
-            evmAccountRaw.mappedAddress,
-            collatorPublicKey
-        );
-        await executeTransaction(executeDelegationRequest, 'executeDelegationRequest');
-        const { data: balanceAfterExecuteDelegation } = await context.api.query.system.account(
-            evmAccountRaw.mappedAddress
-        );
-        printBalance('balanceAfterExecuteDelegation', balanceAfterExecuteDelegation);
-
-        const stakedResult = await precompileContract.methods
-            .delegationRequestIsPending(evmAccountRaw.mappedAddress, collatorPublicKey)
-            .call();
-
-        console.log('pending', stakedResult);
+        // In case evm is not enabled in Normal Mode, switch back to filterMode, after test.
+        let extrinsic = context.api.tx.sudo.sudo(context.api.tx.extrinsicFilter.setMode(filterMode));
+        await signAndSend(extrinsic, context.alice);
     });
+
+    // once we add ability to customise minimum waiting time for `executeDelegationRequest` implement tests for the rest functions
+    // step('test time sensitive functions', async () => {
+    //     delegate(collator, amount);
+    //     const delegate = precompileContract.methods.delegate(collatorPublicKey, toBigNumber(55));
+    //     await executeTransaction(delegate, 'delegate');
+    //     const { data: balanceAfterDelegate } = await context.api.query.system.account(evmAccountRaw.mappedAddress);
+    //     printBalance('balanceAfterDelegate', balanceAfterDelegate);
+    //     scheduleRevokeDelegation(collator);
+    //     const scheduleRevokeDelegation = precompileContract.methods.scheduleRevokeDelegation(collatorPublicKey);
+    //     await executeTransaction(scheduleRevokeDelegation, 'scheduleRevokeDelegation');
+    //     const { data: balanceAfterRevoke } = await context.api.query.system.account(evmAccountRaw.mappedAddress);
+    //     printBalance('balanceAfterRevoke', balanceAfterRevoke);
+    //     executeDelegationRequest(delegator, collator);
+    //     const executeDelegationRequest = precompileContract.methods.executeDelegationRequest(
+    //         evmAccountRaw.mappedAddress,
+    //         collatorPublicKey
+    //     );
+    //     await executeTransaction(executeDelegationRequest, 'executeDelegationRequest');
+    //     const { data: balanceAfterExecuteDelegation } = await context.api.query.system.account(
+    //         evmAccountRaw.mappedAddress
+    //     );
+    //     printBalance('balanceAfterExecuteDelegation', balanceAfterExecuteDelegation);
+    // });
 });
