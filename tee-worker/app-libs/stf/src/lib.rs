bd40b3b445fd778955d710569a18f8d0c34014ef
felixfaisal <faisalahmedfarooq46@gmail.com>
2023-12-15 06:39:51 +0000

    fix: update const scope

--- tee-worker/app-libs/stf/src/lib.rs
+++ tee-worker/app-libs/stf/src/lib.rs
@@ -48,7 +48,7 @@ pub mod trusted_call;
 pub mod trusted_call_litentry;
 pub mod trusted_call_result;
 
-pub(crate) const ENCLAVE_ACCOUNT_KEY: &str = "Enclave_Account_Key";
+pub const ENCLAVE_ACCOUNT_KEY: &str = "Enclave_Account_Key";
 
 // fixme: this if  a temporary hack only
 pub const STF_TX_FEE: Balance = 100000000;
