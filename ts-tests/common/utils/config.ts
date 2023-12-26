9c0a6ef221ef2947ac342e05ffd678da813b4eb2
Verin1005 <daqingchong0809@gmail.com>
2023-12-26 19:46:38 +0800

    fix path issue

--- ts-tests/common/utils/config.ts
+++ ts-tests/common/utils/config.ts
@@ -2,12 +2,12 @@ export function loadConfig() {
     require('dotenv').config();
     switch (process.env.NODE_ENV) {
         case 'local':
-            return require('../config.local.json');
+            return require('../../config.local.json');
         case 'test':
         case 'ci':
-            return require('../config.ci.json');
+            return require('../../config.ci.json');
         case 'staging':
-            return require('../config.staging.json');
+            return require('../../config.staging.json');
         default:
             throw new Error(`Invalid NODE_ENV: ${process.env.NODE_ENV}`);
     }
