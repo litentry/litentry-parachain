5db723ad083dc23ede5e2d4098372d5681ac1019
Verin1005 <daqingchong0809@gmail.com>
2023-12-28 01:05:58 +0800

    debug clean-local-docker.sh

--- scripts/clean-local-docker.sh
+++ scripts/clean-local-docker.sh
@@ -40,7 +40,10 @@ IMG=$(docker images --filter=reference="generated-$CHAIN*" --format "{{.Reposito
 [ -z "$IMG" ] || docker rmi -f $IMG
 
 rm -rf "$LITENTRY_PARACHAIN_DIR"
-rm -rf "$ROOTDIR/ts-tests/bridge/bob.json"
-rm -rf "$ROOTDIR/ts-tests/bridge/data/"
-
+ls $ROOTDIR/ts-tests
+echo "remove ts-tests/common/bob.json and ts-tests/common/data/..."
+rm -rf "$ROOTDIR/ts-tests/common/bob.json"
+rm -rf "$ROOTDIR/ts-tests/common/data/"
+echo "remove ts-tests/..."
+ls $ROOTDIR/ts-tests
 echo "cleaned up."
