aaff9885aeac002d43fe905a7d121d63a72f9c67
BillyWooo <thedreamofbilly@gmail.com>
2024-01-15 12:18:29 +0100

    bug fix: compose teerex::register_enclave extrinsics; use intel 'dev' server for attestation temporary; add one example config file (#2400)

--- tee-worker/core-primitives/attestation-handler/src/attestation_handler.rs
+++ tee-worker/core-primitives/attestation-handler/src/attestation_handler.rs
@@ -65,10 +65,11 @@ use std::{
 
 pub const DEV_HOSTNAME: &str = "api.trustedservices.intel.com";
 
+// Litentry TODO: use `dev` for production temporary. Will switch to dcap later.
 #[cfg(feature = "production")]
-pub const SIGRL_SUFFIX: &str = "/sgx/attestation/v4/sigrl/";
+pub const SIGRL_SUFFIX: &str = "/sgx/dev/attestation/v4/sigrl/";
 #[cfg(feature = "production")]
-pub const REPORT_SUFFIX: &str = "/sgx/attestation/v4/report";
+pub const REPORT_SUFFIX: &str = "/sgx/dev/attestation/v4/report";
 
 #[cfg(not(feature = "production"))]
 pub const SIGRL_SUFFIX: &str = "/sgx/dev/attestation/v4/sigrl/";
