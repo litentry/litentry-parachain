0c1befd00feede5af270861f3b873cdbbf75d5cb
Kasper Ziemianek <kasper.ziemianek@gmail.com>
2024-01-17 14:56:07 +0100

    remove problematic function

--- tee-worker/ts-tests/worker/resuming_worker.test.ts
+++ tee-worker/ts-tests/worker/resuming_worker.test.ts
@@ -93,27 +93,6 @@ type RetryConfig = {
     backoffFactor: number;
 };
 
-async function withRetry<T>(
-    task: () => Promise<T>,
-    { isRetriable, maxRetries, initialDelaySeconds, backoffFactor }: RetryConfig
-): Promise<T> {
-    let attempt = 0;
-    let delaySeconds = initialDelaySeconds;
-    // eslint-disable-next-line no-constant-condition
-    while (true) {
-        try {
-            return await task();
-        } catch (err) {
-            if (attempt > maxRetries || !isRetriable(err)) {
-                throw err;
-            }
-            attempt += 1;
-            await sleep(delaySeconds);
-            delaySeconds *= backoffFactor;
-        }
-    }
-}
-
 type JobConfig = {
     workerConfig: WorkerConfig;
     nodeConfig: NodeConfig;
