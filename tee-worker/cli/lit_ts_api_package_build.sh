b86b527674569e073d3c3cf43c65afa4bf2655c6
Igor Trofimov <igor@kawagarbo-tech.io>
2024-01-19 10:49:23 +0000

    add meaningful comment

--- tee-worker/cli/lit_ts_api_package_build.sh
+++ tee-worker/cli/lit_ts_api_package_build.sh
@@ -42,7 +42,7 @@ echo "Using client binary $CLIENT_BIN"
 echo "Using node uri $NODEURL:$NPORT"
 echo "Using trusted-worker uri $WORKER1URL:$WORKER1PORT"
 echo "Using node http uri $NODEHTTPURL:$NPORT"
-echo "sleeping for 20 secs IGOR DEBUG"
+echo "waiting 20 secs worker to run successfully"
 sleep 20
 cd /client-api/parachain-api
 curl -s -H "Content-Type: application/json" -d '{"id": "1", "jsonrpc": "2.0", "method": "state_getMetadata", "params": []}' $NODEHTTPURL:$NPORT > prepare-build/litentry-parachain-metadata.json
