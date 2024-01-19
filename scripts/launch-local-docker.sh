d170941f12ddfe9a1505380ccfb57a1b3882b70e
felixfaisal <faisalahmedfarooq46@gmail.com>
2024-01-19 07:35:27 +0000

    fix: update launch-local-docker script

--- scripts/launch-local-docker.sh
+++ scripts/launch-local-docker.sh
@@ -75,9 +75,9 @@ for _ in $(seq 1 $WAIT_ROUNDS); do
     pnpm run upgrade-parathread 2>&1
     if [[ "$1" = "rococo" ]]; then
       pnpm run skip-schedule-enclave-check 2>&1 | tee "$LITENTRY_PARACHAIN_DIR/skip-schedule-enclave-check.log"
+      print_divider
+
     fi
-    pnpm run skip-schedule-enclave-check 2>&1 | tee "$LITENTRY_PARACHAIN_DIR/skip-schedule-enclave-check.log"
-    print_divider
     echo "Done."
     exit 0
   fi
