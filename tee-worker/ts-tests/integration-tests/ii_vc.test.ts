d0759fb60703c89186357bd0b9c53c8df374422c
Kasper Ziemianek <kasper.ziemianek@gmail.com>
2024-01-19 15:48:10 +0100

    Use whole unsigned VC as VC proof signature payload (#2404)
    * Use whole unsigned VC as VC proof signature payload
    
    * fix fmt
    
    * fix ii-vc ts-test
    
    * improve test
    
    * remove VCRegistry and events
    
    * [benchmarking bot] Auto commit generated weights files (#2409)
    
    Co-authored-by: kziemianek <kziemianek@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: BillyWooo <thedreamofbilly@gmail.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: kziemianek <kziemianek@users.noreply.github.com>

--- tee-worker/ts-tests/integration-tests/ii_vc.test.ts
+++ tee-worker/ts-tests/integration-tests/ii_vc.test.ts
@@ -1,8 +1,7 @@
-import { describeLitentry, handleVcEvents } from './common/utils';
+import { describeLitentry } from './common/utils';
 import { step } from 'mocha-steps';
-import type { HexString } from '@polkadot/util/types';
 import { assert } from 'chai';
-import { sendTxsWithUtility, sendTxUntilInBlockList } from './common/transactions';
+import { sendTxsWithUtility } from './common/transactions';
 import { ApiTypes, SubmittableExtrinsic } from '@polkadot/api/types';
 
 // TODO: keep the list short, the manual types will be solved in #1878
@@ -16,7 +15,6 @@ const allAssertions = [
 // It doesn't make much difference test A1 only vs test A1 - A11, one VC type is enough.
 // So only use A1 to trigger the wrong event
 describeLitentry('VC ii test', async (context) => {
-    const indexList: HexString[] = [];
     step('Request VC', async () => {
         // request all vc
         const txs: {
@@ -36,80 +34,6 @@ describeLitentry('VC ii test', async (context) => {
             ['VCIssued'],
             30
         );
-        const res = await handleVcEvents(events, 'VCIssued');
-
-        for (let k = 0; k < res.length; k++) {
-            const registry = (await context.api.query.vcManagement.vcRegistry(res[k].index)) as any;
-            assert.equal(registry.toHuman()!['status'], 'Active', 'check registry error');
-            indexList.push(res[k].index);
-        }
-    });
-    step('Disable VC', async () => {
-        const txs: {
-            tx: SubmittableExtrinsic<ApiTypes>;
-        }[] = [];
-        for (let i = 0; i < indexList.length; i++) {
-            const tx = context.api.tx.vcManagement.disableVc(indexList[i]);
-            txs.push({ tx });
-        }
-        const events = await sendTxsWithUtility(context, context.substrateWallet.alice, txs, 'vcManagement', [
-            'VCDisabled',
-        ]);
-        const res = await handleVcEvents(events, 'VCDisabled');
-
-        for (let k = 0; k < res.length; k++) {
-            assert.equal(res[k], indexList[k], 'check index error');
-            const registry = (await context.api.query.vcManagement.vcRegistry(indexList[k])) as any;
-            assert.equal(registry.toHuman()!['status'], 'Disabled');
-        }
-    });
-    step('Disable error VC(A1)', async () => {
-        // Alice has already disabled the A1 VC
-        const tx = context.api.tx.vcManagement.disableVc(indexList[0]);
-        const nonce = (await context.api.rpc.system.accountNextIndex(context.substrateWallet.alice.address)).toNumber();
-
-        const [error] = await sendTxUntilInBlockList(context.api, [{ tx, nonce }], context.substrateWallet.alice);
-
-        assert.equal(
-            error,
-            'vcManagement.VCAlreadyDisabled',
-            'check disable vc error: error should be equal to vcManagement.VCAlreadyDisabled'
-        );
-    });
-
-    step('Revoke VC', async () => {
-        const txs: {
-            tx: SubmittableExtrinsic<ApiTypes>;
-        }[] = [];
-
-        for (let i = 0; i < indexList.length; i++) {
-            const tx = context.api.tx.vcManagement.revokeVc(indexList[i]);
-            txs.push({ tx });
-        }
-        const events = await sendTxsWithUtility(context, context.substrateWallet.alice, txs, 'vcManagement', [
-            'VCRevoked',
-        ]);
-
-        const res = await handleVcEvents(events, 'VCRevoked');
-
-        for (let k = 0; k < indexList.length; k++) {
-            assert.equal(res[k], indexList[k], 'check index error');
-            const registry = (await context.api.query.vcManagement.vcRegistry(indexList[k])) as any;
-
-            assert.equal(registry.toHuman(), null);
-        }
-    });
-
-    step('Revoke Error VC(A1)', async () => {
-        // Alice has already revoked the A1 VC
-        const tx = context.api.tx.vcManagement.revokeVc(indexList[0]);
-        const nonce = (await context.api.rpc.system.accountNextIndex(context.substrateWallet.alice.address)).toNumber();
-        const [error] = await sendTxUntilInBlockList(context.api, [{ tx, nonce }], context.substrateWallet.alice);
-
-        assert.equal(
-            error,
-            'vcManagement.VCNotExist',
-            'check revoke vc error: error should be equal to vcManagement.VCNotExist'
-        );
+        assert.equal(events.length, 1);
     });
 });
