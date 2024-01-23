481dc2cb79161700c5a6719f1a7e8be55775b412
Kasper Ziemianek <kasper.ziemianek@gmail.com>
2024-01-23 09:33:58 +0100

    more and more

--- tee-worker/litentry/core/assertion-build/src/dynamic/contracts/IsAgeOver.sol
+++ tee-worker/litentry/core/assertion-build/src/dynamic/contracts/IsAgeOver.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.8;
 
 import {DynamicAssertion} from "DynamicAssertion.sol";
-import {Identity, IdentityUtils} from "Identity.sol";
+import "./Identity.sol";
 
 contract IsAgeOver is DynamicAssertion {
     function doExecute(Identity[] memory identities)
@@ -19,7 +19,7 @@ contract IsAgeOver is DynamicAssertion {
         string memory url = "https://dummy.restapiexample.com/api/v1/employees";
         string memory pointer = "/data/3/employee_age";
 
-        int64 age = GetI64(url, pointer);
+        // int64 age = GetI64(url, pointer);
         string memory description = "Is the employee over 50 years old ?";
         string memory assertion_type = "Is over 50";
         string memory assertion = "age > 50";
