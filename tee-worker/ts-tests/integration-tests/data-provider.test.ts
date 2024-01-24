56034732934cbd32fbe250ad1d8374626e09cd39
Verin1005 <daqingchong0809@gmail.com>
2024-01-24 12:20:25 +0000

    remove vipp3 test file

--- tee-worker/ts-tests/integration-tests/data-provider.test.ts
+++ tee-worker/ts-tests/integration-tests/data-provider.test.ts
@@ -15,16 +15,17 @@ import type { IntegrationTestContext } from './common/common-types';
 import { CorePrimitivesIdentity } from 'parachain-api';
 import { LitentryValidationData, Web3Network } from 'parachain-api';
 import { Vec } from '@polkadot/types';
-
+import { spawn } from 'child_process';
+import { aesKey } from './common/call';
 
 const assertion = {
-    VIP3MembershipCard:'gold'
-}
+    VIP3MembershipCard: 'gold',
+};
 describe('Test Vc (direct invocation)', function () {
     let context: IntegrationTestContext = undefined as any;
     let teeShieldingKey: KeyObject = undefined as any;
     let aliceSubstrateIdentity: CorePrimitivesIdentity = undefined as any;
-    // const client= process.env.BINARY_DIR+'/litentry-cli';
+    const client = process.env.BINARY_DIR + '/litentry-cli';
     // Alice links:
     // - a `mock_user` twitter
     // - alice's evm identity
@@ -51,34 +52,59 @@ describe('Test Vc (direct invocation)', function () {
         );
     });
 
-    step(`create idGrapgh via cli`, async function () { 
+    step(`create idGrapgh via cli`, async function () {
+        //         console.log(process.argv);
 
-        // console.log(client);
+        //       const evmIndex = process.argv.indexOf('--evm');
+        // if (evmIndex > -1) {
+        //   const evmAddress = process.argv[evmIndex + 1];
+        //   console.log(evmAddress);
+        // }
+
         
+        // todo: check idgrpah ,if exist, skip, else create
+        const aliceAddressFormat = '0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d';
+        const command = `${client}`;
+        const args = [
+            'trusted',
+            '-d',
+            'link-identity',
+            `did: litentry: substrate:${aliceAddressFormat}`,
+            'did: litentry: evm: 0x651614cA9097C5ba189Ef85e7851Ef9cff592B2c',
+        ];
+
+        const child = spawn(command, args);
+        // console.log(child);
+
+        child.on('error', (error) => {
+            console.error(`spawn error: ${error}`);
+        });
+        child.on('close', (code) => {
+            console.log(`child process exited with code ${code}`);
+        });
     });
+
     step(`request vc`, async function () {
-        console.log(aliceSubstrateIdentity.toHuman());
-        
-        // let currentNonce = (await getSidechainNonce(context, teeShieldingKey, aliceSubstrateIdentity)).toNumber();
-        // const getNextNonce = () => currentNonce++;
-        // const nonce = getNextNonce();
-        // const requestIdentifier = `0x${randomBytes(32).toString('hex')}`;
-        // // console.log(`request vc ${Object.keys(assertion)[0]} for Alice ... Assertion description: ${description}`);
+        let currentNonce = (await getSidechainNonce(context, teeShieldingKey, aliceSubstrateIdentity)).toNumber();
+        const getNextNonce = () => currentNonce++;
+        const nonce = getNextNonce();
+        const requestIdentifier = `0x${randomBytes(32).toString('hex')}`;
+        // console.log(`request vc ${Object.keys(assertion)[0]} for Alice ... Assertion description: ${description}`);
         // const eventsPromise = subscribeToEventsWithExtHash(requestIdentifier, context);
 
-        // const requestVcCall = await createSignedTrustedCallRequestVc(
-        //     context.api,
-        //     context.mrEnclave,
-        //     context.api.createType('Index', nonce),
-        //     new PolkadotSigner(context.substrateWallet.alice),
-        //     aliceSubstrateIdentity,
-        //     context.api.createType('Assertion', assertion).toHex(),
-        //     context.api.createType('Option<RequestAesKey>', aesKey).toHex(),
-        //     requestIdentifier
-        // );
+        const requestVcCall = await createSignedTrustedCallRequestVc(
+            context.api,
+            context.mrEnclave,
+            context.api.createType('Index', nonce),
+            new PolkadotSigner(context.substrateWallet.alice),
+            aliceSubstrateIdentity,
+            context.api.createType('Assertion', assertion).toHex(),
+            context.api.createType('Option<RequestAesKey>', aesKey).toHex(),
+            requestIdentifier
+        );
 
-        // const res = await sendRequestFromTrustedCall(context, teeShieldingKey, requestVcCall);
-        // await assertIsInSidechainBlock(`${Object.keys(assertion)[0]} requestVcCall`, res);
+        const res = await sendRequestFromTrustedCall(context, teeShieldingKey, requestVcCall);
+        await assertIsInSidechainBlock(`${Object.keys(assertion)[0]} requestVcCall`, res);
         // const events = await eventsPromise;
         // const vcIssuedEvents = events
         //     .map(({ event }) => event)
@@ -89,6 +115,6 @@ describe('Test Vc (direct invocation)', function () {
         //     1,
         //     `vcIssuedEvents.length != 1, please check the ${Object.keys(assertion)[0]} call`
         // );
-        // await assertVc(context, aliceSubstrateIdentity, res.value);
-    })
+        await assertVc(context, aliceSubstrateIdentity, res.value);
+    });
 });
