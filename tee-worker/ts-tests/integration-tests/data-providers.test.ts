10292b521276858340bdd25f17a55651773d364e
Verin1005 <daqingchong0809@gmail.com>
2024-01-26 17:41:12 +0000

    debug local

--- tee-worker/ts-tests/integration-tests/data-providers.test.ts
+++ tee-worker/ts-tests/integration-tests/data-providers.test.ts
@@ -65,6 +65,10 @@ describe('Test Vc (direct invocation)', function () {
         const eventsPromise = subscribeToEventsWithExtHash(reqExtHash, context);
         try {
             // CLIENT = "$CLIENT_BIN -p $NPORT -P $WORKER1PORT -u $NODEURL -U $WORKER1URL"
+
+            await zx`ls /usr/local/`
+            await zx`ls /usr/local/bin`
+
             const commandPromise = zx`${clientDir} -p ${nodePort} -P ${workerPort} -u ${
                 nodeProtocal + nodeHostname
             } -U ${workerProtocal + workerHostname}\
