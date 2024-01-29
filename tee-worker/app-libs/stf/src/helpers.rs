425bcb238f4b213723c350282fe0b6c651ff5162
Kailai Wang <Kailai.Wang@hotmail.com>
2024-01-29 19:44:36 +0000

    some more fix

--- tee-worker/app-libs/stf/src/helpers.rs
+++ tee-worker/app-libs/stf/src/helpers.rs
@@ -17,7 +17,6 @@
 use crate::ENCLAVE_ACCOUNT_KEY;
 use codec::{Decode, Encode};
 use frame_support::ensure;
-use hex_literal::hex;
 use itp_stf_primitives::error::{StfError, StfResult};
 use itp_storage::{storage_double_map_key, storage_map_key, storage_value_key, StorageHasher};
 use itp_types::Index;
@@ -25,12 +24,10 @@ use itp_utils::stringify::account_id_to_string;
 use litentry_primitives::{ErrorDetail, Identity, Web3ValidationData};
 use log::*;
 use sp_core::blake2_256;
-use sp_runtime::AccountId32;
 use std::prelude::v1::*;
 
 #[cfg(not(feature = "production"))]
-pub const ALICE_ACCOUNTID32: AccountId32 =
-	AccountId32::new(hex!["d43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d"]);
+pub use non_prod::*;
 
 pub fn get_storage_value<V: Decode>(
 	storage_prefix: &'static str,
@@ -129,16 +126,6 @@ pub fn ensure_enclave_signer_or_self<AccountId: Encode + Decode + PartialEq>(
 	}
 }
 
-#[cfg(not(feature = "production"))]
-pub fn ensure_alice(signer: &AccountId32) -> bool {
-	signer == &ALICE_ACCOUNTID32
-}
-
-#[cfg(not(feature = "production"))]
-pub fn ensure_enclave_signer_or_alice(signer: &AccountId32) -> bool {
-	signer == &enclave_signer_account::<AccountId32>() || ensure_alice(signer)
-}
-
 // verification message format:
 // ```
 // blake2_256(<sidechain nonce> + <primary account> + <identity-to-be-linked>)
@@ -173,4 +160,22 @@ pub fn verify_web3_identity(
 	);
 
 	Ok(())
+}
+
+#[cfg(not(feature = "production"))]
+mod non_prod {
+	use super::*;
+	use hex_literal::hex;
+	use sp_runtime::AccountId32;
+
+	pub const ALICE_ACCOUNTID32: AccountId32 =
+		AccountId32::new(hex!["d43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d"]);
+
+	pub fn ensure_alice(signer: &AccountId32) -> bool {
+		signer == &ALICE_ACCOUNTID32
+	}
+
+	pub fn ensure_enclave_signer_or_alice(signer: &AccountId32) -> bool {
+		signer == &enclave_signer_account::<AccountId32>() || ensure_alice(signer)
+	}
 }
