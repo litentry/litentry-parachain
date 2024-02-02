04016d2e2fe0b43671259f59cbba7b74faf8eff1
Kai <7630809+Kailai-Wang@users.noreply.github.com>
2024-02-02 18:18:33 +0100

    Refactor teerex/sidechain pallet into teebag (#2459)
    * init
    
    * init refactoring
    
    * rococo runtime compiles
    
    * add tests
    
    * add sidchain methods
    
    * small update
    
    * fix tests
    
    * Remove ScheduledEnclaveNotApplicable
    
    * remove comment

---
+++ pallets/teebag/src/mock.rs
@@ -0,0 +1,185 @@
+// Copyright 2020-2024 Trust Computing GmbH.
+// This file is part of Litentry.
+//
+// Litentry is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Litentry is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Litentry.  If not, see <https://www.gnu.org/licenses/>.
+
+#![allow(dead_code, unused_imports, const_item_mutation)]
+use crate::{self as pallet_teebag, OperationalMode};
+use frame_support::{
+	assert_ok, construct_runtime,
+	pallet_prelude::GenesisBuild,
+	parameter_types,
+	traits::{OnFinalize, OnInitialize},
+};
+use frame_system as system;
+use frame_system::EnsureRoot;
+use sp_core::H256;
+use sp_keyring::AccountKeyring;
+use sp_runtime::{
+	generic,
+	traits::{BlakeTwo256, IdentifyAccount, IdentityLookup, Verify},
+};
+
+pub type Signature = sp_runtime::MultiSignature;
+pub type AccountId = <<Signature as Verify>::Signer as IdentifyAccount>::AccountId;
+pub type Address = sp_runtime::MultiAddress<AccountId, ()>;
+
+pub type BlockNumber = u32;
+pub type Header = generic::Header<BlockNumber, BlakeTwo256>;
+pub type Block = generic::Block<Header, UncheckedExtrinsic>;
+pub type UncheckedExtrinsic =
+	generic::UncheckedExtrinsic<Address, RuntimeCall, Signature, SignedExtra>;
+
+pub type SignedExtra = (
+	frame_system::CheckSpecVersion<Test>,
+	frame_system::CheckTxVersion<Test>,
+	frame_system::CheckGenesis<Test>,
+	frame_system::CheckEra<Test>,
+	frame_system::CheckNonce<Test>,
+	frame_system::CheckWeight<Test>,
+);
+
+construct_runtime!(
+	pub enum Test where
+		Block = Block,
+		NodeBlock = Block,
+		UncheckedExtrinsic = UncheckedExtrinsic,
+	{
+		System: frame_system,
+		Balances: pallet_balances,
+		Timestamp: pallet_timestamp,
+		Teebag: pallet_teebag,
+	}
+);
+
+parameter_types! {
+	pub const BlockHashCount: u32 = 250;
+}
+impl frame_system::Config for Test {
+	type BaseCallFilter = frame_support::traits::Everything;
+	type BlockWeights = ();
+	type BlockLength = ();
+	type DbWeight = ();
+	type RuntimeOrigin = RuntimeOrigin;
+	type Index = u64;
+	type RuntimeCall = RuntimeCall;
+	type BlockNumber = BlockNumber;
+	type Hash = H256;
+	type Hashing = BlakeTwo256;
+	type AccountId = AccountId;
+	type Lookup = IdentityLookup<Self::AccountId>;
+	type Header = Header;
+	type RuntimeEvent = RuntimeEvent;
+	type BlockHashCount = BlockHashCount;
+	type Version = ();
+	type PalletInfo = PalletInfo;
+	type AccountData = pallet_balances::AccountData<Balance>;
+	type OnNewAccount = ();
+	type OnKilledAccount = ();
+	type SystemWeightInfo = ();
+	type SS58Prefix = ();
+	type OnSetCode = ();
+	type MaxConsumers = frame_support::traits::ConstU32<16>;
+}
+
+pub type Balance = u64;
+
+parameter_types! {
+	pub const ExistentialDeposit: u64 = 1;
+}
+
+impl pallet_balances::Config for Test {
+	type MaxLocks = ();
+	type Balance = u64;
+	type DustRemoval = ();
+	type RuntimeEvent = RuntimeEvent;
+	type ExistentialDeposit = ExistentialDeposit;
+	type AccountStore = System;
+	type WeightInfo = ();
+	type MaxReserves = ();
+	type ReserveIdentifier = ();
+	type HoldIdentifier = ();
+	type FreezeIdentifier = ();
+	type MaxHolds = ();
+	type MaxFreezes = ();
+}
+
+parameter_types! {
+		pub const MinimumPeriod: u64 = 6000 / 2;
+}
+
+pub type Moment = u64;
+
+impl pallet_timestamp::Config for Test {
+	type Moment = Moment;
+	type OnTimestampSet = ();
+	type MinimumPeriod = MinimumPeriod;
+	type WeightInfo = ();
+}
+
+parameter_types! {
+	pub const MomentsPerDay: u64 = 86_400_000; // [ms/d]
+}
+
+impl pallet_teebag::Config for Test {
+	type RuntimeEvent = RuntimeEvent;
+	type MomentsPerDay = MomentsPerDay;
+	type SetAdminOrigin = EnsureRoot<Self::AccountId>;
+}
+
+// This function basically just builds a genesis storage key/value store according to
+// our desired mockup. RA from enclave compiled in debug mode is allowed
+pub fn new_test_ext(is_dev_mode: bool) -> sp_io::TestExternalities {
+	let mut t = system::GenesisConfig::default().build_storage::<Test>().unwrap();
+	pallet_balances::GenesisConfig::<Test> {
+		balances: vec![(AccountKeyring::Alice.to_account_id(), 1 << 60)],
+	}
+	.assimilate_storage(&mut t)
+	.unwrap();
+
+	let mut genesis_config: pallet_teebag::GenesisConfig<Test> = crate::GenesisConfig {
+		allow_sgx_debug_mode: true,
+		admin: Some(AccountKeyring::Alice.to_account_id()),
+		mode: OperationalMode::Production,
+	};
+
+	if is_dev_mode {
+		genesis_config.mode = OperationalMode::Development;
+	}
+
+	GenesisBuild::<Test>::assimilate_storage(&genesis_config, &mut t).unwrap();
+
+	let mut ext: sp_io::TestExternalities = t.into();
+	ext.execute_with(|| {
+		System::set_block_number(1);
+	});
+	ext
+}
+
+/// Helper method for the OnTimestampSet to be called
+pub fn set_timestamp(t: u64) {
+	let _ = pallet_timestamp::Pallet::<Test>::set(RuntimeOrigin::none(), t);
+}
+
+/// Run until a particular block.
+pub fn run_to_block(n: u32) {
+	while System::block_number() < n {
+		if System::block_number() > 1 {
+			System::on_finalize(System::block_number());
+		}
+		Timestamp::on_finalize(System::block_number());
+		System::set_block_number(System::block_number() + 1);
+		System::on_initialize(System::block_number());
+	}
+}
