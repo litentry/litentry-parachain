7f135231ac908d80287843df1b9abd35c299bb60
Jonathan Alvarez <jonathan@litentry.com>
2024-02-07 15:47:09 +0000

    update schema id

--- tee-worker/litentry/core/credentials/src/jsonschema.rs
+++ tee-worker/litentry/core/credentials/src/jsonschema.rs
@@ -51,17 +51,17 @@ pub fn get_json_schema_url(assertion: Assertion, credential_subject_type: String
 
 		Assertion::Achainable(params) => match params {
 			AchainableParams::AmountHolding(_) =>
-				format!("{BASE_URL}/19-token-holding-amount/1-0-0.json"),
+				format!("{BASE_URL}/18-token-holding-amount/1-0-0.json"),
 
 			AchainableParams::AmountToken(_) =>
-				format!("{BASE_URL}/19-token-holding-amount/1-0-0.json"),
+				format!("{BASE_URL}/18-token-holding-amount/1-0-0.json"),
 
 			AchainableParams::Amount(_) => format!("{BASE_URL}/11-token-holder/1-0-0.json"),
 
 			// Generates both BAB Holder and Uniswap V2/V2 user.
 			AchainableParams::Basic(_) => match credential_subject_type.as_str() {
 				achainable::uniswap_user::UNISWAP_USER_TYPE =>
-					format!("{BASE_URL}/15-uniswap-v2-v3-user/1-0-0.json"),
+					format!("{BASE_URL}/14-uniswap-v2-v3-user/1-0-0.json"),
 
 				achainable::bab_holder::BAB_HOLDER_TYPE =>
 					format!("{BASE_URL}/11-token-holder/1-0-0.json"),
@@ -72,7 +72,7 @@ pub fn get_json_schema_url(assertion: Assertion, credential_subject_type: String
 			AchainableParams::ClassOfYear(_) =>
 				format!("{BASE_URL}/10-account-class-of-year/1-0-0.json"),
 
-			AchainableParams::Mirror(_) => format!("{BASE_URL}/24-mirror-contributor/1-0-0.json"),
+			AchainableParams::Mirror(_) => format!("{BASE_URL}/23-mirror-contributor/1-0-0.json"),
 
 			// The following assertions are Unused and produce no specific claims. They Generates
 			// generic JSON Credentials
@@ -84,31 +84,31 @@ pub fn get_json_schema_url(assertion: Assertion, credential_subject_type: String
 			AchainableParams::Token(_) => format!("{}", NOT_IMPLEMENTED),
 		},
 
-		Assertion::A20 => format!("{BASE_URL}/13-idhub-evm-version-early-bird/1-0-0.json"),
+		Assertion::A20 => format!("{BASE_URL}/12-idhub-evm-version-early-bird/1-0-0.json"),
 
-		Assertion::Oneblock(_) => format!("{BASE_URL}/14-oneblock-student-phase-12/1-0-0.json"),
+		Assertion::Oneblock(_) => format!("{BASE_URL}/13-oneblock-student-phase-12/1-0-0.json"),
 
 		Assertion::GenericDiscordRole(_) =>
-			format!("{BASE_URL}/16-generic-discord-role/1-0-0.json"),
+			format!("{BASE_URL}/15-generic-discord-role/1-0-0.json"),
 
 		Assertion::BnbDomainHolding =>
-			format!("{BASE_URL}/17-bnb-domain-holding-amount/1-0-0.json"),
+			format!("{BASE_URL}/16-bnb-domain-holding-amount/1-0-0.json"),
 
 		Assertion::BnbDigitDomainClub(_) =>
-			format!("{BASE_URL}/18-bnb-3d-4d-club-domain-holding-amount/1-0-0.json"),
+			format!("{BASE_URL}/17-bnb-3d-4d-club-domain-holding-amount/1-0-0.json"),
 
-		Assertion::VIP3MembershipCard(_) => format!("{BASE_URL}/21-vip3-card-holder/1-0-0.json"),
+		Assertion::VIP3MembershipCard(_) => format!("{BASE_URL}/20-vip3-card-holder/1-0-0.json"),
 
 		Assertion::WeirdoGhostGangHolder =>
-			format!("{BASE_URL}/20-weirdoghostgang-holder/1-0-0.json"),
+			format!("{BASE_URL}/19-weirdoghostgang-holder/1-0-0.json"),
 
-		Assertion::LITStaking => format!("{BASE_URL}/19-token-holding-amount/1-0-0.json"),
+		Assertion::LITStaking => format!("{BASE_URL}/18-token-holding-amount/1-0-0.json"),
 
-		Assertion::EVMAmountHolding(_) => format!("{BASE_URL}/23-evm-holding-amount/1-0-0.json"),
+		Assertion::EVMAmountHolding(_) => format!("{BASE_URL}/22-evm-holding-amount/1-0-0.json"),
 
 		Assertion::BRC20AmountHolder =>
-			format!("{BASE_URL}/22-token-holding-amount-list/1-0-0.json"),
+			format!("{BASE_URL}/21-token-holding-amount-list/1-0-0.json"),
 
-		Assertion::CryptoSummary => format!("{BASE_URL}/25-crypto-summary/1-0-0.json"),
+		Assertion::CryptoSummary => format!("{BASE_URL}/24-crypto-summary/1-0-0.json"),
 	}
 }
