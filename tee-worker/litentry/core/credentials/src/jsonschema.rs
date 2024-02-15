d83bbeb4af4a11ffaa2d3320dbf090b34b52a114
Jonathan Alvarez <jonathan@litentry.com>
2024-02-15 13:56:21 +0000

    fix syntax issues

--- tee-worker/litentry/core/credentials/src/jsonschema.rs
+++ tee-worker/litentry/core/credentials/src/jsonschema.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Litentry. If not, see <https://www.gnu.org/licenses/>.
 
-use crate::achainable;
 use litentry_primitives::{AchainableParams, Assertion};
 use std::string::String;
 
