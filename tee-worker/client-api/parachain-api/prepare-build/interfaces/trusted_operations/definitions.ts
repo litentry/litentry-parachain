9b8203828b4c7914986021c5928f279cbc75f28c
Yang <yang@litentry.com>
2024-02-28 22:21:13 +0000

    keep order aligned.

--- tee-worker/client-api/parachain-api/prepare-build/interfaces/trusted_operations/definitions.ts
+++ tee-worker/client-api/parachain-api/prepare-build/interfaces/trusted_operations/definitions.ts
@@ -37,10 +37,10 @@ export default {
                 deactivate_identity: "(LitentryIdentity, LitentryIdentity, LitentryIdentity, Option<RequestAesKey>, H256)",
                 activate_identity: "(LitentryIdentity, LitentryIdentity, LitentryIdentity, Option<RequestAesKey>, H256)",
                 request_vc: "(LitentryIdentity, LitentryIdentity, Assertion, Option<RequestAesKey>, H256)",
-                batch_request_vc: "(LitentryIdentity, LitentryIdentity, Vec<Assertion>, Option<RequestAesKey>, H256)",
                 set_identity_networks:
-                    "(LitentryIdentity, LitentryIdentity, LitentryIdentity, Vec<Web3Network>, Option<RequestAesKey>, H256)",
+                "(LitentryIdentity, LitentryIdentity, LitentryIdentity, Vec<Web3Network>, Option<RequestAesKey>, H256)",
                 __Unused_remove_identity: "Null",
+                batch_request_vc: "(LitentryIdentity, LitentryIdentity, Vec<Assertion>, Option<RequestAesKey>, H256)",
             },
         },
         TrustedOperationStatus: {
