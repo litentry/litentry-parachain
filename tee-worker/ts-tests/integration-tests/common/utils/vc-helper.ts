5aa1aec8e8d23b26ed5fa66769344f18b4cc924d
Verin1005 <daqingchong0809@gmail.com>
2024-02-29 13:39:28 +0000

    add the remaining VC.

--- tee-worker/ts-tests/integration-tests/common/utils/vc-helper.ts
+++ tee-worker/ts-tests/integration-tests/common/utils/vc-helper.ts
@@ -18,150 +18,184 @@ export async function handleVcEvents(events: any[], method: 'VCIssued' | 'Failed
     return [...results];
 }
 
-// https://github.com/litentry/litentry-parachain/tree/dev/tee-worker/litentry/core/assertion-build/src
-export const defaultAssertions = [
-    // {
-    //     description: 'Have identified at least one account/address in both Web2 and Web3.',
-    //     assertion: {
-    //         A1: 'A1',
-    //     },
-    // },
-    // {
-    //     description: 'The user is a member of Litentry Discord.',
-    //     assertion: {
-    //         A2: '807161594245152800',
-    //     },
-    // },
+// @todo move to a better place, and make it more generic, at least define the type
+export const mockAssertions = [
+    {
+        description: 'Have identified at least one account/address in both Web2 and Web3.',
+        assertion: {
+            A1: 'A1',
+        },
+    },
+    {
+        description: 'The user is a member of Litentry Discord.',
+        assertion: {
+            A2: '807161594245152800',
+        },
+    },
 
-    // {
-    //     description:
-    //         'Have commented in Litentry Discord #🪂id-hubber channel. Channel link: https://discord.com/channels/807161594245152800/1093886939746291882',
-    //     assertion: {
-    //         A3: ['A3', 'A3', 'A3'],
-    //     },
-    // },
+    {
+        description:
+            'Have commented in Litentry Discord #🪂id-hubber channel. Channel link: https://discord.com/channels/807161594245152800/1093886939746291882',
+        assertion: {
+            A3: ['A3', 'A3', 'A3'],
+        },
+    },
 
-    // {
-    //     description: 'The length of time a user continues to hold LIT token',
-    //     assertion: {
-    //         A4: '10',
-    //     },
-    // },
-    // {
-    //     description: 'The length of time a user continues to hold DOT token',
-    //     assertion: {
-    //         A7: '5',
-    //     },
-    // },
-    // {
-    //     description:
-    //         'The range of number of transactions a user has made for a specific token on all supported networks(Litentry)',
-    //     assertion: {
-    //         A8: ['Litentry'],
-    //     },
-    // },
-    // {
-    //     description:
-    //         'The user is an early bird user of the IdentityHub EVM version and has generated at least 1 credential during 2023 Aug 14th ~ Aug 21st.',
-    //     assertion: {
-    //         A20: 'A20',
-    //     },
-    // },
+    // litentry-archive
+    {
+        description:
+            'The user is an early bird user of the IdentityHub EVM version and has generated at least 1 credential during 2023 Aug 14th ~ Aug 21st.',
+        assertion: {
+            A20: 'A20',
+        },
+    },
 
-    // // Achainable
-    // // https://www.notion.so/web3builders/Assertion-interface-9126ba85a925417a922f2c6ae5d62e87
+    // Achainable
+    {
+        description: `A trader or liquidity provider of Uniswap V2 or V3
+                      Uniswap V2 Factory Contract: 0x5c69bee701ef814a2b6a3edd4b1652cb9cc5aa6f
+                      Uniswap V3 Factory Contract: 0x1f98431c8ad98523631ae4a59f267346ea31f984`,
+        assertion: {
+            Achainable: {
+                Basic: {
+                    name: 'Uniswap V2/V3 user',
+                    chain: ['Ethereum'],
+                },
+            },
+        },
+    },
+    {
+        description: 'The number of ETH tokens hold > 0',
+        assertion: {
+            Achainable: {
+                Amount: {
+                    name: 'Balance over {amount}',
+                    chain: ['Ethereum'],
+                    amount: '0',
+                },
+            },
+        },
+    },
+    {
+        description: 'The class of year that Ethereum account was created (must have on-chain records)',
+        assertion: {
+            Achainable: {
+                ClassOfYear: {
+                    name: 'Account created between {dates}',
+                    chain: ['Ethereum'],
+                },
+            },
+        },
+    },
+    {
+        description: 'A deployer of a smart contract on Ethereum',
+        assertion: {
+            Achainable: {
+                Amount: {
+                    name: 'Created over {amount} contracts',
+                    chain: ['Ethereum'],
+                    amount: '0',
+                },
+            },
+        },
+    },
+    {
+        description: 'The length of time a user continues to hold LIT token',
+        assertion: {
+            A4: '10',
+        },
+    },
+    {
+        description: 'The length of time a user continues to hold DOT token',
+        assertion: {
+            A7: '5',
+        },
+    },
+    {
+        description:
+            'The range of number of transactions a user has made for a specific token on all supported networks(Litentry)',
+        assertion: {
+            A8: ['Litentry'],
+        },
+    },
 
-    // {
-    //     description: `A trader or liquidity provider of Uniswap V2 or V3
-    //                   Uniswap V2 Factory Contract: 0x5c69bee701ef814a2b6a3edd4b1652cb9cc5aa6f
-    //                   Uniswap V3 Factory Contract: 0x1f98431c8ad98523631ae4a59f267346ea31f984`,
-    //     assertion: {
-    //         Achainable: {
-    //             Basic: {
-    //                 name: 'Uniswap V2/V3 user',
-    //                 chain: ['Ethereum'],
-    //             },
-    //         },
-    //     },
-    // },
-    // {
-    //     description: 'The number of ETH tokens hold > 0',
-    //     assertion: {
-    //         Achainable: {
-    //             Amount: {
-    //                 name: 'Balance over {amount}',
-    //                 chain: ['Ethereum'],
-    //                 amount: '0',
-    //             },
-    //         },
-    //     },
-    // },
-    // {
-    //     description: 'The class of year that Ethereum account was created (must have on-chain records)',
-    //     assertion: {
-    //         Achainable: {
-    //             ClassOfYear: {
-    //                 name: 'Account created between {dates}',
-    //                 chain: ['Ethereum'],
-    //             },
-    //         },
-    //     },
-    // },
-    // {
-    //     description: 'A deployer of a smart contract on Ethereum',
-    //     assertion: {
-    //         Achainable: {
-    //             Amount: {
-    //                 name: 'Created over {amount} contracts',
-    //                 chain: ['Ethereum'],
-    //                 amount: '0',
-    //             },
-    //         },
-    //     },
-    // },
-
-    // // SORA
-    // {
-    //     description:
-    //         'Congratulations on your participation in our first quiz in collaboration with our partner, SORA. You have embarked on an exciting educational journey, exploring the world of DeFi & Web3 Identity, we truly appreciate your curiosity and dedication.',
-    //     assertion: {
-    //         GenericDiscordRole: {
-    //             SoraQuiz: 'Attendee',
-    //         },
-    //     },
-    // },
-    // // VIP3
-    // {
-    //     description: 'VIP3 Silver Card Holder',
-    //     assertion: {
-    //         VIP3MembershipCard: 'Silver',
-    //     },
-    // },
-    // // BNB domain
-    // {
-    //     description: 'Holding a certain amount of bnb domain names',
-    //     assertion: {
-    //         BnbDomainHolding: 'BnbDomainHolding',
-    //     },
-    // },
-    // {
-    //     description: 'Holding a certain amount of 000-999.bnb domain names',
-    //     assertion: {
-    //         BnbDigitDomainClub: 'Bnb999ClubMember',
-    //     },
-    // },
-    // // OneBlock
-    // {
-    //     description: 'A participant to the course co-created by OneBlock+ and Parity',
-    //     assertion: {
-    //         Oneblock: 'CourseCompletion',
-    //     },
-    // },
-    // NFT holder
+    // SORA
+    {
+        description:
+            'Congratulations on your participation in our first quiz in collaboration with our partner, SORA. You have embarked on an exciting educational journey, exploring the world of DeFi & Web3 Identity, we truly appreciate your curiosity and dedication.',
+        assertion: {
+            GenericDiscordRole: {
+                SoraQuiz: 'Attendee',
+            },
+        },
+    },
+    // VIP3
+    {
+        description: 'VIP3 Silver Card Holder',
+        assertion: {
+            VIP3MembershipCard: 'Silver',
+        },
+    },
+    // BNB domain-nodereal
+    {
+        description: 'Holding a certain amount of bnb domain names',
+        assertion: {
+            BnbDomainHolding: 'BnbDomainHolding',
+        },
+    },
+    {
+        description: 'Holding a certain amount of 000-999.bnb domain names',
+        assertion: {
+            BnbDigitDomainClub: 'Bnb999ClubMember',
+        },
+    },
+    // OneBlock
+    {
+        description: 'A participant to the course co-created by OneBlock+ and Parity',
+        assertion: {
+            Oneblock: 'CourseCompletion',
+        },
+    },
+    // Brc20AmountHolder-Geniidata
     {
         description: 'NFT holder',
         assertion: {
             Brc20AmountHolder: [],
         },
     },
+
+    // nodereal_jsonrpc
+    // {
+    //     description: 'You are a holder of a certain kind of NFT',
+    //     assertion: {
+    //         NftHolder: 'WeirdoGhostGang',
+    //     },
+    // },
+    // nodereal_jsonrpc
+    {
+        description: 'The amount of TRX you are holding',
+        assertion: {
+            TokenHoldingAmount: 'TRX',
+        },
+    },
+    //karat_dao
+    {
+        description: 'You are a user of a certain platform',
+        assertion: {
+            PlatformUser: 'KaratDaoUser',
+        },
+    },
+    {
+        description: 'The amount of LIT you are staking',
+        assertion: {
+            LITStaking: 'LITStaking',
+        },
+    },
+    {
+        description: 'The amount of a Ton you are holding',
+        assertion: {
+            EVMAmountHolding: 'Ton',
+        },
+    },
 ];
+ 
