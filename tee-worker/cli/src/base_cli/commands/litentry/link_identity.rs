295ca00a9085674df0b307ed27a30f31391820bd
felixfaisal <faisalahmedfarooq46@gmail.com>
2024-03-01 07:35:13 +0000

    fix: update link-identity command

--- tee-worker/cli/src/base_cli/commands/litentry/link_identity.rs
+++ tee-worker/cli/src/base_cli/commands/litentry/link_identity.rs
@@ -22,6 +22,7 @@ use crate::{
 use base58::FromBase58;
 use codec::{Decode, Encode};
 use ita_stf::{helpers::get_expected_raw_message, Web3Network};
+use itc_rpc_client::direct_client::DirectApi;
 use itp_sgx_crypto::ShieldingCryptoEncrypt;
 use itp_stf_primitives::types::ShardIdentifier;
 use litentry_primitives::{Identity, LitentryMultiSignature, Web3CommonValidationData};
@@ -46,38 +47,109 @@ impl LinkIdentityCommand {
 	pub(crate) fn run(&self, cli: &Cli) -> CliResult {
 		let mut chain_api = get_chain_api(cli);
 
-		let shard_opt = match self.shard.from_base58() {
-			Ok(s) => ShardIdentifier::decode(&mut &s[..]),
-			_ => panic!("shard argument must be base58 encoded"),
-		};
+		let direct_api = get_worker_api_direct(cli);
+		let mrenclave = direct_api.get_state_mrenclave().unwrap();
+		let shard = ShardIdentifier::decode(&mut &mrenclave[..]).unwrap();
 
-		let shard = match shard_opt {
-			Ok(shard) => shard,
-			Err(e) => panic!("{}", e),
-		};
+		// let signer: sr25519_core::Pair = if let Some(account) = &self.delegate {
+		// 	get_pair_from_str(&account).into()
+		// } else {
+		// 	// Normal who which we use
+		// 	get_pair_from_str(&self.account).into()
+		// };
+		// chain_api.set_signer(signer.clone().into());
 
-		let signer: sr25519_core::Pair = if let Some(account) = &self.delegate {
-			get_pair_from_str(&account).into()
-		} else {
-			// Normal who which we use
-			get_pair_from_str(&self.account).into()
-		};
+		// println!("Do something here");
+		// let who: sr25519_core::Pair = get_pair_from_str(&self.account).into();
+		// let identity = Identity::from_did(self.did.as_str()).unwrap();
+		// let identity_account_id = identity.to_account_id().unwrap();
+		// let identity_public_key = format!("{}", identity_account_id);
+		// let identity_pair: sr25519_core::Pair = get_pair_from_str(&identity_public_key).into();
+		// let tee_shielding_key = get_shielding_key(cli).unwrap();
+		// let encrypted_identity = tee_shielding_key.encrypt(&identity.encode()).unwrap();
+		// let who_identity = Identity::from(who.public());
+		// let vdata = get_expected_raw_message(&who_identity, &identity, 1);
+		// let validation_payload = vdata.clone();
+		// let web3network = vec![Web3Network::Litentry];
+		// let encrypted_web3network = tee_shielding_key.encrypt(&web3network.encode()).unwrap();
+
+		// let signature: LitentryMultiSignature = identity_pair.sign(&validation_payload).into();
+		// let web3common = Web3CommonValidationData {
+		// 	message: validation_payload.clone().try_into().unwrap(),
+		// 	signature: signature.into(),
+		// };
+		// let validation_data = litentry_primitives::ValidationData::Web3(
+		// 	litentry_primitives::Web3ValidationData::Substrate(web3common),
+		// );
+		// let encrypted_validation_data =
+		// 	tee_shielding_key.encrypt(&validation_data.encode()).unwrap();
+
+		// let xt = compose_extrinsic!(
+		// 	chain_api,
+		// 	IMP,
+		// 	"link_identity",
+		// 	shard,
+		// 	who.public().0,
+		// 	encrypted_identity.to_vec(),
+		// 	encrypted_validation_data,
+		// 	encrypted_web3network
+		// );
+		// println!("Sending request");
+		// let tx_hash = chain_api.submit_and_watch_extrinsic_until(xt, XtStatus::Broadcast).unwrap();
+		// println!("[+] LinkIdentityCommand TrustedOperation got finalized. Hash: {:?}\n", tx_hash);
+
+		// Ok(CliResultOk::None)
+		let signer = self.get_signer();
 		chain_api.set_signer(signer.clone().into());
 
-		let who: sr25519_core::Pair = get_pair_from_str(&self.account).into();
+		let (identity, encrypted_identity) = self.encrypt_identity(cli);
+		let (encrypted_web3network, encrypted_validation_data) =
+			self.prepare_validation_data(&identity, cli);
 
-		let identity = Identity::from_did(self.did.as_str()).unwrap();
+		let xt = compose_extrinsic!(
+			chain_api,
+			IMP,
+			"link_identity",
+			shard,
+			signer.public().0,
+			encrypted_identity,
+			encrypted_validation_data,
+			encrypted_web3network
+		);
+
+		let tx_hash = chain_api.submit_and_watch_extrinsic_until(xt, XtStatus::Broadcast).unwrap();
+		info!("[+] LinkIdentityCommand TrustedOperation got finalized. Hash: {:?}", tx_hash);
+
+		Ok(CliResultOk::None)
+	}
+
+	fn get_signer(&self) -> sr25519_core::Pair {
+		let account = self.delegate.as_ref().unwrap_or(&self.account);
+		get_pair_from_str(account).into()
+	}
+
+	fn encrypt_identity(&self, cli: &Cli) -> (Identity, Vec<u8>) {
+		let identity = Identity::from_did(&self.did).unwrap();
 		let tee_shielding_key = get_shielding_key(cli).unwrap();
 		let encrypted_identity = tee_shielding_key.encrypt(&identity.encode()).unwrap();
-		let who_identity = Identity::from(who.public());
-		let vdata = get_expected_raw_message(&who_identity, &identity, 1);
+		(identity, encrypted_identity)
+	}
+
+	fn prepare_validation_data(&self, identity: &Identity, cli: &Cli) -> (Vec<u8>, Vec<u8>) {
+		let who_identity = Identity::from(self.get_signer().public());
+		let vdata = get_expected_raw_message(&who_identity, identity, 1);
 		let validation_payload = vdata.clone();
 		let web3network = vec![Web3Network::Litentry];
+		let tee_shielding_key = get_shielding_key(cli).unwrap();
 		let encrypted_web3network = tee_shielding_key.encrypt(&web3network.encode()).unwrap();
 
-		let signature: LitentryMultiSignature = who.sign(&validation_payload).into();
+		let identity_account_id = identity.to_account_id().unwrap();
+		let identity_public_key = format!("{}", identity_account_id);
+		let identity_pair: sr25519_core::Pair = get_pair_from_str(&identity_public_key).into();
+
+		let signature: LitentryMultiSignature = identity_pair.sign(&validation_payload).into();
 		let web3common = Web3CommonValidationData {
-			message: validation_payload.clone().try_into().unwrap(),
+			message: validation_payload.try_into().unwrap(),
 			signature: signature.into(),
 		};
 		let validation_data = litentry_primitives::ValidationData::Web3(
@@ -86,20 +158,6 @@ impl LinkIdentityCommand {
 		let encrypted_validation_data =
 			tee_shielding_key.encrypt(&validation_data.encode()).unwrap();
 
-		let xt = compose_extrinsic!(
-			chain_api,
-			IMP,
-			"link_identity",
-			shard,
-			who.public().0,
-			encrypted_identity.to_vec(),
-			encrypted_validation_data,
-			encrypted_web3network
-		);
-		println!("Sending request");
-		let tx_hash = chain_api.submit_and_watch_extrinsic_until(xt, XtStatus::Finalized).unwrap();
-		println!("[+] LinkIdentityCommand TrustedOperation got finalized. Hash: {:?}\n", tx_hash);
-
-		Ok(CliResultOk::None)
+		(encrypted_web3network, encrypted_validation_data)
 	}
 }
