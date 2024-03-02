a133504e69eafc5e74f494ac9d63e7ac5b23cebe
Igor Trofimov <igor@kawagarbo-tech.io>
2024-03-02 08:56:08 +0000

    cleanup

--- local-setup/utils/index.js
+++
@@ -1,7 +0,0 @@
-export const homePath = process.env.PWD.substring(0, process.env.PWD.indexOf("/tee-worker"));
-export const workerPath = `${homePath}/tee-worker`;
-
-export function printLabel(label) {
-  const separator = "=".repeat(30);
-  console.log(`\n${separator} ${label} ${separator}`);
-}
