50da58510a88dd2b899d7970508fcb8a2096d29d
Verin1005 <daqingchong0809@gmail.com>
2024-03-07 05:12:42 +0000

    complete tests

--- tee-worker/ts-tests/integration-tests/di_bitcoin_identity.test.ts
+++ tee-worker/ts-tests/integration-tests/di_bitcoin_identity.test.ts
@@ -77,7 +77,6 @@ describe('Test Identity (bitcoin direct invocation)', function () {
     });
 
     step('check idGraph from sidechain storage before linking', async function () {
-        for (let index = 0; index < 2000; index++) {
             const idGraphGetter = await createSignedTrustedGetterIdGraph(
                 context.api,
                 new BitcoinSigner(context.bitcoinWallet.alice),
@@ -86,7 +85,6 @@ describe('Test Identity (bitcoin direct invocation)', function () {
             const res = await sendRequestFromGetter(context, teeShieldingKey, idGraphGetter);
             const idGraph = decodeIdGraph(context.sidechainRegistry, res.value);
             assert.lengthOf(idGraph, 0);
-        }
     });
 
     step('linking identities (alice bitcoin account)', async function () {
@@ -158,6 +156,7 @@ describe('Test Identity (bitcoin direct invocation)', function () {
             );
 
             const res = await sendRequestFromTrustedCall(context, teeShieldingKey, linkIdentityCall);
+            
             idGraphHashResults.push(
                 await assertIdGraphMutationResult(
                     context,
