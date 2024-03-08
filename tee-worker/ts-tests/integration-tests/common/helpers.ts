744bc852a35bd01e1666ea15fd9cd39682d0904d
0xverin <104152026+0xverin@users.noreply.github.com>
2024-03-08 15:49:18 +0800

    fix: p-460 bitcore message signature issue (#2553)
    * debug:check idGraph with aliceBitcoinIdentity
    
    * fmt
    
    * fix bitcore signmessage issue
    
    * remove log && uncomment
    
    * modify bitcore sign message logic
    
    * switch to bitcoinjs lib
    
    * del unused deps
    
    * try 2000 tests
    
    * fmt
    
    * complete tests
    
    * fmt one more
    
    * pnpm-lock file
    
    * pass ci
    
    * Uncomment karat_dao test
    
    * rollback handleVcEvents
    
    * uncomment one more
    
    * fix comments

--- tee-worker/ts-tests/integration-tests/common/helpers.ts
+++ tee-worker/ts-tests/integration-tests/common/helpers.ts
@@ -6,7 +6,8 @@ import type { HexString } from '@polkadot/util/types';
 import './config';
 import { IntegrationTestContext, JsonRpcRequest } from './common-types';
 import { randomBytes } from 'crypto';
-
+import { ECPairFactory, ECPairInterface } from 'ecpair';
+import * as ecc from 'tiny-secp256k1';
 // format and setup
 const keyring = new Keyring({ type: 'sr25519' });
 export function getSubstrateSigner(): {
@@ -76,4 +77,10 @@ export function nextRequestId(context: IntegrationTestContext): number {
 export function randomSubstrateWallet(): KeyringPair {
     const keyring = new Keyring({ type: 'sr25519' });
     return keyring.addFromSeed(randomBytes(32));
+}
+
+export function randomBitcoinWallet(): ECPairInterface {
+    const ecPair = ECPairFactory(ecc);
+    const keyPair = ecPair.makeRandom();
+    return keyPair;
 }
