dd0a039226157ef18696386d82788d16a7714180
Minqi Wang <wangminqi@aliyun.com>
2024-03-11 15:34:39 +0800

    debug: remove brief

--- ts-tests/integration-tests/evm-transfer.test.ts
+++ ts-tests/integration-tests/evm-transfer.test.ts
@@ -74,30 +74,6 @@ describeLitentry('Test EVM Module Transfer', ``, (context) => {
         const { nonce: evmAccountCurrentNonce, data: evmAccountCurrentBalance } =
             await context.api.query.system.account(evmAccountRaw.mappedAddress);
 
-        // -- test only --
-        // Subscribe to system events via storage
-        context.api.query.system.events((events) => {
-            console.log(`\nReceived ${events.length} events:`);
-
-            // Loop through the Vec<EventRecord>
-            events.forEach((record) => {
-            // Extract the phase, event and the event types
-            const { event, phase } = record;
-            const types = event.typeDef;
-
-            // Show what we are busy with
-            console.log(`\t${event.section}:${event.method}:: (phase=${phase.toString()})`);
-            console.log(`\t\t${event.meta.docs.toString()}`);
-
-            // Loop through each of the parameters, displaying the type and data
-            event.data.forEach((data, index) => {
-                console.log(`\t\t\t${types[index].type}: ${data.toString()}`);
-            });
-            });
-        });
-
-        // -- test only --
-
         // If a substrate account using pallet_evm to trigger evm transaction,
         // it will bump 2 for nonce (one for substrate extrinsic, one for evm).
         // +1 nonce for original substrate account, plus another 1 nonce for original substrate account's truncated evm address's mapped susbtrate account.
