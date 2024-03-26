6892284d4a32a7b40167613117f5f41442546df4
Francisco Silva <francisco@kawagarbo-tech.io>
2024-03-26 16:43:11 +0000

    updating event handler to handle filtered events

--- tee-worker/app-libs/parentchain-interface/src/integritee/event_handler.rs
+++ tee-worker/app-libs/parentchain-interface/src/integritee/event_handler.rs
@@ -15,7 +15,9 @@
 
 */
 
-use codec::Encode;
+use codec::{Decode, Encode};
+
+use sp_std::vec::Vec;
 
 pub use ita_sgx_runtime::{Balance, Index};
 use ita_stf::{Getter, TrustedCall, TrustedCallSigned};
@@ -25,6 +27,7 @@ use itp_types::parentchain::{
 	AccountId, FilterEvents, HandleParentchainEvents, ParentchainError, ParentchainId,
 };
 use litentry_hex_utils::hex_encode;
+use litentry_primitives::{Identity, ValidationData, Web3Network};
 use log::*;
 
 pub struct ParentchainEventHandler {}
@@ -53,6 +56,42 @@ impl ParentchainEventHandler {
 
 		Ok(())
 	}
+
+	fn link_identity<Executor: IndirectExecutor<TrustedCallSigned, Error>>(
+		executor: &Executor,
+		account: &AccountId,
+		encrypted_identity: Vec<u8>,
+		encrypted_validation_data: Vec<u8>,
+		encrypted_web3networks: Vec<u8>,
+	) -> Result<(), Error> {
+		let shard = executor.get_default_shard();
+		let enclave_account_id = executor.get_enclave_account().expect("no enclave account");
+
+		let identity: Identity =
+			Identity::decode(&mut executor.decrypt(&encrypted_identity)?.as_slice())?;
+		let validation_data =
+			ValidationData::decode(&mut executor.decrypt(&encrypted_validation_data)?.as_slice())?;
+		let web3networks: Vec<Web3Network> =
+			Decode::decode(&mut executor.decrypt(&encrypted_web3networks)?.as_slice())?;
+
+		let trusted_call = TrustedCall::link_identity(
+			enclave_account_id.into(),
+			account.clone().into(),
+			identity,
+			validation_data,
+			web3networks,
+			None,
+			Default::default(),
+		);
+		let signed_trusted_call = executor.sign_call_with_self(&trusted_call, &shard)?;
+		let trusted_operation =
+			TrustedOperation::<TrustedCallSigned, Getter>::indirect_call(signed_trusted_call);
+
+		let encrypted_trusted_call = executor.encrypt(&trusted_operation.encode())?;
+		executor.submit_trusted_call(shard, encrypted_trusted_call);
+
+		Ok(())
+	}
 }
 
 impl<Executor> HandleParentchainEvents<Executor, TrustedCallSigned, Error>
@@ -65,12 +104,11 @@ where
 		events: impl FilterEvents,
 		vault_account: &AccountId,
 	) -> Result<(), Error> {
-		let filter_events = events.get_transfer_events();
-		trace!(
-			"filtering transfer events to shard vault account: {}",
-			hex_encode(vault_account.encode().as_slice())
-		);
-		if let Ok(events) = filter_events {
+		if let Ok(events) = events.get_transfer_events() {
+			trace!(
+				"filtering transfer events to shard vault account: {}",
+				hex_encode(vault_account.encode().as_slice())
+			);
 			events
 				.iter()
 				.filter(|&event| event.to == *vault_account)
@@ -82,6 +120,25 @@ where
 				})
 				.map_err(|_| ParentchainError::ShieldFundsFailure)?;
 		}
+
+		debug!("Handling link_identity events");
+
+		if let Ok(events) = events.get_link_identity_events() {
+			events
+				.iter()
+				.try_for_each(|event| {
+					info!("found link_identity_event: {}", event);
+					Self::link_identity(
+						executor,
+						&event.account,
+						event.encrypted_identity.clone(),
+						event.encrypted_validation_data.clone(),
+						event.encrypted_web3networks.clone(),
+					)
+				})
+				.map_err(|_| ParentchainError::LinkIdentityFailure)?;
+		}
+
 		Ok(())
 	}
 }
