5471063ba306ca65c907f256681ed03d4912ed3e
Kailai Wang <Kailai.Wang@hotmail.com>
2024-03-27 12:06:37 +0000

    use 2.0.0-preview branch

--- tee-worker/core-primitives/test/src/mock/shielding_crypto_mock.rs
+++ tee-worker/core-primitives/test/src/mock/shielding_crypto_mock.rs
@@ -18,23 +18,20 @@
 use itp_sgx_crypto::{
 	ed25519_derivation::DeriveEd25519, ShieldingCryptoDecrypt, ShieldingCryptoEncrypt,
 };
-use sgx_crypto::rsa::Rsa3072KeyPair;
+use sgx_crypto_helper::{rsa3072::Rsa3072KeyPair, RsaKeyPair};
 use sp_core::ed25519::Pair as Ed25519Pair;
 use std::vec::Vec;
-
-// TODO(Litentry):
-// `Rsa3072KeyPair::create()` seems to be broken for `ucrypto` feature under v2.0 SDK, see
-// https://github.com/apache/incubator-teaclave-sgx-sdk/issues/456
 
 #[derive(Clone)]
 pub struct ShieldingCryptoMock {
 	key: Rsa3072KeyPair,
 }
 
-#[allow(clippy::derivable_impls)]
 impl Default for ShieldingCryptoMock {
 	fn default() -> Self {
-		ShieldingCryptoMock { key: Rsa3072KeyPair::default() }
+		ShieldingCryptoMock {
+			key: Rsa3072KeyPair::new().expect("default RSA3072 key for shielding key mock"),
+		}
 	}
 }
 
@@ -42,10 +39,7 @@ impl ShieldingCryptoEncrypt for ShieldingCryptoMock {
 	type Error = itp_sgx_crypto::Error;
 
 	fn encrypt(&self, data: &[u8]) -> Result<Vec<u8>, Self::Error> {
-		// self.key
-		// 	.encrypt(data)
-		// 	.map_err(|e| itp_sgx_crypto::Error::Other(format!("encrypt error: {:?}", e).into()))
-		Ok(data.to_vec())
+		self.key.encrypt(data)
 	}
 }
 
@@ -53,10 +47,7 @@ impl ShieldingCryptoDecrypt for ShieldingCryptoMock {
 	type Error = itp_sgx_crypto::Error;
 
 	fn decrypt(&self, data: &[u8]) -> Result<Vec<u8>, Self::Error> {
-		// self.key
-		// 	.decrypt(data)
-		// 	.map_err(|e| itp_sgx_crypto::Error::Other(format!("decrypt error: {:?}", e).into()))
-		Ok(data.to_vec())
+		self.key.decrypt(data)
 	}
 }
 
