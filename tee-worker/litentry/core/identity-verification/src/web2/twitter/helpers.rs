7db8add2b2ecc522235de9263fa4423e9ef7b049
Francisco Silva <francisco@kawagarbo-tech.io>
2024-04-05 15:23:38 +0000

    fixing usages of rand

--- tee-worker/litentry/core/identity-verification/src/web2/twitter/helpers.rs
+++ tee-worker/litentry/core/identity-verification/src/web2/twitter/helpers.rs
@@ -1,4 +1,9 @@
-use sgx_rand::{thread_rng, Rng};
+#[cfg(all(not(feature = "std"), feature = "sgx"))]
+extern crate sgx_tstd as std;
+#[cfg(all(not(feature = "std"), feature = "sgx"))]
+extern crate sgx_rand as rand;
+
+use rand::{thread_rng, Rng};
 use std::string::String;
 
 pub fn get_code_verifier() -> String {
@@ -14,7 +19,11 @@ fn get_random_string(length: usize) -> String {
 	let charset: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 	let random_string: String = (0..length)
 		.map(|_| {
+            #[cfg(all(not(feature = "std"), feature = "sgx"))]
 			let idx = rng.gen_range(0, charset.len());
+            #[cfg(all(not(feature = "sgx"), feature = "std"))]
+			let idx = rng.gen_range(0..charset.len());
+
 			charset[idx] as char
 		})
 		.collect();
