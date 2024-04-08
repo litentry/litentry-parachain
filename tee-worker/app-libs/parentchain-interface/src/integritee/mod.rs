8aa4d4515d8135a3021b70a6b3a1dd3fa5025abc
Francisco Silva <francisco@kawagarbo-tech.io>
2024-04-08 14:24:02 +0000

    removing litentry extrinsics from indirect calls

--- tee-worker/app-libs/parentchain-interface/src/integritee/mod.rs
+++ tee-worker/app-libs/parentchain-interface/src/integritee/mod.rs
@@ -59,20 +59,6 @@ pub type ParentchainExtrinsicParser = ExtrinsicParser<ParentchainSignedExtra>;
 pub enum IndirectCall {
 	#[codec(index = 1)]
 	Invoke(InvokeArgs),
-	// Litentry
-	// we ignore `TimestampSet` for now, see unused `TrustedCall::timestamp_set`
-	#[codec(index = 2)]
-	LinkIdentity(LinkIdentityArgs, Option<MultiAddress<AccountId32, ()>>, H256),
-	#[codec(index = 3)]
-	DeactivateIdentity(DeactivateIdentityArgs, Option<MultiAddress<AccountId32, ()>>, H256),
-	#[codec(index = 4)]
-	ActivateIdentity(ActivateIdentityArgs, Option<MultiAddress<AccountId32, ()>>, H256),
-	#[codec(index = 5)]
-	RequestVC(RequestVCArgs, Option<MultiAddress<AccountId32, ()>>, H256),
-	#[codec(index = 6)]
-	SetScheduledEnclave(SetScheduledEnclaveArgs),
-	#[codec(index = 7)]
-	RemoveScheduledEnclave(RemoveScheduledEnclaveArgs),
 	#[codec(index = 8)]
 	BatchAll(Vec<IndirectCall>),
 }
@@ -85,19 +71,6 @@ impl<Executor: IndirectExecutor<TrustedCallSigned, Error>>
 		trace!("dispatching indirect call {:?}", self);
 		match self {
 			IndirectCall::Invoke(invoke_args) => invoke_args.dispatch(executor, ()),
-			// Litentry
-			IndirectCall::LinkIdentity(verify_id, address, hash) =>
-				verify_id.dispatch(executor, (address.clone(), *hash)),
-			IndirectCall::DeactivateIdentity(deactivate_identity, address, hash) =>
-				deactivate_identity.dispatch(executor, (address.clone(), *hash)),
-			IndirectCall::ActivateIdentity(activate_identity, address, hash) =>
-				activate_identity.dispatch(executor, (address.clone(), *hash)),
-			IndirectCall::RequestVC(request_vc, address, hash) =>
-				request_vc.dispatch(executor, (address.clone(), *hash)),
-			IndirectCall::SetScheduledEnclave(update_enclave_args) =>
-				update_enclave_args.dispatch(executor, ()),
-			IndirectCall::RemoveScheduledEnclave(remove_enclave_args) =>
-				remove_enclave_args.dispatch(executor, ()),
 			IndirectCall::BatchAll(calls) => {
 				for x in calls.clone() {
 					if let Err(e) = x.dispatch(executor, ()) {
@@ -133,16 +106,14 @@ impl<NodeMetadata: NodeMetadataTrait> FilterIntoDataFrom<NodeMetadata> for Extri
 				return None
 			},
 		};
-		let address = if let Some(signature) = xt.signature { Some(signature.0) } else { None };
 		let index = xt.call_index;
 		let call_args = &mut &xt.call_args[..];
 		trace!("ExtrinsicFilter: attempting to execute indirect call with index {:?}", index);
 		if index == metadata.post_opaque_task_call_indexes().ok()? {
 			let args = decode_and_log_error::<InvokeArgs>(call_args)?;
 			Some(IndirectCall::Invoke(args))
-		// Litentry
 		} else if index == metadata.batch_all_call_indexes().ok()? {
-			parse_batch_all(call_args, metadata, address, xt.hashed_extrinsic)
+			parse_batch_all(call_args, metadata, xt.hashed_extrinsic)
 		} else {
 			None
 		}
@@ -152,7 +123,6 @@ impl<NodeMetadata: NodeMetadataTrait> FilterIntoDataFrom<NodeMetadata> for Extri
 fn parse_batch_all<NodeMetadata: NodeMetadataTrait>(
 	call_args: &mut &[u8],
 	metadata: &NodeMetadata,
-	address: Option<MultiAddress<AccountId32, ()>>,
 	hash: H256,
 ) -> Option<IndirectCall> {
 	let call_count: Vec<()> = Decode::decode(call_args).ok()?;
@@ -163,28 +133,6 @@ fn parse_batch_all<NodeMetadata: NodeMetadataTrait>(
 		if index == metadata.post_opaque_task_call_indexes().ok()? {
 			let args = decode_and_log_error::<InvokeArgs>(call_args)?;
 			calls.push(IndirectCall::Invoke(args))
-		} else if index == metadata.link_identity_call_indexes().ok()? {
-			let args = decode_and_log_error::<LinkIdentityArgs>(call_args)?;
-			let hashed_extrinsic = hash;
-			calls.push(IndirectCall::LinkIdentity(args, address.clone(), hashed_extrinsic))
-		} else if index == metadata.deactivate_identity_call_indexes().ok()? {
-			let args = decode_and_log_error::<DeactivateIdentityArgs>(call_args)?;
-			let hashed_extrinsic = hash;
-			calls.push(IndirectCall::DeactivateIdentity(args, address.clone(), hashed_extrinsic))
-		} else if index == metadata.activate_identity_call_indexes().ok()? {
-			let args = decode_and_log_error::<ActivateIdentityArgs>(call_args)?;
-			let hashed_extrinsic = hash;
-			calls.push(IndirectCall::ActivateIdentity(args, address.clone(), hashed_extrinsic))
-		} else if index == metadata.request_vc_call_indexes().ok()? {
-			let args = decode_and_log_error::<RequestVCArgs>(call_args)?;
-			let hashed_extrinsic = hash;
-			calls.push(IndirectCall::RequestVC(args, address.clone(), hashed_extrinsic))
-		} else if index == metadata.set_scheduled_enclave_call_indexes().ok()? {
-			let args = decode_and_log_error::<SetScheduledEnclaveArgs>(call_args)?;
-			calls.push(IndirectCall::SetScheduledEnclave(args))
-		} else if index == metadata.remove_scheduled_enclave_call_indexes().ok()? {
-			let args = decode_and_log_error::<RemoveScheduledEnclaveArgs>(call_args)?;
-			calls.push(IndirectCall::RemoveScheduledEnclave(args))
 		}
 	}
 	Some(IndirectCall::BatchAll(calls))
