3dda5424e4f3da94462458a44062dc2c699ca7b0
Kasper Ziemianek <kasper.ziemianek@gmail.com>
2024-04-14 11:46:05 +0200

    remove dev feature check from integration tests

--- tee-worker/ts-tests/integration-tests/scheduled_enclave.test.ts
+++ tee-worker/ts-tests/integration-tests/scheduled_enclave.test.ts
@@ -38,47 +38,4 @@ describe('Scheduled Enclave', function () {
         assert.equal(blockNumber, 0);
         assert.equal(mrEnclave, context.mrEnclave);
     });
-
-    step('setting new scheduled enclave', async function () {
-        const buildRequest = (method: string, params?: any[]) =>
-            createJsonRpcRequest(method, params, nextRequestId(context));
-        const callRPC = async (request: JsonRpcRequest) => sendRequest(context.tee, request, context.api);
-
-        let response = await callRPC(buildRequest('state_getScheduledEnclave'));
-        let scheduledEnclaveList = context.api.createType('Vec<(u64, [u8; 32])>', response.value).toJSON() as [
-            number,
-            string
-        ][];
-
-        assert.equal(scheduledEnclaveList.length, 1);
-
-        // set new mrenclave
-        let setEnclaveResponse = await callRPC(
-            buildRequest('state_setScheduledEnclave', [20, 'some invalid mrenclave'])
-        );
-        assert.include(decodeRpcBytesAsString(setEnclaveResponse.value), 'Failed to decode mrenclave');
-
-        setEnclaveResponse = await callRPC(buildRequest('state_setScheduledEnclave', [20, '48656c6c6f20776f726c6421']));
-        assert.include(
-            decodeRpcBytesAsString(setEnclaveResponse.value),
-            'mrenclave len mismatch, expected 32 bytes long'
-        );
-
-        // valid mutation
-        const validParams = [20, '97f516a61ff59c5eab74b8a9b1b7273d6986b9c0e6c479a4010e22402ca7cee6'];
-        await callRPC(buildRequest('state_setScheduledEnclave', validParams));
-
-        // checking mutated state
-        response = await callRPC(buildRequest('state_getScheduledEnclave'));
-        scheduledEnclaveList = context.api.createType('Vec<(u64, [u8; 32])>', response.value).toJSON() as [
-            number,
-            string
-        ][];
-
-        assert.equal(scheduledEnclaveList.length, 2);
-
-        const [blockNumber, mrEnclave] = scheduledEnclaveList[1];
-        assert.equal(blockNumber, validParams[0]);
-        assert.equal(mrEnclave, `0x${validParams[1]}`);
-    });
 });
