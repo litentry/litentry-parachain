152a3d6dd5dbc87f61bff547d5d98446a329158a
Francisco Silva <francisco@kawagarbo-tech.io>
2024-04-16 09:27:02 +0000

    updating base64 usage

--- tee-worker/litentry/core/identity-verification/src/web2/twitter/mod.rs
+++ tee-worker/litentry/core/identity-verification/src/web2/twitter/mod.rs
@@ -3,7 +3,7 @@ pub use code_verifier_store::*;
 
 pub(crate) mod helpers;
 
-use base64::{encode_config, URL_SAFE_NO_PAD};
+use base64::{engine::general_purpose::URL_SAFE_NO_PAD, Engine};
 use sp_core::hashing::sha2_256;
 use std::{format, string::String};
 
@@ -17,7 +17,7 @@ pub fn get_authorize_data(client_id: &str, redirect_uri: &str) -> AuthorizeData 
 	let state = helpers::get_state_verifier();
 	let code_verifier = helpers::get_code_verifier();
 	let code_verifier_hash = sha2_256(code_verifier.as_bytes());
-	let code_challenge = encode_config(code_verifier_hash, URL_SAFE_NO_PAD);
+	let code_challenge = URL_SAFE_NO_PAD.encode(code_verifier_hash);
 
 	let authorize_url = format!(
 		"https://twitter.com/i/oauth2/authorize?response_type=code&client_id={}&redirect_uri={}&scope=tweet.read%20users.read%20follows.read%20follows.write&state={}&code_challenge={}&code_challenge_method=S256",
