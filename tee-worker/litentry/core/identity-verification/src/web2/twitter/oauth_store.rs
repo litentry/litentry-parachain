adeee7b49fa5338021d7513ea611ffb7c5f70222
Francisco Silva <francisco@kawagarbo-tech.io>
2024-04-24 16:05:10 +0000

    refactoring oauth store

--- tee-worker/litentry/core/identity-verification/src/web2/twitter/oauth_store.rs
+++ tee-worker/litentry/core/identity-verification/src/web2/twitter/oauth_store.rs
@@ -1,6 +1,3 @@
-#[cfg(feature = "sgx")]
-use thiserror_sgx as thiserror;
-
 #[cfg(feature = "std")]
 use std::sync::RwLock;
 #[cfg(feature = "sgx")]
@@ -14,31 +11,32 @@ use hex_sgx as hex;
 use codec::Encode;
 use core::result::Result;
 use lazy_static::lazy_static;
-use std::{collections::HashMap, string::String, sync::Arc};
+use lru::LruCache;
+use std::{num::NonZeroUsize, string::String};
 
 use litentry_primitives::ParentchainAccountId as AccountId;
 
 lazy_static! {
-	static ref TWITTER_CODE_VERIFIER_STORE: Arc<RwLock<HashMap<String, (String, String)>>> =
-		Arc::new(RwLock::new(HashMap::new()));
+	static ref STORE: RwLock<LruCache<String, (String, String)>> =
+		RwLock::new(LruCache::new(NonZeroUsize::new(250).unwrap()));
 }
 
 pub struct OAuthStore;
 
 impl OAuthStore {
 	pub fn save_data(account_id: AccountId, code: String, state: String) -> Result<(), String> {
-		TWITTER_CODE_VERIFIER_STORE
+		STORE
 			.write()
 			.map_err(|_| String::from("Lock poisoning"))?
-			.insert(hex::encode(account_id.encode()), (code, state));
+			.put(hex::encode(account_id.encode()), (code, state));
 		Ok(())
 	}
 
 	pub fn get_data(account_id: &AccountId) -> Result<Option<(String, String)>, String> {
-		let data = TWITTER_CODE_VERIFIER_STORE
+		let data = STORE
 			.write()
 			.map_err(|_| String::from("Lock poisoning"))?
-			.remove(hex::encode(account_id.encode()).as_str());
+			.pop(hex::encode(account_id.encode()).as_str());
 		Ok(data)
 	}
 }
