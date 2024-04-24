a5e6307b8b83910ae9344f9bc26f10b83ff10a57
higherordertech <higherordertech>
2024-04-24 17:41:21 +1000

    feat: P-693 add MCRT token holding amount VC, refactored test cases and supported customized assertion ranges

--- tee-worker/litentry/core/service/src/web3_token/token_balance/common.rs
+++ tee-worker/litentry/core/service/src/web3_token/token_balance/common.rs
@@ -22,46 +22,82 @@ extern crate sgx_tstd as std;
 
 use core::result::Result;
 
-use lc_common::web3_token::{TokenAddress, TokenDecimals};
-use lc_data_providers::nodereal_jsonrpc::{
-	FungibleApiList, GetTokenBalance20Param, Web3NetworkNoderealJsonrpcClient,
+use lc_common::{
+	abort_strategy::{loop_with_abort_strategy, AbortStrategy, LoopControls},
+	web3_token::{TokenAddress, TokenDecimals},
 };
+use lc_data_providers::{
+	moralis::{BalanceApiList, MoralisClient},
+	nodereal_jsonrpc::{FungibleApiList, GetTokenBalance20Param, Web3NetworkNoderealJsonrpcClient},
+};
+use log::error;
 
 use crate::*;
 
 // only support to get balance for non-native token
-pub fn get_balance_from_evm(
+pub fn get_balance(
 	addresses: Vec<(Web3Network, String)>,
 	token_type: Web3TokenType,
 	data_provider_config: &DataProviderConfig,
 ) -> Result<f64, Error> {
 	let mut total_balance = 0_f64;
 
-	for address in addresses.iter() {
-		let network = address.0;
-		let decimals = token_type.get_decimals(network);
-		let param = GetTokenBalance20Param {
-			contract_address: token_type.get_token_address(network).unwrap_or_default().into(),
-			address: address.1.clone(),
-			block_number: "latest".into(),
-		};
+	loop_with_abort_strategy(
+		addresses,
+		|address| {
+			let network = address.0;
+			let token_address = token_type.get_token_address(network).unwrap_or_default();
 
-		match network {
-			Web3Network::Bsc | Web3Network::Ethereum => {
-				if let Some(mut client) =
-					network.create_nodereal_jsonrpc_client(data_provider_config)
-				{
-					match client.get_token_balance_20(&param, false) {
-						Ok(balance) => {
-							total_balance += calculate_balance_with_decimals(balance, decimals);
+			match network {
+				Web3Network::Bsc | Web3Network::Ethereum => {
+					let decimals = token_type.get_decimals(network);
+					match network.create_nodereal_jsonrpc_client(data_provider_config) {
+						Some(mut client) => {
+							let param = GetTokenBalance20Param {
+								contract_address: token_address.into(),
+								address: address.1.clone(),
+								block_number: "latest".into(),
+							};
+							let result = client.get_token_balance_20(&param, false);
+							match result {
+								Ok(balance) => {
+									total_balance +=
+										calculate_balance_with_decimals(balance, decimals);
+									Ok(LoopControls::Continue)
+								},
+								Err(err) => Err(err.into_error_detail()),
+							}
 						},
-						Err(err) => return Err(err.into_error_detail()),
+						None => Ok(LoopControls::Continue),
 					}
-				}
-			},
-			_ => {},
-		}
-	}
+				},
+				Web3Network::Solana => {
+					let mut client = MoralisClient::new(data_provider_config);
+					let result =
+						client.get_solana_tokens_balance_by_wallet(address.1.clone(), false);
+
+					match result {
+						Ok(items) => match items.iter().find(|&item| item.mint == token_address) {
+							Some(item) => match item.amount.parse::<f64>() {
+								Ok(balance) => {
+									total_balance += balance;
+									Ok(LoopControls::Continue)
+								},
+								Err(err) => {
+									error!("Failed to parse {} to f64: {}", item.amount, err);
+									Err(Error::ParseError)
+								},
+							},
+							None => Ok(LoopControls::Continue),
+						},
+						Err(err) => Err(err.into_error_detail()),
+					}
+				},
+				_ => Ok(LoopControls::Continue),
+			}
+		},
+		AbortStrategy::FailFast::<fn(&_) -> bool>,
+	)?;
 
 	Ok(total_balance)
 }
