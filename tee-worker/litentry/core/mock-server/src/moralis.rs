30a3334da0afc3266897b2f8f983c8ec69df6af2
Kasper Ziemianek <kasper.ziemianek@gmail.com>
2024-06-17 15:29:30 +0200

    fmt

--- tee-worker/litentry/core/mock-server/src/moralis.rs
+++ tee-worker/litentry/core/mock-server/src/moralis.rs
@@ -17,11 +17,16 @@
 
 use std::collections::HashMap;
 
-use lc_data_providers::moralis::{GetEvmTokenBalanceByWalletResponse, GetNftsByWalletResult, GetSolanaNativeBalanceByWalletResponse, GetSolanaTokenBalanceByWalletResponse, MoralisPageResponse};
+use lc_data_providers::moralis::{
+	GetEvmTokenBalanceByWalletResponse, GetNftsByWalletResult,
+	GetSolanaNativeBalanceByWalletResponse, GetSolanaTokenBalanceByWalletResponse,
+	MoralisPageResponse,
+};
 
 use warp::{http::Response, Filter};
 
-pub(crate) fn query_nft() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+pub(crate) fn query_nft() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone
+{
 	warp::get()
 		.and(warp::path!("moralis" / String / "nft"))
 		.and(warp::query::<HashMap<String, String>>())
@@ -54,29 +59,27 @@ pub(crate) fn query_nft() -> impl Filter<Extract = impl warp::Reply, Error = war
 		})
 }
 
-pub(crate) fn query_erc20() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+pub(crate) fn query_erc20(
+) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
 	warp::get()
 		.and(warp::path!("moralis" / String / "erc20"))
 		.and(warp::query::<HashMap<String, String>>())
 		.map(move |address, _| {
 			if address == "0x49ad262c49c7aa708cc2df262ed53b64a17dd5ee" {
-				let body = vec![
-					GetEvmTokenBalanceByWalletResponse {
-						token_address: "0x49ad262c49c7aa708cc2df262ed53b64a17dd5ee".to_string(),
-						symbol: "SYM".to_string(),
-						name: "Name #1".to_string(),
-						logo: None,
-						thumbnail: None,
-						decimals: 10,
-						balance: "1000".to_string(),
-						possible_spam: false,
-						verified_contract: true,
-						total_supply: "100000000".to_string(),
-						total_supply_formatted: "100000000".to_string(),
-						percentage_relative_to_total_supply: 0.00001
-
-					}
-				];
+				let body = vec![GetEvmTokenBalanceByWalletResponse {
+					token_address: "0x49ad262c49c7aa708cc2df262ed53b64a17dd5ee".to_string(),
+					symbol: "SYM".to_string(),
+					name: "Name #1".to_string(),
+					logo: None,
+					thumbnail: None,
+					decimals: 10,
+					balance: "1000".to_string(),
+					possible_spam: false,
+					verified_contract: true,
+					total_supply: "100000000".to_string(),
+					total_supply_formatted: "100000000".to_string(),
+					percentage_relative_to_total_supply: 0.00001,
+				}];
 				Response::builder().body(serde_json::to_string(&body).unwrap())
 			} else {
 				let body: Vec<GetEvmTokenBalanceByWalletResponse> = vec![];
