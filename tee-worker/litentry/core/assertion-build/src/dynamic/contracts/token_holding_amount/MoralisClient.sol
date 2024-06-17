59a5d7feedfe5153ca70c14acf4624397d639fa5
Kasper Ziemianek <kasper.ziemianek@gmail.com>
2024-06-17 12:46:46 +0200

    moralis client

--- tee-worker/litentry/core/assertion-build/src/dynamic/contracts/token_holding_amount/MoralisClient.sol
+++ tee-worker/litentry/core/assertion-build/src/dynamic/contracts/token_holding_amount/MoralisClient.sol
@@ -19,8 +19,18 @@
 pragma solidity ^0.8.8;
 
 import "../libraries/Http.sol";
-import "../libraries/Http.sol";
-import "../libraries/Utils.sol";
+import "../libraries/Json.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.0/contracts/utils/Strings.sol";
+
+struct SolanaTokenBalance {
+    string mint;
+    string amount;
+}
+
+struct EvmTokenBalance {
+    string tokenAddress;
+    string balance;
+}
 
 library MoralisClient {
     function getSolanaNativeBalance(
@@ -30,22 +40,159 @@ library MoralisClient {
     ) internal returns (bool, string memory) {
         url = string(abi.encodePacked(url, "/", account, "/balance"));
 
-        HttpHeader[] memory headers = new HttpHeader[](0);
+        HttpHeader[] memory headers = new HttpHeader[](1);
         headers[0] = HttpHeader("X-API-Key", apiKey);
-        return Http.GetString(url, "/solana", headers);
+        (bool balanceSuccess, string memory balanceResponse) = Http.Get(
+            url,
+            headers
+        );
+        if (balanceSuccess) {
+            (bool solanaSuccess, string memory solana) = Json.getString(
+                balanceResponse,
+                "/solana"
+            );
+            if (solanaSuccess) {
+                return (solanaSuccess, solana);
+            } else {
+                return (false, "");
+            }
+        } else {
+            return (false, "");
+        }
     }
 
-    // function getSolanaTokensBalance(
-    //     string memory url,
-    //     string memory apiKey,
-    //     string memory account
-    // ) internal returns (bool, string memory) {
-    //     // this return array of tokens so we need to find a way to iterate over it - currently that's not possible
-    //     url = string(abi.encodePacked(url, "/", account, "/tokens"));
-    //     HttpHeader[] memory headers = new HttpHeader[](0);
-    //     headers[0] = HttpHeader("X-API-Key", apiKey);
-    //     return Http.GetString(url, "/solana", headers);
+    function getSolanaTokensBalance(
+        string memory url,
+        string memory apiKey,
+        string memory account
+    ) internal returns (bool, SolanaTokenBalance[] memory) {
+        url = string(abi.encodePacked(url, "/", account, "/tokens"));
+        HttpHeader[] memory headers = new HttpHeader[](1);
+        headers[0] = HttpHeader("X-API-Key", apiKey);
+        (bool tokensSuccess, string memory tokensResponse) = Http.Get(
+            url,
+            headers
+        );
+        if (tokensSuccess) {
+            (bool arrayLenSuccess, int64 arrayLen) = Json.getArrayLen(
+                tokensResponse,
+                ""
+            );
+            if (arrayLenSuccess) {
+                SolanaTokenBalance[] memory balances = new SolanaTokenBalance[](
+                    uint256(int256(arrayLen))
+                );
+                for (uint256 i = 0; i < uint256(int256(arrayLen)); i++) {
+                    (bool mintSuccess, string memory mint) = Json.getString(
+                        tokensResponse,
+                        string(
+                            abi.encodePacked("/", Strings.toString(i), "/mint")
+                        )
+                    );
+                    (bool amountSuccess, string memory amount) = Json.getString(
+                        tokensResponse,
+                        string(
+                            abi.encodePacked(
+                                "/",
+                                Strings.toString(i),
+                                "/amount"
+                            )
+                        )
+                    );
+                    if (!mintSuccess || !amountSuccess) {
+                        return (false, new SolanaTokenBalance[](0));
+                    } else {
+                        balances[i] = SolanaTokenBalance(mint, amount);
+                    }
+                }
+                return (true, balances);
+            } else {
+                return (false, new SolanaTokenBalance[](0));
+            }
+        } else {
+            return (false, new SolanaTokenBalance[](0));
+        }
+    }
 
-    // }
-
+    function getErcTokensBalance(
+        string memory url,
+        string memory apiKey,
+        string memory account,
+        string memory chain,
+        string[] memory tokenAddresses
+    ) internal returns (bool, EvmTokenBalance[] memory) {
+        url = string(
+            abi.encodePacked(url, "/", account, "/erc20", "?chain=", chain)
+        );
+        HttpHeader[] memory headers = new HttpHeader[](1);
+        headers[0] = HttpHeader("X-API-Key", apiKey);
+        if (tokenAddresses.length > 0) {
+            url = string(abi.encodePacked(url, "&"));
+        }
+        for (uint256 i = 0; i < tokenAddresses.length; i++) {
+            url = string(
+                abi.encodePacked(
+                    url,
+                    "token_addresses[",
+                    Strings.toString(i),
+                    "]=",
+                    tokenAddresses[i]
+                )
+            );
+            if (i != tokenAddresses.length - 1) {
+                url = string(abi.encodePacked(url, "&"));
+            }
+        }
+        (bool tokensSuccess, string memory tokensResponse) = Http.Get(
+            url,
+            headers
+        );
+        if (tokensSuccess) {
+            (bool arrayLenSuccess, int64 arrayLen) = Json.getArrayLen(
+                tokensResponse,
+                ""
+            );
+            if (arrayLenSuccess) {
+                EvmTokenBalance[] memory balances = new EvmTokenBalance[](
+                    uint256(int256(arrayLen))
+                );
+                for (uint256 i = 0; i < uint256(int256(arrayLen)); i++) {
+                    (
+                        bool tokenAddressSuccess,
+                        string memory tokenAddress
+                    ) = Json.getString(
+                            tokensResponse,
+                            string(
+                                abi.encodePacked(
+                                    "/",
+                                    Strings.toString(i),
+                                    "/token_address"
+                                )
+                            )
+                        );
+                    (bool balanceSuccess, string memory balance) = Json
+                        .getString(
+                            tokensResponse,
+                            string(
+                                abi.encodePacked(
+                                    "/",
+                                    Strings.toString(i),
+                                    "/balance"
+                                )
+                            )
+                        );
+                    if (!tokenAddressSuccess || !balanceSuccess) {
+                        return (false, new EvmTokenBalance[](0));
+                    } else {
+                        balances[i] = EvmTokenBalance(tokenAddress, balance);
+                    }
+                }
+                return (true, balances);
+            } else {
+                return (false, new EvmTokenBalance[](0));
+            }
+        } else {
+            return (false, new EvmTokenBalance[](0));
+        }
+    }
 }
