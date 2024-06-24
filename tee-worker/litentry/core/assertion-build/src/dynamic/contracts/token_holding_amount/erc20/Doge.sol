281064982c80b23c9b4a2e5c480a116bdf8e176c
0xverin <104152026+0xverin@users.noreply.github.com>
2024-06-24 15:59:47 +0800

    p-867 Refactoring extra params (#2830)
    * refactor TokenHoldingAmount
    
    * refactor erc20&brc20
    
    * add isStringsEqual
    
    * refactor erc20 token
    
    * refactor brc20 tokens
    
    * remove console
    
    * reset url
    
    * add ERC20TokenLibrary
    
    * add BRC20TokenLibrary
    
    * getTokenInfo for erc20
    
    * getTokenInfo for brc20
    
    * move if statement
    
    * add mapping for tokens
    
    * remove unuse setting
    
    * fix comment
    
    * replace brc20&erc20
    
    * fix mod bytecode
    
    * add erc20 tests
    
    * fmt
    
    * merge brc20&&erc20
    
    * add mod test
    
    * fix ci
    
    * fix ci one more
    
    ---------
    
    Co-authored-by: will.li <120463031+higherordertech@users.noreply.github.com>

--- tee-worker/litentry/core/assertion-build/src/dynamic/contracts/token_holding_amount/erc20/Doge.sol
+++ tee-worker/litentry/core/assertion-build/src/dynamic/contracts/token_holding_amount/erc20/Doge.sol
@@ -17,40 +17,38 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.8;
 
-import { ERC20 } from "../ERC20.sol";
 import "../../libraries/Identities.sol";
+import "../Constants.sol";
 
-contract Doge is ERC20 {
-	constructor() {
-		// Initialize network token addresses
-		networkTokenAddresses[
-			Web3Networks.Ethereum
-		] = "";
-		networkTokenAddresses[
-			Web3Networks.Bsc
-		] = "0xba2ae424d960c26247dd6c32edc70b295c744c43";
-		// Add more addresses as needed
+library Doge {
+	function getTokenBscAddress() internal pure returns (string memory) {
+		return "0xba2ae424d960c26247dd6c32edc70b295c744c43";
+	}
+	function getTokenEthereumAddress() internal pure returns (string memory) {
+		return "";
 	}
 
-	function getTokenName() internal pure override returns (string memory) {
+	function getTokenName() internal pure returns (string memory) {
 		return "doge";
 	}
 
-	function getTokenRanges()
-		internal
-		pure
-		override
-		returns (uint256[] memory)
-	{
+	function getTokenRanges() internal pure returns (uint256[] memory) {
 		uint256[] memory ranges = new uint256[](7);
-		ranges[0] = 0 * decimals_factor;
-		ranges[1] = 1000 * decimals_factor;
-		ranges[2] = 5000 * decimals_factor;
-		ranges[3] = 20000 * decimals_factor;
-		ranges[4] = 50000 * decimals_factor;
-		ranges[5] = 100000 * decimals_factor;
-		ranges[6] = 300000 * decimals_factor;
+		ranges[0] = 0 * Constants.decimals_factor;
+		ranges[1] = 1000 * Constants.decimals_factor;
+		ranges[2] = 5000 * Constants.decimals_factor;
+		ranges[3] = 20000 * Constants.decimals_factor;
+		ranges[4] = 50000 * Constants.decimals_factor;
+		ranges[5] = 100000 * Constants.decimals_factor;
+		ranges[6] = 300000 * Constants.decimals_factor;
 
 		return ranges;
+	}
+	function getTokenNetworks() internal pure returns (uint32[] memory) {
+		uint32[] memory networks = new uint32[](2);
+		networks[0] = Web3Networks.Ethereum;
+		networks[1] = Web3Networks.Bsc;
+
+		return networks;
 	}
 }
