3fe0178892684e927487ba8c12e890a99df8dd36
Kailai Wang <Kailai.Wang@hotmail.com>
2024-07-09 20:58:56 +0000

    Revert "add rsa-wrapper"
    This reverts commit 440a61ec9633a8bfcb297a53013767414a4350c8.

--- bitacross-worker/core-primitives/test/src/mock/shielding_crypto_mock.rs
+++ bitacross-worker/core-primitives/test/src/mock/shielding_crypto_mock.rs
@@ -18,41 +18,19 @@
 use itp_sgx_crypto::{
 	ed25519_derivation::DeriveEd25519, ShieldingCryptoDecrypt, ShieldingCryptoEncrypt,
 };
-use sgx_crypto::rsa::Rsa3072KeyPair;
+use sgx_crypto_helper::{rsa3072::Rsa3072KeyPair, RsaKeyPair};
 use sp_core::ed25519::Pair as Ed25519Pair;
-use std::{format, vec::Vec};
-
-#[cfg(all(not(feature = "sgx"), feature = "std"))]
-use lc_rsa_wrapper::{RsaWrapperCreate, RsaWrapperDecrypt, RsaWrapperEncrypt};
+use std::vec::Vec;
 
 #[derive(Clone)]
 pub struct ShieldingCryptoMock {
 	key: Rsa3072KeyPair,
-	// this flag forces to use dummy(identity) encryption and decryption, otherwise
-	// `test_threadpool_behaviour` won't pass, the root cause is unclear, but I suspect that
-	// it's related to multi-threaded key creation and encryption - the `encrypt` call never
-	// returns on another thread (somehow).
-	//
-	// since this struct is used in `std` only and `lc_rsa_wrapper` is a workaround anyway,
-	// I consider it fine for now
-	use_dummy_enc_dec: bool,
 }
 
 impl Default for ShieldingCryptoMock {
 	fn default() -> Self {
-		Self::new(false)
-	}
-}
-
-impl ShieldingCryptoMock {
-	pub fn new(use_dummy_enc_dec: bool) -> Self {
 		ShieldingCryptoMock {
-			use_dummy_enc_dec,
-			#[cfg(all(not(feature = "std"), feature = "sgx"))]
-			key: Rsa3072KeyPair::create().expect("default RSA3072 key for shielding key mock"),
-			#[cfg(all(not(feature = "sgx"), feature = "std"))]
-			key: Rsa3072KeyPair::create_with_rsa_wrapper()
-				.expect("default RSA3072 key for shielding key mock"),
+			key: Rsa3072KeyPair::new().expect("default RSA3072 key for shielding key mock"),
 		}
 	}
 }
@@ -61,21 +39,7 @@ impl ShieldingCryptoEncrypt for ShieldingCryptoMock {
 	type Error = itp_sgx_crypto::Error;
 
 	fn encrypt(&self, data: &[u8]) -> Result<Vec<u8>, Self::Error> {
-		if self.use_dummy_enc_dec {
-			return Ok(data.to_vec())
-		}
-
-		#[cfg(all(not(feature = "std"), feature = "sgx"))]
-		return self
-			.key
-			.encrypt(data)
-			.map_err(|e| itp_sgx_crypto::Error::Other(format!("encrypt error: {:?}", e).into()))
-
-		#[cfg(all(not(feature = "sgx"), feature = "std"))]
-		self.key
-			.public_key()
-			.encrypt_with_rsa_wrapper(data)
-			.map_err(|e| itp_sgx_crypto::Error::Other(format!("encrypt error: {:?}", e).into()))
+		self.key.encrypt(data)
 	}
 }
 
@@ -83,21 +47,7 @@ impl ShieldingCryptoDecrypt for ShieldingCryptoMock {
 	type Error = itp_sgx_crypto::Error;
 
 	fn decrypt(&self, data: &[u8]) -> Result<Vec<u8>, Self::Error> {
-		if self.use_dummy_enc_dec {
-			return Ok(data.to_vec())
-		}
-
-		#[cfg(all(not(feature = "std"), feature = "sgx"))]
-		return self
-			.key
-			.decrypt(data)
-			.map_err(|e| itp_sgx_crypto::Error::Other(format!("decrypt error: {:?}", e).into()))
-
-		#[cfg(all(not(feature = "sgx"), feature = "std"))]
-		self.key
-			.private_key()
-			.decrypt_with_rsa_wrapper(data)
-			.map_err(|e| itp_sgx_crypto::Error::Other(format!("decrypt error: {:?}", e).into()))
+		self.key.decrypt(data)
 	}
 }
 
