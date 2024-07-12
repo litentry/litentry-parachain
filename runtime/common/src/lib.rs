2c3d72bcfd15e70b43958187ba5f732b6d4a348d
Minqi Wang <wangminqi@aliyun.com>
2024-07-12 12:56:02 +0800

    feat: change basic balance decimal type

--- runtime/common/src/lib.rs
+++ runtime/common/src/lib.rs
@@ -77,10 +77,10 @@ pub const WEIGHT_PER_GAS: u64 = WEIGHT_REF_TIME_PER_SECOND / GAS_PER_SECOND;
 pub mod currency {
 	use core_primitives::Balance;
 
-	pub const UNIT: Balance = 1_000_000_000_000;
-	pub const DOLLARS: Balance = UNIT; // 1_000_000_000_000
-	pub const CENTS: Balance = DOLLARS / 100; // 10_000_000_000
-	pub const MILLICENTS: Balance = CENTS / 1_000; // 10_000_000
+	pub const UNIT: Balance = 1_000_000_000_000_000_000;
+	pub const DOLLARS: Balance = UNIT; // 1_000_000_000_000_000_000
+	pub const CENTS: Balance = DOLLARS / 100; // 10_000_000_000_000_000
+	pub const MILLICENTS: Balance = CENTS / 1_000; // 10_000_000_000_000
 
 	/// The existential deposit.
 	pub const EXISTENTIAL_DEPOSIT: Balance = 10 * CENTS;
