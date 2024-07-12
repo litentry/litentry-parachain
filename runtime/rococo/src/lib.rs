8b6a76e3f728bed15440caae9cd38cece7a89368
Kailai Wang <Kailai.Wang@hotmail.com>
2024-07-12 11:38:42 +0000

    Bump runtime versions

--- runtime/rococo/src/lib.rs
+++ runtime/rococo/src/lib.rs
@@ -245,7 +245,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	impl_name: create_runtime_str!("rococo-parachain"),
 	authoring_version: 1,
 	// same versioning-mechanism as polkadot: use last digit for minor updates
-	spec_version: 9182,
+	spec_version: 9183,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
