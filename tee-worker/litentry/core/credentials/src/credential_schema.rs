555315d222b77c5c2cf78593764e83dcad6e0bd2
will.li <120463031+higherordertech@users.noreply.github.com>
2024-07-12 15:44:13 +1000

    feat: P-924 implemented NFT token holder VC for MVP on Ethereum (#2889)
    Co-authored-by: higherordertech <higherordertech>

--- tee-worker/litentry/core/credentials/src/credential_schema.rs
+++ tee-worker/litentry/core/credentials/src/credential_schema.rs
@@ -109,7 +109,7 @@ pub fn get_schema_url(assertion: &Assertion) -> Option<String> {
 
 		Assertion::PlatformUser(_) => Some(format!("{BASE_URL}/24-platform-user/1-1-1.json")),
 
-		Assertion::NftHolder(_) => Some(format!("{BASE_URL}/26-nft-holder/1-1-1.json")),
+		Assertion::NftHolder(_) => Some(format!("{BASE_URL}/26-nft-holder/1-1-2.json")),
 
 		Assertion::TokenHoldingAmount(_) =>
 			Some(format!("{BASE_URL}/25-token-holding-amount/1-1-3.json")),
