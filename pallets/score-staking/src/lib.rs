6756c1caafe5fdc52064f043720f96601de6da65
Kailai Wang <Kailai.Wang@hotmail.com>
2024-07-13 23:39:48 +0000

    initial version

--- pallets/score-staking/src/lib.rs
+++ pallets/score-staking/src/lib.rs
@@ -43,6 +43,7 @@
 
 use core_primitives::{DAYS, YEARS};
 use frame_support::traits::Currency;
+use pallet_parachain_staking as ParaStaking;
 use sp_core::crypto::AccountId32;
 use sp_runtime::{traits::CheckedSub, Perbill};
 use sp_std::marker::PhantomData;
@@ -67,10 +68,11 @@ pub mod pallet {
 	use core_primitives::Identity;
 	use frame_support::{
 		dispatch::DispatchResultWithPostInfo,
-		pallet_prelude::{ValueQuery, *},
-		traits::{Imbalance, ReservableCurrency, StorageVersion},
+		pallet_prelude::*,
+		traits::{Imbalance, LockableCurrency, ReservableCurrency, StorageVersion},
 	};
 	use frame_system::pallet_prelude::*;
+	use sp_runtime::traits::Zero;
 
 	/// The current storage version.
 	const STORAGE_VERSION: StorageVersion = StorageVersion::new(0);
@@ -81,8 +83,10 @@ pub mod pallet {
 	pub struct Pallet<T>(_);
 
 	#[pallet::config]
-	pub trait Config: frame_system::Config + pallet_parachain_staking::Config {
-		type Currency: Currency<Self::AccountId> + ReservableCurrency<Self::AccountId>;
+	pub trait Config: frame_system::Config + ParaStaking::Config {
+		type Currency: Currency<Self::AccountId>
+			+ ReservableCurrency<Self::AccountId>
+			+ LockableCurrency<Self::AccountId>;
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
 		#[pallet::constant]
 		type YearlyIssuance: Get<BalanceOf<Self>>;
@@ -126,7 +130,11 @@ pub mod pallet {
 		PoolStarted { start_block: BlockNumberFor<T> },
 		PoolStopped {},
 		ScoreFeederSet { new_score_feeder: Option<T::AccountId> },
+		RoundConfigSet { new_config: RoundSetting },
 		ScoreUpdated { who: Identity, new_score: Score },
+		ScoreRemoved { who: Identity },
+		ScoreCleared {},
+		RewardCalculated { total: BalanceOf<T>, distributed: BalanceOf<T> },
 		RewardClaimed { who: T::AccountId, amount: BalanceOf<T> },
 	}
 
@@ -210,8 +218,37 @@ pub mod pallet {
 			weight = weight.saturating_add(T::DbWeight::get().reads_writes(1, 1));
 
 			// 2. calculate payout
-			let total = (T::YearlyInflation::get() * T::YearlyIssuance::get() / YEARS.into()) *
-				previous_duration.into();
+			let round_reward: BalanceOf<T> = (T::YearlyInflation::get() * T::YearlyIssuance::get() /
+				YEARS.into()) * previous_duration.into();
+
+			let total_stake = ParaStaking::Pallet::<T>::total();
+			let total_score =
+				Scores::<T>::iter_values().fold(0u32, |a, b| a.checked_add(b.score).unwrap_or(a));
+			let score_coef = Self::round_config().score_coefficient;
+			let stake_coef = Self::round_config().stake_coefficient;
+
+			let mut total_user_reward = BalanceOf::<T>::zero();
+
+			for (a, mut p) in Scores::<T>::iter() {
+				let user_stake = ParaStaking::Pallet::<T>::delegator_state(&a)
+					.map(|s| s.total)
+					.unwrap_or_default();
+				let user_reward: BalanceOf<T> = Perbill::from_rational(
+					stake_coef * user_stake +
+						score_coef * <u32 as Into<ParaStaking::BalanceOf<T>>>::into(p.score),
+					stake_coef * total_stake +
+						score_coef * <u32 as Into<ParaStaking::BalanceOf<T>>>::into(total_score),
+				) * round_reward;
+				p.unpaid += user_reward;
+				total_user_reward += user_reward;
+				Scores::<T>::insert(&a, p);
+				weight = weight.saturating_add(T::DbWeight::get().reads_writes(2, 1));
+			}
+
+			Self::deposit_event(Event::<T>::RewardCalculated {
+				total: round_reward,
+				distributed: total_user_reward,
+			});
 
 			weight
 		}
@@ -271,17 +308,29 @@ pub mod pallet {
 
 		#[pallet::call_index(3)]
 		#[pallet::weight((195_000_000, DispatchClass::Normal))]
+		pub fn set_round_config(
+			origin: OriginFor<T>,
+			config: RoundSetting,
+		) -> DispatchResultWithPostInfo {
+			T::AdminOrigin::ensure_origin(origin)?;
+			RoundConfig::<T>::put(config);
+			Self::deposit_event(Event::RoundConfigSet { new_config: config });
+			Ok(Pays::No.into())
+		}
+
+		#[pallet::call_index(4)]
+		#[pallet::weight((195_000_000, DispatchClass::Normal))]
 		pub fn update_score(
 			origin: OriginFor<T>,
 			user: Identity,
 			score: Score,
 		) -> DispatchResultWithPostInfo {
+			T::ScoreFeedOrigin::ensure_origin(origin)?;
 			let account = T::AccountIdConvert::convert(
 				user.to_account_id().ok_or(Error::<T>::ConvertIdentityFailed)?,
 			);
-			T::ScoreFeedOrigin::ensure_origin(origin)?;
 			Scores::<T>::try_mutate(&account, |payment| {
-				let state = pallet_parachain_staking::Pallet::<T>::delegator_state(&account)
+				let state = ParaStaking::Pallet::<T>::delegator_state(&account)
 					.ok_or(Error::<T>::UserNotStaked)?;
 				ensure!(state.total > 0u32.into(), Error::<T>::UserStakedAmountZero);
 
@@ -298,7 +347,31 @@ pub mod pallet {
 			Ok(Pays::No.into())
 		}
 
-		#[pallet::call_index(4)]
+		#[pallet::call_index(5)]
+		#[pallet::weight((195_000_000, DispatchClass::Normal))]
+		pub fn remove_score(origin: OriginFor<T>, user: Identity) -> DispatchResultWithPostInfo {
+			// only admin can remove entries in `Scores`
+			T::AdminOrigin::ensure_origin(origin)?;
+			let account = T::AccountIdConvert::convert(
+				user.to_account_id().ok_or(Error::<T>::ConvertIdentityFailed)?,
+			);
+			ensure!(Scores::<T>::contains_key(&account), Error::<T>::UserNotExist);
+			Scores::<T>::remove(&account);
+			Self::deposit_event(Event::ScoreRemoved { who: user });
+			Ok(Pays::No.into())
+		}
+
+		#[pallet::call_index(6)]
+		#[pallet::weight((195_000_000, DispatchClass::Normal))]
+		pub fn clear_score(origin: OriginFor<T>) -> DispatchResultWithPostInfo {
+			// only admin can clear all entries in `Scores`
+			T::AdminOrigin::ensure_origin(origin)?;
+			let _ = Scores::<T>::clear(u32::MAX, None);
+			Self::deposit_event(Event::ScoreCleared {});
+			Ok(Pays::No.into())
+		}
+
+		#[pallet::call_index(7)]
 		#[pallet::weight((195_000_000, DispatchClass::Normal))]
 		pub fn claim(origin: OriginFor<T>, amount: BalanceOf<T>) -> DispatchResultWithPostInfo {
 			let account = ensure_signed(origin)?;
@@ -318,7 +391,7 @@ pub mod pallet {
 			})
 		}
 
-		#[pallet::call_index(5)]
+		#[pallet::call_index(8)]
 		#[pallet::weight((195_000_000, DispatchClass::Normal))]
 		pub fn claim_all(origin: OriginFor<T>) -> DispatchResultWithPostInfo {
 			let account = ensure_signed(origin.clone())?;
