cdeda55dc5c85b9dd853b61c55b3d8a264103b76
Minqi Wang <wangminqi@aliyun.com>
2024-07-15 16:46:50 +0800

    debug: copy right

--- precompiles/assets-erc20/src/tests.rs
+++ precompiles/assets-erc20/src/tests.rs
@@ -1,20 +1,18 @@
-// This file is part of Astar.
-
-// Copyright (C) 2019-2023 Stake Technologies Pte.Ltd.
-// SPDX-License-Identifier: GPL-3.0-or-later
-
-// Astar is free software: you can redistribute it and/or modify
+// Copyright 2020-2024 Trust Computing GmbH.
+// This file is part of Litentry.
+//
+// Litentry is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-
-// Astar is distributed in the hope that it will be useful,
+//
+// Litentry is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-
+//
 // You should have received a copy of the GNU General Public License
-// along with Astar. If not, see <http://www.gnu.org/licenses/>.
+// along with Litentry.  If not, see <https://www.gnu.org/licenses/>.
 
 // Copyright 2019-2022 PureStake Inc.
 // Copyright 2022      Stake Technologies
