6795216010cb07dcff4089b36258e7a5aea3771c
Minqi Wang <wangminqi@aliyun.com>
2024-07-17 10:52:41 +0800

    chore

--- runtime/rococo/src/asset_config.rs
+++ runtime/rococo/src/asset_config.rs
@@ -52,16 +52,6 @@ impl AddressToAssetId<AssetId> for Runtime {
 	}
 }
 
-pub struct AssetsBenchmarkHelper;
-#[cfg(feature = "runtime-benchmarks")]
-impl<AssetIdParameter: From<u128>> pallet_assets::BenchmarkHelper<AssetIdParameter>
-	for AssetsBenchmarkHelper
-{
-	fn create_asset_id_parameter(id: u32) -> AssetIdParameter {
-		AssetId::from(id).into()
-	}
-}
-
 impl pallet_assets::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 	type Balance = Balance;
@@ -81,8 +71,6 @@ impl pallet_assets::Config for Runtime {
 	type RemoveItemsLimit = ConstU32<1000>;
 	type AssetIdParameter = Compact<AssetId>;
 	type CallbackHandle = ();
-	#[cfg(feature = "runtime-benchmarks")]
-	type BenchmarkHelper = AssetsBenchmarkHelper;
 }
 
 impl pallet_asset_manager::Config for Runtime {
