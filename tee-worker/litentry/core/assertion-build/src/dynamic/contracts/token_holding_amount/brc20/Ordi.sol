f9aeedfc097bf800b53d3e5d4e8b55d2bfcf7f02
higherordertech <higherordertech>
2024-07-18 15:59:32 +1000

    feat: P-926 support rest 4 bitcoin networks for brc20 contract

--- tee-worker/litentry/core/assertion-build/src/dynamic/contracts/token_holding_amount/brc20/Ordi.sol
+++ tee-worker/litentry/core/assertion-build/src/dynamic/contracts/token_holding_amount/brc20/Ordi.sol
@@ -18,8 +18,9 @@
 
 pragma solidity ^0.8.8;
 
-import "../Constants.sol";
 import "../../libraries/Identities.sol";
+import "../Constants.sol";
+import "./BRC20.sol";
 
 library Ordi {
 	function getTokenName() internal pure returns (string memory) {
@@ -38,9 +39,8 @@ library Ordi {
 		ranges[7] = 500 * Constants.decimals_factor;
 		return ranges;
 	}
+	
 	function getTokenNetworks() internal pure returns (uint32[] memory) {
-		uint32[] memory networks = new uint32[](1);
-		networks[0] = Web3Networks.BitcoinP2tr;
-		return networks;
+		return BRC20.getDefaultTokenNetworks();
 	}
 }
