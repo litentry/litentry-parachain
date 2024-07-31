bbca01ced487770d364523aa9634a3da3fa559c4
Francisco Silva <francisco@kawagarbo-tech.io>
2024-07-31 14:29:32 +0000

    updating runtimes

--- runtime/rococo/src/lib.rs
+++ runtime/rococo/src/lib.rs
@@ -1015,6 +1015,7 @@ impl pallet_teebag::Config for Runtime {
 	type SetAdminOrigin = EnsureRootOrHalfCouncil;
 	type MaxEnclaveIdentifier = ConstU32<3>;
 	type MaxAuthorizedEnclave = ConstU32<5>;
+	type WeightInfo = weights::pallet_teebag::WeightInfo<Runtime>;
 }
 
 impl pallet_identity_management::Config for Runtime {
