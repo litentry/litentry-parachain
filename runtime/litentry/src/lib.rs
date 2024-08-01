1b96d38eea9fc03e51b934ed79139a0283735277
Francisco Silva <francisco@kawagarbo-tech.io>
2024-08-01 08:05:43 +0000

    updating type usages

--- runtime/litentry/src/lib.rs
+++ runtime/litentry/src/lib.rs
@@ -857,7 +857,7 @@ impl pallet_teebag::Config for Runtime {
 	type SetAdminOrigin = EnsureRootOrHalfCouncil;
 	type MaxEnclaveIdentifier = ConstU32<3>;
 	type MaxAuthorizedEnclave = ConstU32<5>;
-	type WeightInfo = weights::pallet_teebag::LitentryWeight<Runtime>;
+	type WeightInfo = weights::pallet_teebag::WeightInfo<Runtime>;
 }
 
 impl pallet_bitacross::Config for Runtime {
