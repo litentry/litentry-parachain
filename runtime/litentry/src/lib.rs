9374e4a991929f5c7aa73c7415d799407dfa34b2
Francisco Silva <francisco@kawagarbo-tech.io>
2024-08-23 07:54:06 +0000

    making runtimes consistent

--- runtime/litentry/src/lib.rs
+++ runtime/litentry/src/lib.rs
@@ -867,7 +867,7 @@ impl pallet_parachain_staking::Config for Runtime {
 	type OnNewRound = ();
 	type WeightInfo = weights::pallet_parachain_staking::WeightInfo<Runtime>;
 	type IssuanceAdapter = AssetsHandler;
-	type OnAllDelegationRemoved = pallet_score_staking::Pallet<Runtime>;
+	type OnAllDelegationRemoved = ScoreStaking;
 }
 
 parameter_types! {
