9e519e60c4c3bc12b3742240b03b49002569dca7
Kailai Wang <Kailai.Wang@hotmail.com>
2024-09-04 07:09:15 +0000

    try to update ci

--- tee-worker/cli/lit_ts_integration_test.sh
+++ tee-worker/cli/lit_ts_integration_test.sh
@@ -28,9 +28,9 @@ while getopts ":p:A:u:W:V:C:" opt; do
 done
 
 # Using default port if none given as arguments.
-NPORT=${NPORT:-9912}
-NODEURL=${NODEURL:-"ws://litentry-node"}
-NODEHTTPURL=${NODEHTTPURL:-"http://litentry-node"}
+NPORT=${NPORT:-9944}
+NODEURL=${NODEURL:-"ws://localhost"}
+NODEHTTPURL=${NODEHTTPURL:-"http://localhost"}
 WORKER1PORT=${WORKER1PORT:-2011}
 WORKER1URL=${WORKER1URL:-"ws://litentry-worker-1"}
 
