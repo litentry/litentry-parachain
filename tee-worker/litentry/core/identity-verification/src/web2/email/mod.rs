c20cb7fe9932b896711b4727543ceb5db6399f35
Francisco Silva <francisco@kawagarbo-tech.io>
2024-09-10 15:55:51 +0000

    implementing send_verification_email

--- tee-worker/litentry/core/identity-verification/src/web2/email/mod.rs
+++ tee-worker/litentry/core/identity-verification/src/web2/email/mod.rs
@@ -11,3 +11,16 @@ pub fn generate_verification_code() -> String {
 	helpers::get_random_string(32)
 }
 
+pub fn send_verification_email(
+	mailer: &mut impl Mailer,
+	to_email: String,
+	redirect_url: String,
+) -> Result<(), String> {
+	let mail = mailer::Mail {
+		to: to_email,
+		subject: String::from("Verify your email address"),
+		body: template::VERYFY_EMAIL_TEMPLATE.replace("{{ redirect_url }}", &redirect_url),
+	};
+
+	mailer.send(mail)
+}
