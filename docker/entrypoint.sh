75fcd7f3613a9229a5b1cf23c70210e851cbf78a
mi1ktea <871158362@qq.com>
2024-09-18 11:05:50 +0800

    update

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -23,10 +23,6 @@ check(){
 }
 
 init(){
-    # cd "$PARACHAIN_BASEDIR" || exit
-    # polkadot build-spec --chain rococo-local --disable-default-bootnode --raw > rococo-local-chain-spec.json
-    # litentry-collator export-genesis-state --chain $CHAIN-dev > genesis-state
-    # litentry-collator export-genesis-wasm --chain $CHAIN-dev > genesis-wasm
     export PARA_ID=$(grep -i "${CHAIN}_para_id" ${REPO_DIR}/primitives/core/src/lib.rs | sed 's/.* = //;s/\;.*//')
     export PARA_CHAIN_SPEC=${CHAIN}-dev
     export COLLATOR_WS_PORT=${CollatorWSPort:-9944}
@@ -36,63 +32,14 @@ run_zombienet(){
     cd "$PARACHAIN_BASEDIR" || exit
     cp ${REPO_DIR}/zombienet/config.toml .
     zombienet setup polkadot -y || true
+    export PATH=${PATH}:${PARACHAIN_BASEDIR}
     cp /usr/local/bin/litentry-collator .
-    nohup zombienet -d $ZOMBIENET_DIR -l silent spawn config.toml &
+    nohup zombienet -d $ZOMBIENET_DIR -l silent spawn config.toml >> ${PARACHAIN_BASEDIR}/zombienet.log 2>&1 &
     zombienet_pid=$!
-}
-
-run_parachain_alice(){
-    echo "Starting parachain alice..."
-    local flags="--base-path ${PARACHAIN_BASEDIR}/para-alice \
-    --alice \
-    --collator \
-    --force-authoring \
-    --chain ${CHAIN}-dev \
-    --unsafe-rpc-external \
-    --rpc-cors=all \
-    --ws-max-connections 3000 \
-    --port 30333 \
-    --rpc-port 9944 \
-    --execution wasm \
-    --state-pruning archive \
-    --blocks-pruning archive \
-    -- --execution wasm --chain ${PARACHAIN_BASEDIR}/rococo-local-chain-spec.json --port 30332 --rpc-port 9943"
-    echo "Flags: ${flags}"
-
-    nohup litentry-collator ${flags} >> ${PARACHAIN_BASEDIR}/para-alice.log 2>&1 &
-    parachain_alice_pid=$!
-}
-
-run_relay_alice(){
-    echo "Starting relay alice..."
-    local flags="--base-path ${PARACHAIN_BASEDIR}/relay-alice \
-    --chain ${PARACHAIN_BASEDIR}/rococo-local-chain-spec.json \
-    --alice \
-    --port 30336 \
-    --rpc-port 9946"
-    echo "Flags: ${flags}"
-
-    nohup polkadot ${flags} >> ${PARACHAIN_BASEDIR}/relay-alice.log 2>&1 &
-    relay_alice_pid=$!
-}
-
-run_relay_bob(){
-    echo "Starting relay bob..."
-    local flags="--base-path ${PARACHAIN_BASEDIR}/relay-bob \
-    --chain ${PARACHAIN_BASEDIR}/rococo-local-chain-spec.json \
-    --bob \
-    --port 30337 \
-    --rpc-port 9947"
-    echo "Flags: ${flags}"
-
-    nohup polkadot ${flags} >> ${PARACHAIN_BASEDIR}/relay-bob.log 2>&1 &
-    relay_bob_pid=$!
 }
 
 register_parachain(){
     echo "Register parathread now ..."
-    # cd "$REPO_DIR" || exit
-    # export PARACHAIN_ID=$(grep -i "${CHAIN}_para_id" primitives/core/src/lib.rs | sed 's/.* = //;s/\;.*//')
     cd "$REPO_DIR/ts-tests" || exit
     if [[ -z "$NODE_ENV" ]]; then
         echo "NODE_ENV=ci" > .env
@@ -101,15 +48,6 @@ register_parachain(){
     fi
 
     corepack pnpm install
-    # jq --arg genesis_state "$PARACHAIN_BASEDIR/genesis-state" --arg genesis_wasm "$PARACHAIN_BASEDIR/genesis-wasm" '.genesis_state_path = $genesis_state | .genesis_wasm_path = $genesis_wasm' config.ci.json > config.ci.json.1
-    # mv config.ci.json.1 config.ci.json
-    # pnpm install
-    # pnpm run register-parathread 2>&1 | tee "$PARACHAIN_BASEDIR/register-parathread.log"
-
-    # echo "Upgrade parathread to parachain in 90s ..."
-    # # Wait for 90s to allow onboarding finish, after that we do the upgrade
-    # sleep 90
-    # pnpm run upgrade-parathread 2>&1 | tee "$PARACHAIN_BASEDIR/upgrade-parathread.log"
 
     echo "wait for parachain to produce block #1..."
     pnpm run wait-finalized-block 2>&1
@@ -120,28 +58,20 @@ register_parachain(){
 
 print_help(){
     echo "Parachain ${CHAIN} initialized successfully!"
-    echo "If you need to monitor the logs, please try the command 'docker exec <containers name> tail -f /opt/litentry/parachain/para-alice.log'."
+    echo "If you need to monitor the logs, please try the command 'docker exec <containers name> tail -f ${PARACHAIN_BASEDIR}/zombienet.log'."
     echo "Next, it will enter daemon mode."
 }
 
 watch_pid(){
-    # wait -n ${relay_alice_pid} ${relay_bob_pid} ${parachain_alice_pid} ${zombienet_pid}
     wait -n ${zombienet_pid}
     EXIT_STATUS=$?
-    # kill ${relay_alice_pid} ${relay_bob_pid} ${parachain_alice_pid} ${zombienet_pid}
     kill ${zombienet_pid}
     exit $EXIT_STATUS
 }
 
 main(){
-    # check
-    # init
-    # run_relay_alice
-    # sleep 5
-    # run_relay_bob
-    # sleep 5
-    # run_parachain_alice
-    # sleep 5
+    check
+    init
     run_zombienet
     register_parachain
     print_help
