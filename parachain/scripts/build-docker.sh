92968dea527f5b827ee26fd00a16301550cfefc3
Yang <yang@trustcomputing.de>
2024-09-27 09:12:04 +0000

    optimization

--- parachain/scripts/build-docker.sh
+++ parachain/scripts/build-docker.sh
@@ -47,6 +47,21 @@ GITUSER=litentry
 GITREPO=litentry-parachain
 PROXY="${HTTP_PROXY//localhost/host.docker.internal}"
 
+# Build the local-builder image
+echo "------------------------------------------------------------"
+echo "Building ${GITUSER}/${GITREPO}:${TAG} docker image ..."
+docker build ${NOCACHE_FLAG} --pull -f ./parachain/docker/Dockerfile \
+    --build-arg PROFILE="$PROFILE" \
+    --build-arg BUILD_ARGS="$ARGS" \
+    --build-arg HTTP_PROXY="$PROXY" \
+    --build-arg HTTPS_PROXY="$PROXY" \
+    --build-arg http_proxy="$PROXY" \
+    --build-arg https_proxy="$PROXY" \
+    --add-host=host.docker.internal:host-gateway \
+    --network host \
+    --target builder \
+    --tag local-builder:latest .
+
 # Build the image
 echo "------------------------------------------------------------"
 echo "Building ${GITUSER}/${GITREPO}:${TAG} docker image ..."
@@ -60,10 +75,10 @@ docker build ${NOCACHE_FLAG} --pull -f ./parachain/docker/Dockerfile \
     --add-host=host.docker.internal:host-gateway \
     --network host \
     --target parachain \
-    -t ${GITUSER}/${GITREPO}:${TAG} .
+    --tag ${GITUSER}/${GITREPO}:${TAG} .
 
 # Tag it with latest if no tag parameter was provided
-[ -z "$2" ] && docker tag ${GITUSER}/${GITREPO}:${TAG} ${GITUSER}/${GITREPO}:latest
+[ -z "$TAG" ] && docker tag ${GITUSER}/${GITREPO}:${TAG} ${GITUSER}/${GITREPO}:latest
 
 # Show the list of available images for this repo
 echo "Image is ready"
@@ -71,29 +86,31 @@ echo "------------------------------------------------------------"
 docker images | grep ${GITREPO}
 
 # ===================================================================================
-GITUSER=litentry
-GITREPO=litentry-chain-aio
-PROXY="${HTTP_PROXY//localhost/host.docker.internal}"
+if [ -z "$TAG" ] || [ "$TAG" = "latest" ]; then
+    GITUSER=litentry
+    GITREPO=litentry-chain-aio
+    PROXY="${HTTP_PROXY//localhost/host.docker.internal}"
 
-# Build the image
-echo "------------------------------------------------------------"
-echo "Building ${GITUSER}/${GITREPO}:${TAG} docker image ..."
-docker build ${NOCACHE_FLAG} --pull -f ./parachain/docker/Dockerfile \
-    --build-arg PROFILE="$PROFILE" \
-    --build-arg BUILD_ARGS="$ARGS" \
-    --build-arg HTTP_PROXY="$PROXY" \
-    --build-arg HTTPS_PROXY="$PROXY" \
-    --build-arg http_proxy="$PROXY" \
-    --build-arg https_proxy="$PROXY" \
-    --add-host=host.docker.internal:host-gateway \
-    --network host \
-    --target chain-aio \
-    -t ${GITUSER}/${GITREPO}:${TAG} .
+    # Build the image
+    echo "------------------------------------------------------------"
+    echo "Building ${GITUSER}/${GITREPO}:${TAG} docker image ..."
+    docker build ${NOCACHE_FLAG} --pull -f ./parachain/docker/Dockerfile \
+        --build-arg PROFILE="$PROFILE" \
+        --build-arg BUILD_ARGS="$ARGS" \
+        --build-arg HTTP_PROXY="$PROXY" \
+        --build-arg HTTPS_PROXY="$PROXY" \
+        --build-arg http_proxy="$PROXY" \
+        --build-arg https_proxy="$PROXY" \
+        --add-host=host.docker.internal:host-gateway \
+        --network host \
+        --target chain-aio \
+        --tag ${GITUSER}/${GITREPO}:${TAG} .
 
-# Tag it with latest if no tag parameter was provided
-[ -z "$2" ] && docker tag ${GITUSER}/${GITREPO}:${TAG} ${GITUSER}/${GITREPO}:latest
+    # Tag it with latest if no tag parameter was provided
+    [ -z "$TAG" ] && docker tag ${GITUSER}/${GITREPO}:${TAG} ${GITUSER}/${GITREPO}:latest
 
-# Show the list of available images for this repo
-echo "Image is ready"
-echo "------------------------------------------------------------"
-docker images | grep ${GITREPO}
+    # Show the list of available images for this repo
+    echo "Image is ready"
+    echo "------------------------------------------------------------"
+    docker images | grep ${GITREPO}
+fi
