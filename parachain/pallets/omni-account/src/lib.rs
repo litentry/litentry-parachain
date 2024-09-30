889d11e16acb1f72bbb1f94e22d7cd9a6d8e16fc
Francisco Silva <francisco@kawagarbo-tech.io>
2024-09-30 13:23:44 +0000

    refactoring omni-account pallet

--- parachain/pallets/omni-account/src/lib.rs
+++ parachain/pallets/omni-account/src/lib.rs
@@ -5,18 +5,19 @@ mod mock;
 #[cfg(test)]
 mod tests;
 
-pub(crate) mod identity_context;
-
-pub use core_primitives::{assertion::network::Web3Network, Identity, OmniAccountIdentity};
+pub use core_primitives::{Identity, MemberIdentity};
 pub use frame_system::pallet_prelude::BlockNumberFor;
 pub use pallet::*;
 
 use frame_support::pallet_prelude::*;
 use frame_system::pallet_prelude::*;
-use identity_context::*;
 use sp_core::H256;
 use sp_core_hashing::blake2_256;
 use sp_std::vec::Vec;
+
+pub trait AccountIdConverter<T: Config> {
+	fn convert(account_id: T::AccountId) -> Identity;
+}
 
 #[frame_support::pallet]
 pub mod pallet {
@@ -39,44 +40,43 @@ pub mod pallet {
 		/// The maximum number of identities an id graph can have.
 		#[pallet::constant]
 		type MaxIDGraphLength: Get<u32>;
+		/// AccountId converter
+		type AccountIdConverter: AccountIdConverter<Self>;
 	}
-	pub type IDGraphLinks<T> =
-		BoundedVec<(OmniAccountIdentity, IdentityContext<T>), <T as Config>::MaxIDGraphLength>;
+	pub type IDGraphLinks<T> = BoundedVec<(H256, MemberIdentity), <T as Config>::MaxIDGraphLength>;
 
 	#[pallet::storage]
-	pub type LinkedIdentities<T: Config> =
+	pub type LinkedIdentityHashes<T: Config> =
 		StorageMap<Hasher = Blake2_128Concat, Key = H256, Value = ()>;
 
 	#[pallet::storage]
 	#[pallet::getter(fn id_graphs)]
 	pub type IDGraphs<T: Config> =
-		StorageMap<Hasher = Blake2_128Concat, Key = Identity, Value = IDGraphLinks<T>>;
+		StorageMap<Hasher = Blake2_128Concat, Key = T::AccountId, Value = IDGraphLinks<T>>;
 
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
 		/// Identity linked
-		IdentityLinked { who: Identity, identity: H256 },
-		/// Identity deactivate_identity
-		IdentityDeactivated { who: Identity, identity: H256 },
-		/// Identity activate_identity
-		IdentityActivated { who: Identity, identity: H256 },
-		/// Web3 networks updated
-		Web3NetworksUpdated { identity: H256, web3networks: Vec<Web3Network> },
+		IdentityLinked { who: T::AccountId, identity: H256 },
 		/// Identity remove
-		IdentityRemoved { who: Identity, identities: Vec<OmniAccountIdentity> },
+		IdentityRemoved { who: T::AccountId, identity_hashes: Vec<H256> },
 		/// Identity made public
-		IdentityMadePublic { who: Identity, identity: H256 },
+		IdentityMadePublic { who: T::AccountId, identity_hash: H256 },
 	}
 
 	#[pallet::error]
 	pub enum Error<T> {
-		/// The Identity is already linked
+		/// Identity is already linked
 		IdentityAlreadyLinked,
 		/// IDGraph len limit reached
 		IDGraphLenLimitReached,
 		/// Identity not found
 		IdentityNotFound,
+		/// Invalid identity
+		PrimeIdentityInvalid,
+		/// Identity is private
+		IdentityIsPrivate,
 	}
 
 	#[pallet::call]
@@ -85,160 +85,68 @@ pub mod pallet {
 		#[pallet::weight((195_000_000, DispatchClass::Normal))]
 		pub fn link_identity(
 			origin: OriginFor<T>,
-			who: Identity,
-			identity: OmniAccountIdentity,
-			web3networks: Vec<Web3Network>,
+			who: T::AccountId,
+			identity_hash: H256,
+			identity: MemberIdentity,
 		) -> DispatchResult {
 			let _ = T::TEECallOrigin::ensure_origin(origin)?;
-			let identity_hash = H256::from(blake2_256(&identity.encode()));
 			ensure!(
-				!LinkedIdentities::<T>::contains_key(identity_hash),
+				!LinkedIdentityHashes::<T>::contains_key(identity_hash),
 				Error::<T>::IdentityAlreadyLinked
 			);
 			let mut id_graph_links = match IDGraphs::<T>::get(&who) {
 				Some(id_graph_links) => id_graph_links,
 				None => {
-					let prime_identity_hash =
-						H256::from(blake2_256(&OmniAccountIdentity::from(who.clone()).encode()));
-					if LinkedIdentities::<T>::contains_key(prime_identity_hash) {
+					let prime_identity = T::AccountIdConverter::convert(who.clone());
+					let prime_did =
+						prime_identity.to_did().map_err(|_| Error::<T>::PrimeIdentityInvalid)?;
+					let prime_identity_hash = H256::from(blake2_256(&prime_did.encode()));
+					if LinkedIdentityHashes::<T>::contains_key(prime_identity_hash) {
 						return Err(Error::<T>::IdentityAlreadyLinked.into());
 					}
-					let context = IdentityContext::<T>::new(
-						<frame_system::Pallet<T>>::block_number(),
-						who.default_web3networks(),
-					);
 					let mut id_graph_links: IDGraphLinks<T> = BoundedVec::new();
 					id_graph_links
-						.try_push((who.clone().into(), context))
+						.try_push((prime_identity_hash, MemberIdentity::from(prime_identity)))
 						.map_err(|_| Error::<T>::IDGraphLenLimitReached)?;
-					LinkedIdentities::<T>::insert(prime_identity_hash, ());
+					LinkedIdentityHashes::<T>::insert(prime_identity_hash, ());
 					IDGraphs::<T>::insert(who.clone(), id_graph_links.clone());
 					id_graph_links
 				},
 			};
-			let context =
-				IdentityContext::<T>::new(<frame_system::Pallet<T>>::block_number(), web3networks);
 			id_graph_links
-				.try_push((identity, context))
+				.try_push((identity_hash, identity))
 				.map_err(|_| Error::<T>::IDGraphLenLimitReached)?;
-			LinkedIdentities::<T>::insert(identity_hash, ());
+			LinkedIdentityHashes::<T>::insert(identity_hash, ());
 			IDGraphs::<T>::insert(who.clone(), id_graph_links);
 			Self::deposit_event(Event::IdentityLinked { who, identity: identity_hash });
 
 			Ok(())
 		}
 
-		#[pallet::call_index(1)]
-		#[pallet::weight((195_000_000, DispatchClass::Normal))]
-		pub fn deactivate_identity(
-			origin: OriginFor<T>,
-			who: Identity,
-			identity: OmniAccountIdentity,
-		) -> DispatchResult {
-			let _ = T::TEECallOrigin::ensure_origin(origin)?;
-			let mut id_graph_links =
-				IDGraphs::<T>::get(&who).ok_or(Error::<T>::IdentityNotFound)?;
-			let id_graph_link = id_graph_links
-				.iter_mut()
-				.find(|(id, _)| id == &identity)
-				.ok_or(Error::<T>::IdentityNotFound)?;
-			id_graph_link.1.deactivate();
-
-			IDGraphs::<T>::insert(who.clone(), id_graph_links);
-
-			let identity_hash = H256::from(blake2_256(&identity.encode()));
-
-			Self::deposit_event(Event::IdentityDeactivated { who, identity: identity_hash });
-
-			Ok(())
-		}
-
-		#[pallet::call_index(2)]
-		#[pallet::weight((195_000_000, DispatchClass::Normal))]
-		pub fn activate_identity(
-			origin: OriginFor<T>,
-			who: Identity,
-			identity: OmniAccountIdentity,
-		) -> DispatchResult {
-			let _ = T::TEECallOrigin::ensure_origin(origin)?;
-			let mut id_graph_links =
-				IDGraphs::<T>::get(&who).ok_or(Error::<T>::IdentityNotFound)?;
-			let id_graph_link = id_graph_links
-				.iter_mut()
-				.find(|(id, _)| id == &identity)
-				.ok_or(Error::<T>::IdentityNotFound)?;
-			id_graph_link.1.activate();
-
-			IDGraphs::<T>::insert(who.clone(), id_graph_links);
-
-			let identity_hash = H256::from(blake2_256(&identity.encode()));
-
-			Self::deposit_event(Event::IdentityActivated { who, identity: identity_hash });
-
-			Ok(())
-		}
-
-		#[pallet::call_index(3)]
-		#[pallet::weight((195_000_000, DispatchClass::Normal))]
-		pub fn set_identity_networks(
-			origin: OriginFor<T>,
-			who: Identity,
-			identity: OmniAccountIdentity,
-			web3networks: Vec<Web3Network>,
-		) -> DispatchResult {
-			let _ = T::TEECallOrigin::ensure_origin(origin)?;
-			let mut id_graph_links =
-				IDGraphs::<T>::get(&who).ok_or(Error::<T>::IdentityNotFound)?;
-			let id_graph_link = id_graph_links
-				.iter_mut()
-				.find(|(id, _)| id == &identity)
-				.ok_or(Error::<T>::IdentityNotFound)?;
-			id_graph_link.1.set_web3networks(web3networks.clone());
-
-			IDGraphs::<T>::insert(who.clone(), id_graph_links);
-
-			let identity_hash = H256::from(blake2_256(&identity.encode()));
-
-			Self::deposit_event(Event::Web3NetworksUpdated {
-				identity: identity_hash,
-				web3networks,
-			});
-
-			Ok(())
-		}
-
 		#[pallet::call_index(4)]
 		#[pallet::weight((195_000_000, DispatchClass::Normal))]
-		pub fn remove_identity(
+		pub fn remove_identities(
 			origin: OriginFor<T>,
-			who: Identity,
-			identities: Vec<OmniAccountIdentity>,
+			who: T::AccountId,
+			identity_hashes: Vec<H256>,
 		) -> DispatchResult {
 			let _ = T::TEECallOrigin::ensure_origin(origin)?;
 
 			let mut id_graph_links =
 				IDGraphs::<T>::get(&who).ok_or(Error::<T>::IdentityNotFound)?;
 
-			if !identities.is_empty() {
-				id_graph_links.retain(|(id, _)| {
-					if identities.contains(id) {
-						let identity_hash = H256::from(blake2_256(&id.encode()));
-						LinkedIdentities::<T>::remove(identity_hash);
-						false
-					} else {
-						true
-					}
-				});
-				IDGraphs::<T>::insert(who.clone(), id_graph_links);
-			} else {
-				id_graph_links.iter().for_each(|(id, _)| {
-					let identity_hash = H256::from(blake2_256(&id.encode()));
-					LinkedIdentities::<T>::remove(identity_hash);
-				});
+			if identity_hashes.is_empty() || identity_hashes.len() == id_graph_links.len() {
 				IDGraphs::<T>::remove(&who);
+			} else {
+				id_graph_links.retain(|(id_hash, _)| !identity_hashes.contains(id_hash));
+				IDGraphs::<T>::insert(who.clone(), id_graph_links);
 			}
 
-			Self::deposit_event(Event::IdentityRemoved { who, identities });
+			identity_hashes.iter().for_each(|identity_hash| {
+				LinkedIdentityHashes::<T>::remove(identity_hash);
+			});
+
+			Self::deposit_event(Event::IdentityRemoved { who, identity_hashes });
 
 			Ok(())
 		}
@@ -247,26 +155,24 @@ pub mod pallet {
 		#[pallet::weight((195_000_000, DispatchClass::Normal))]
 		pub fn make_identity_public(
 			origin: OriginFor<T>,
-			who: Identity,
-			private_identity: OmniAccountIdentity,
-			public_identity: OmniAccountIdentity,
+			who: T::AccountId,
+			identity_hash: H256,
+			public_identity: MemberIdentity,
 		) -> DispatchResult {
 			let _ = T::TEECallOrigin::ensure_origin(origin)?;
+			ensure!(public_identity.is_public(), Error::<T>::IdentityIsPrivate);
+
 			let mut id_graph_links =
 				IDGraphs::<T>::get(&who).ok_or(Error::<T>::IdentityNotFound)?;
 			let id_graph_link = id_graph_links
 				.iter_mut()
-				.find(|(id, _)| id == &private_identity)
+				.find(|(id_hash, _)| *id_hash == identity_hash)
 				.ok_or(Error::<T>::IdentityNotFound)?;
+			id_graph_link.1 = public_identity;
 
-			let public_identity_hash = H256::from(blake2_256(&public_identity.encode()));
-			id_graph_link.0 = public_identity;
 			IDGraphs::<T>::insert(who.clone(), id_graph_links);
-			let private_identity_hash = H256::from(blake2_256(&private_identity.encode()));
-			LinkedIdentities::<T>::remove(private_identity_hash);
-			LinkedIdentities::<T>::insert(public_identity_hash, ());
 
-			Self::deposit_event(Event::IdentityMadePublic { who, identity: public_identity_hash });
+			Self::deposit_event(Event::IdentityMadePublic { who, identity_hash });
 
 			Ok(())
 		}
