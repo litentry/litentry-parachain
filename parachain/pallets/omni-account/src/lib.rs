f1f3f62db3df8cd6a50f868aed5db2c15603c2c2
Francisco Silva <franjs.francisco@gmail.com>
2024-10-04 09:25:51 +0200

    Adding omni-account pallet (#3098)

---
+++ parachain/pallets/omni-account/src/lib.rs
@@ -0,0 +1,279 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+#[cfg(test)]
+mod mock;
+#[cfg(test)]
+mod tests;
+
+pub use core_primitives::{Identity, MemberIdentity};
+pub use frame_system::pallet_prelude::BlockNumberFor;
+pub use pallet::*;
+
+use frame_support::pallet_prelude::*;
+use frame_system::pallet_prelude::*;
+use sp_core::H256;
+use sp_core_hashing::blake2_256;
+use sp_std::vec::Vec;
+
+#[derive(Encode, Decode, TypeInfo, Clone, PartialEq, Eq, RuntimeDebug)]
+pub struct IDGraphMember {
+	pub id: MemberIdentity,
+	pub hash: H256,
+}
+
+pub trait AccountIdConverter<T: Config> {
+	fn convert(identity: &Identity) -> Option<T::AccountId>;
+}
+
+pub trait IDGraphHash {
+	fn graph_hash(&self) -> H256;
+}
+
+impl<T> IDGraphHash for BoundedVec<IDGraphMember, T> {
+	fn graph_hash(&self) -> H256 {
+		let id_graph_members_hashes: Vec<H256> = self.iter().map(|member| member.hash).collect();
+		H256::from(blake2_256(&id_graph_members_hashes.encode()))
+	}
+}
+
+#[frame_support::pallet]
+pub mod pallet {
+	use super::*;
+
+	/// The current storage version.
+	const STORAGE_VERSION: StorageVersion = StorageVersion::new(0);
+
+	#[pallet::pallet]
+	#[pallet::storage_version(STORAGE_VERSION)]
+	#[pallet::without_storage_info]
+	pub struct Pallet<T>(_);
+
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		/// The event type of this pallet.
+		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+		/// The origin which can manage the pallet.
+		type TEECallOrigin: EnsureOrigin<Self::RuntimeOrigin>;
+		/// The maximum number of identities an id graph can have.
+		#[pallet::constant]
+		type MaxIDGraphLength: Get<u32>;
+		/// AccountId converter
+		type AccountIdConverter: AccountIdConverter<Self>;
+	}
+	pub type IDGraph<T> = BoundedVec<IDGraphMember, <T as Config>::MaxIDGraphLength>;
+
+	#[pallet::storage]
+	pub type LinkedIdentityHashes<T: Config> =
+		StorageMap<Hasher = Blake2_128Concat, Key = H256, Value = ()>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn id_graphs)]
+	pub type IDGraphs<T: Config> =
+		StorageMap<Hasher = Blake2_128Concat, Key = T::AccountId, Value = IDGraph<T>>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn id_graph_hashes)]
+	pub type IDGraphHashes<T: Config> =
+		StorageMap<Hasher = Blake2_128Concat, Key = T::AccountId, Value = H256>;
+
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {
+		/// Identity linked
+		IdentityLinked { who: T::AccountId, identity: H256 },
+		/// Identity remove
+		IdentityRemoved { who: T::AccountId, identity_hashes: Vec<H256> },
+		/// Identity made public
+		IdentityMadePublic { who: T::AccountId, identity_hash: H256 },
+	}
+
+	#[pallet::error]
+	pub enum Error<T> {
+		/// Identity is already linked
+		IdentityAlreadyLinked,
+		/// IDGraph len limit reached
+		IDGraphLenLimitReached,
+		/// Identity not found
+		IdentityNotFound,
+		/// Invalid identity
+		InvalidIdentity,
+		/// IDGraph not found
+		UnknownIDGraph,
+		/// Identity is private
+		IdentityIsPrivate,
+		/// Identities empty
+		IdentitiesEmpty,
+		/// IDGraph hash does not match
+		IDGraphHashMismatch,
+		/// Missing IDGraph hash
+		IDGraphHashMissing,
+	}
+
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		#[pallet::call_index(0)]
+		#[pallet::weight((195_000_000, DispatchClass::Normal))]
+		pub fn link_identity(
+			origin: OriginFor<T>,
+			who: Identity,
+			member_account: IDGraphMember,
+			maybe_id_graph_hash: Option<H256>,
+		) -> DispatchResult {
+			let _ = T::TEECallOrigin::ensure_origin(origin)?;
+			ensure!(
+				!LinkedIdentityHashes::<T>::contains_key(member_account.hash),
+				Error::<T>::IdentityAlreadyLinked
+			);
+			let who_account_id = match T::AccountIdConverter::convert(&who) {
+				Some(account_id) => account_id,
+				None => return Err(Error::<T>::InvalidIdentity.into()),
+			};
+			let identity_hash = member_account.hash;
+			let mut id_graph = Self::get_or_create_id_graph(
+				who.clone(),
+				who_account_id.clone(),
+				maybe_id_graph_hash,
+			)?;
+			id_graph
+				.try_push(member_account)
+				.map_err(|_| Error::<T>::IDGraphLenLimitReached)?;
+
+			LinkedIdentityHashes::<T>::insert(identity_hash, ());
+			IDGraphHashes::<T>::insert(who_account_id.clone(), id_graph.graph_hash());
+			IDGraphs::<T>::insert(who_account_id.clone(), id_graph);
+
+			Self::deposit_event(Event::IdentityLinked {
+				who: who_account_id,
+				identity: identity_hash,
+			});
+
+			Ok(())
+		}
+
+		#[pallet::call_index(4)]
+		#[pallet::weight((195_000_000, DispatchClass::Normal))]
+		pub fn remove_identities(
+			origin: OriginFor<T>,
+			who: Identity,
+			identity_hashes: Vec<H256>,
+		) -> DispatchResult {
+			let _ = T::TEECallOrigin::ensure_origin(origin)?;
+			ensure!(!identity_hashes.is_empty(), Error::<T>::IdentitiesEmpty);
+
+			let who_account_id = match T::AccountIdConverter::convert(&who) {
+				Some(account_id) => account_id,
+				None => return Err(Error::<T>::InvalidIdentity.into()),
+			};
+
+			let mut id_graph_members =
+				IDGraphs::<T>::get(&who_account_id).ok_or(Error::<T>::UnknownIDGraph)?;
+
+			id_graph_members.retain(|member| {
+				if identity_hashes.contains(&member.hash) {
+					LinkedIdentityHashes::<T>::remove(member.hash);
+					false
+				} else {
+					true
+				}
+			});
+
+			if id_graph_members.is_empty() {
+				IDGraphs::<T>::remove(&who_account_id);
+			} else {
+				IDGraphs::<T>::insert(who_account_id.clone(), id_graph_members);
+			}
+
+			Self::deposit_event(Event::IdentityRemoved { who: who_account_id, identity_hashes });
+
+			Ok(())
+		}
+
+		#[pallet::call_index(5)]
+		#[pallet::weight((195_000_000, DispatchClass::Normal))]
+		pub fn make_identity_public(
+			origin: OriginFor<T>,
+			who: Identity,
+			identity_hash: H256,
+			public_identity: MemberIdentity,
+		) -> DispatchResult {
+			let _ = T::TEECallOrigin::ensure_origin(origin)?;
+			ensure!(public_identity.is_public(), Error::<T>::IdentityIsPrivate);
+
+			let who_account_id = match T::AccountIdConverter::convert(&who) {
+				Some(account_id) => account_id,
+				None => return Err(Error::<T>::InvalidIdentity.into()),
+			};
+
+			let mut id_graph_members =
+				IDGraphs::<T>::get(&who_account_id).ok_or(Error::<T>::UnknownIDGraph)?;
+			let id_graph_link = id_graph_members
+				.iter_mut()
+				.find(|member| member.hash == identity_hash)
+				.ok_or(Error::<T>::IdentityNotFound)?;
+			id_graph_link.id = public_identity;
+
+			IDGraphs::<T>::insert(who_account_id.clone(), id_graph_members);
+
+			Self::deposit_event(Event::IdentityMadePublic { who: who_account_id, identity_hash });
+
+			Ok(())
+		}
+	}
+
+	impl<T: Config> Pallet<T> {
+		pub fn get_or_create_id_graph(
+			who: Identity,
+			who_account_id: T::AccountId,
+			maybe_id_graph_hash: Option<H256>,
+		) -> Result<IDGraph<T>, Error<T>> {
+			match IDGraphs::<T>::get(&who_account_id) {
+				Some(id_graph_members) => {
+					Self::verify_id_graph_hash(&who_account_id, maybe_id_graph_hash)?;
+					Ok(id_graph_members)
+				},
+				None => Self::create_id_graph(who, who_account_id),
+			}
+		}
+
+		fn verify_id_graph_hash(
+			who: &T::AccountId,
+			maybe_id_graph_hash: Option<H256>,
+		) -> Result<(), Error<T>> {
+			let current_id_graph_hash =
+				IDGraphHashes::<T>::get(who).ok_or(Error::<T>::IDGraphHashMissing)?;
+			match maybe_id_graph_hash {
+				Some(id_graph_hash) => {
+					ensure!(
+						current_id_graph_hash == id_graph_hash,
+						Error::<T>::IDGraphHashMismatch
+					);
+				},
+				None => return Err(Error::<T>::IDGraphHashMissing),
+			}
+
+			Ok(())
+		}
+
+		fn create_id_graph(
+			owner_identity: Identity,
+			owner_account_id: T::AccountId,
+		) -> Result<IDGraph<T>, Error<T>> {
+			let owner_identity_hash =
+				owner_identity.hash().map_err(|_| Error::<T>::InvalidIdentity)?;
+			if LinkedIdentityHashes::<T>::contains_key(owner_identity_hash) {
+				return Err(Error::<T>::IdentityAlreadyLinked);
+			}
+			let mut id_graph_members: IDGraph<T> = BoundedVec::new();
+			id_graph_members
+				.try_push(IDGraphMember {
+					id: MemberIdentity::from(owner_identity.clone()),
+					hash: owner_identity_hash,
+				})
+				.map_err(|_| Error::<T>::IDGraphLenLimitReached)?;
+			LinkedIdentityHashes::<T>::insert(owner_identity_hash, ());
+			IDGraphs::<T>::insert(owner_account_id.clone(), id_graph_members.clone());
+
+			Ok(id_graph_members)
+		}
+	}
+}
---
+++ parachain/pallets/omni-account/src/tests.rs
@@ -0,0 +1,538 @@
+use crate::{mock::*, IDGraphs, LinkedIdentityHashes, *};
+use core_primitives::Identity;
+use frame_support::{assert_noop, assert_ok};
+use sp_runtime::traits::BadOrigin;
+use sp_std::vec;
+
+#[test]
+fn link_identity_works() {
+	new_test_ext().execute_with(|| {
+		let tee_signer = get_tee_signer();
+
+		let who = alice();
+		let who_identity = Identity::from(who.clone());
+		let who_identity_hash = who_identity.hash().unwrap();
+
+		let bob_member_account = IDGraphMember {
+			id: MemberIdentity::Private(bob().encode()),
+			hash: Identity::from(bob()).hash().unwrap(),
+		};
+		let charlie_member_account = IDGraphMember {
+			id: MemberIdentity::Public(Identity::from(charlie())),
+			hash: Identity::from(charlie()).hash().unwrap(),
+		};
+
+		let expected_id_graph: IDGraph<TestRuntime> = BoundedVec::truncate_from(vec![
+			IDGraphMember {
+				id: MemberIdentity::from(who_identity.clone()),
+				hash: who_identity_hash,
+			},
+			bob_member_account.clone(),
+		]);
+		let expected_id_graph_hash = H256::from(blake2_256(
+			&expected_id_graph
+				.iter()
+				.map(|member| member.hash)
+				.collect::<Vec<H256>>()
+				.encode(),
+		));
+
+		assert_ok!(OmniAccount::link_identity(
+			RuntimeOrigin::signed(tee_signer.clone()),
+			who_identity.clone(),
+			bob_member_account.clone(),
+			None
+		));
+		System::assert_last_event(
+			Event::IdentityLinked { who: who.clone(), identity: bob_member_account.hash }.into(),
+		);
+
+		assert!(IDGraphs::<TestRuntime>::contains_key(&who));
+		assert_eq!(IDGraphs::<TestRuntime>::get(&who).unwrap(), expected_id_graph);
+		assert_eq!(IDGraphHashes::<TestRuntime>::get(&who).unwrap(), expected_id_graph_hash);
+
+		assert_ok!(OmniAccount::link_identity(
+			RuntimeOrigin::signed(tee_signer),
+			who_identity.clone(),
+			charlie_member_account.clone(),
+			Some(expected_id_graph_hash),
+		));
+		System::assert_last_event(
+			Event::IdentityLinked { who: who.clone(), identity: charlie_member_account.hash }
+				.into(),
+		);
+
+		let expected_id_graph: IDGraph<TestRuntime> = BoundedVec::truncate_from(vec![
+			IDGraphMember {
+				id: MemberIdentity::from(who_identity.clone()),
+				hash: who_identity_hash,
+			},
+			bob_member_account.clone(),
+			charlie_member_account.clone(),
+		]);
+		let expecte_id_graph_hash = H256::from(blake2_256(
+			&expected_id_graph
+				.iter()
+				.map(|member| member.hash)
+				.collect::<Vec<H256>>()
+				.encode(),
+		));
+
+		assert_eq!(IDGraphs::<TestRuntime>::get(&who).unwrap(), expected_id_graph);
+		assert_eq!(IDGraphHashes::<TestRuntime>::get(&who).unwrap(), expecte_id_graph_hash);
+
+		assert!(LinkedIdentityHashes::<TestRuntime>::contains_key(bob_member_account.hash));
+		assert!(LinkedIdentityHashes::<TestRuntime>::contains_key(charlie_member_account.hash));
+	});
+}
+
+#[test]
+fn link_identity_exising_id_graph_id_graph_hash_missing_works() {
+	new_test_ext().execute_with(|| {
+		let tee_signer = get_tee_signer();
+		let who_identity = Identity::from(alice());
+
+		let bob_member_account = IDGraphMember {
+			id: MemberIdentity::Private(bob().encode()),
+			hash: Identity::from(bob()).hash().unwrap(),
+		};
+		let charlie_member_account = IDGraphMember {
+			id: MemberIdentity::Public(Identity::from(charlie())),
+			hash: Identity::from(charlie()).hash().unwrap(),
+		};
+
+		// IDGraph gets created with the first identity
+		assert_ok!(OmniAccount::link_identity(
+			RuntimeOrigin::signed(tee_signer.clone()),
+			who_identity.clone(),
+			bob_member_account,
+			None
+		));
+
+		// to mutate IDGraph with a new identity, the current id_graph_hash must be provided
+		assert_noop!(
+			OmniAccount::link_identity(
+				RuntimeOrigin::signed(tee_signer),
+				who_identity,
+				charlie_member_account,
+				None
+			),
+			Error::<TestRuntime>::IDGraphHashMissing
+		);
+	});
+}
+
+#[test]
+fn link_identity_origin_check_works() {
+	new_test_ext().execute_with(|| {
+		let who = Identity::from(alice());
+		let member_account = IDGraphMember {
+			id: MemberIdentity::Private(vec![1, 2, 3]),
+			hash: H256::from(blake2_256(&[1, 2, 3])),
+		};
+
+		assert_noop!(
+			OmniAccount::link_identity(RuntimeOrigin::signed(bob()), who, member_account, None),
+			BadOrigin
+		);
+	});
+}
+
+#[test]
+fn link_identity_already_linked_works() {
+	new_test_ext().execute_with(|| {
+		let tee_signer = get_tee_signer();
+		let who = alice();
+		let who_identity = Identity::from(who.clone());
+
+		let member_account = IDGraphMember {
+			id: MemberIdentity::Public(Identity::from(bob())),
+			hash: Identity::from(bob()).hash().unwrap(),
+		};
+
+		assert_ok!(OmniAccount::link_identity(
+			RuntimeOrigin::signed(tee_signer.clone()),
+			who_identity.clone(),
+			member_account.clone(),
+			None
+		));
+		assert_noop!(
+			OmniAccount::link_identity(
+				RuntimeOrigin::signed(tee_signer.clone()),
+				who_identity.clone(),
+				member_account,
+				None
+			),
+			Error::<TestRuntime>::IdentityAlreadyLinked
+		);
+
+		// intent to create a new id_graph with an identity that is already linked
+		let who = Identity::from(bob());
+		let alice_member_account = IDGraphMember {
+			id: MemberIdentity::Public(Identity::from(alice())),
+			hash: Identity::from(alice()).hash().unwrap(),
+		};
+		assert_noop!(
+			OmniAccount::link_identity(
+				RuntimeOrigin::signed(tee_signer),
+				who.clone(),
+				alice_member_account,
+				None
+			),
+			Error::<TestRuntime>::IdentityAlreadyLinked
+		);
+	});
+}
+
+#[test]
+fn link_identity_id_graph_len_limit_reached_works() {
+	new_test_ext().execute_with(|| {
+		let tee_signer = get_tee_signer();
+
+		let who = alice();
+		let who_identity = Identity::from(who.clone());
+		let who_identity_hash = who_identity.hash().unwrap();
+
+		let member_account_2 = IDGraphMember {
+			id: MemberIdentity::Private(vec![1, 2, 3]),
+			hash: H256::from(blake2_256(&[1, 2, 3])),
+		};
+		let member_account_3 = IDGraphMember {
+			id: MemberIdentity::Private(vec![4, 5, 6]),
+			hash: H256::from(blake2_256(&[4, 5, 6])),
+		};
+
+		let id_graph: IDGraph<TestRuntime> = BoundedVec::truncate_from(vec![
+			IDGraphMember {
+				id: MemberIdentity::from(who_identity.clone()),
+				hash: who_identity_hash,
+			},
+			member_account_2.clone(),
+			member_account_3.clone(),
+		]);
+		let id_graph_hash = H256::from(blake2_256(&id_graph.encode()));
+
+		IDGraphs::<TestRuntime>::insert(who.clone(), id_graph.clone());
+		IDGraphHashes::<TestRuntime>::insert(who.clone(), id_graph_hash);
+
+		assert_noop!(
+			OmniAccount::link_identity(
+				RuntimeOrigin::signed(tee_signer),
+				who_identity,
+				IDGraphMember {
+					id: MemberIdentity::Private(vec![7, 8, 9]),
+					hash: H256::from(blake2_256(&[7, 8, 9])),
+				},
+				Some(id_graph_hash),
+			),
+			Error::<TestRuntime>::IDGraphLenLimitReached
+		);
+	});
+}
+
+#[test]
+fn link_identity_id_graph_hash_mismatch_works() {
+	new_test_ext().execute_with(|| {
+		let tee_signer = get_tee_signer();
+
+		let who = alice();
+		let who_identity = Identity::from(who.clone());
+		let who_identity_hash = who_identity.hash().unwrap();
+
+		let member_account = IDGraphMember {
+			id: MemberIdentity::Private(vec![1, 2, 3]),
+			hash: H256::from(blake2_256(&[1, 2, 3])),
+		};
+
+		let id_graph: IDGraph<TestRuntime> = BoundedVec::truncate_from(vec![
+			IDGraphMember {
+				id: MemberIdentity::from(who_identity.clone()),
+				hash: who_identity_hash,
+			},
+			member_account.clone(),
+		]);
+		let id_graph_hash = H256::from(blake2_256(
+			&id_graph.iter().map(|member| member.hash).collect::<Vec<H256>>().encode(),
+		));
+
+		assert_ok!(OmniAccount::link_identity(
+			RuntimeOrigin::signed(tee_signer.clone()),
+			who_identity.clone(),
+			member_account.clone(),
+			None
+		));
+
+		assert_eq!(IDGraphs::<TestRuntime>::get(&who).unwrap(), id_graph);
+		assert_eq!(IDGraphHashes::<TestRuntime>::get(&who).unwrap(), id_graph_hash);
+
+		// link another identity to the id_graph with the correct id_graph_hash
+		assert_ok!(OmniAccount::link_identity(
+			RuntimeOrigin::signed(tee_signer.clone()),
+			who_identity.clone(),
+			IDGraphMember {
+				id: MemberIdentity::Private(vec![4, 5, 6]),
+				hash: H256::from(blake2_256(&[4, 5, 6])),
+			},
+			Some(id_graph_hash),
+		));
+
+		let id_graph: IDGraph<TestRuntime> = BoundedVec::truncate_from(vec![
+			IDGraphMember {
+				id: MemberIdentity::from(who_identity.clone()),
+				hash: who_identity_hash,
+			},
+			member_account.clone(),
+			IDGraphMember {
+				id: MemberIdentity::Private(vec![4, 5, 6]),
+				hash: H256::from(blake2_256(&[4, 5, 6])),
+			},
+		]);
+		assert_eq!(IDGraphs::<TestRuntime>::get(&who).unwrap(), id_graph);
+
+		// attempt to link an identity with an old id_graph_hash
+		assert_noop!(
+			OmniAccount::link_identity(
+				RuntimeOrigin::signed(tee_signer),
+				who_identity,
+				IDGraphMember {
+					id: MemberIdentity::Private(vec![7, 8, 9]),
+					hash: H256::from(blake2_256(&[7, 8, 9])),
+				},
+				Some(id_graph_hash),
+			),
+			Error::<TestRuntime>::IDGraphHashMismatch
+		);
+	});
+}
+
+#[test]
+fn remove_identity_works() {
+	new_test_ext().execute_with(|| {
+		let tee_signer = get_tee_signer();
+		let who = alice();
+		let who_identity = Identity::from(who.clone());
+		let who_identity_hash = who_identity.hash().unwrap();
+
+		let member_account = IDGraphMember {
+			id: MemberIdentity::Private(vec![1, 2, 3]),
+			hash: H256::from(blake2_256(&[1, 2, 3])),
+		};
+		let identity_hash = member_account.hash;
+		let identities_to_remove = vec![identity_hash];
+
+		assert_ok!(OmniAccount::link_identity(
+			RuntimeOrigin::signed(tee_signer.clone()),
+			who_identity.clone(),
+			member_account.clone(),
+			None
+		));
+		assert_ok!(OmniAccount::remove_identities(
+			RuntimeOrigin::signed(tee_signer.clone()),
+			who_identity.clone(),
+			identities_to_remove.clone()
+		));
+		System::assert_last_event(
+			Event::IdentityRemoved { who: who.clone(), identity_hashes: identities_to_remove }
+				.into(),
+		);
+
+		let expected_id_graph: IDGraph<TestRuntime> =
+			BoundedVec::truncate_from(vec![IDGraphMember {
+				id: MemberIdentity::Public(who_identity.clone()),
+				hash: who_identity_hash,
+			}]);
+
+		assert_eq!(IDGraphs::<TestRuntime>::get(&who).unwrap(), expected_id_graph);
+		assert!(!LinkedIdentityHashes::<TestRuntime>::contains_key(identity_hash));
+
+		assert_ok!(OmniAccount::remove_identities(
+			RuntimeOrigin::signed(tee_signer.clone()),
+			who_identity.clone(),
+			vec![who_identity_hash],
+		));
+		System::assert_last_event(
+			Event::IdentityRemoved { who: who.clone(), identity_hashes: vec![who_identity_hash] }
+				.into(),
+		);
+
+		assert!(!IDGraphs::<TestRuntime>::contains_key(&who));
+	});
+}
+
+#[test]
+fn remove_identity_empty_identity_check_works() {
+	new_test_ext().execute_with(|| {
+		let tee_signer = get_tee_signer();
+		let who = Identity::from(alice());
+
+		assert_ok!(OmniAccount::link_identity(
+			RuntimeOrigin::signed(tee_signer.clone()),
+			who.clone(),
+			IDGraphMember {
+				id: MemberIdentity::Private(vec![1, 2, 3]),
+				hash: H256::from(blake2_256(&[1, 2, 3])),
+			},
+			None
+		));
+		assert_noop!(
+			OmniAccount::remove_identities(RuntimeOrigin::signed(tee_signer.clone()), who, vec![],),
+			Error::<TestRuntime>::IdentitiesEmpty
+		);
+	});
+}
+
+#[test]
+fn remove_identity_origin_check_works() {
+	new_test_ext().execute_with(|| {
+		let who = Identity::from(alice());
+		let identities_to_remove = vec![H256::from(blake2_256(&[1, 2, 3]))];
+
+		assert_noop!(
+			OmniAccount::remove_identities(RuntimeOrigin::signed(bob()), who, identities_to_remove),
+			BadOrigin
+		);
+	});
+}
+
+#[test]
+fn make_identity_public_works() {
+	new_test_ext().execute_with(|| {
+		let tee_signer = get_tee_signer();
+		let who = alice();
+		let who_identity = Identity::from(who.clone());
+
+		let private_identity = MemberIdentity::Private(vec![1, 2, 3]);
+		let public_identity = MemberIdentity::Public(Identity::from(bob()));
+		let identity_hash =
+			H256::from(blake2_256(&Identity::from(bob()).to_did().unwrap().encode()));
+
+		assert_ok!(OmniAccount::link_identity(
+			RuntimeOrigin::signed(tee_signer.clone()),
+			who_identity.clone(),
+			IDGraphMember { id: private_identity.clone(), hash: identity_hash },
+			None
+		));
+
+		let expected_id_graph: IDGraph<TestRuntime> = BoundedVec::truncate_from(vec![
+			IDGraphMember {
+				id: MemberIdentity::Public(who_identity.clone()),
+				hash: who_identity.hash().unwrap(),
+			},
+			IDGraphMember { id: private_identity.clone(), hash: identity_hash },
+		]);
+		assert_eq!(IDGraphs::<TestRuntime>::get(&who).unwrap(), expected_id_graph);
+
+		assert_ok!(OmniAccount::make_identity_public(
+			RuntimeOrigin::signed(tee_signer.clone()),
+			who_identity.clone(),
+			identity_hash,
+			public_identity.clone()
+		));
+		System::assert_last_event(
+			Event::IdentityMadePublic { who: who.clone(), identity_hash }.into(),
+		);
+
+		let expected_id_graph: IDGraph<TestRuntime> = BoundedVec::truncate_from(vec![
+			IDGraphMember {
+				id: MemberIdentity::Public(who_identity.clone()),
+				hash: who_identity.hash().unwrap(),
+			},
+			IDGraphMember { id: public_identity.clone(), hash: identity_hash },
+		]);
+		assert_eq!(IDGraphs::<TestRuntime>::get(&who).unwrap(), expected_id_graph);
+	});
+}
+
+#[test]
+fn make_identity_public_origin_check_works() {
+	new_test_ext().execute_with(|| {
+		let who = Identity::from(alice());
+		let identity = Identity::from(bob());
+		let identity_hash = identity.hash().unwrap();
+		let public_identity = MemberIdentity::Public(identity.clone());
+
+		assert_noop!(
+			OmniAccount::make_identity_public(
+				RuntimeOrigin::signed(bob()),
+				who,
+				identity_hash,
+				public_identity
+			),
+			BadOrigin
+		);
+	});
+}
+
+#[test]
+fn make_identity_public_identity_not_found_works() {
+	new_test_ext().execute_with(|| {
+		let tee_signer = get_tee_signer();
+		let who = Identity::from(alice());
+
+		let private_identity = MemberIdentity::Private(vec![1, 2, 3]);
+		let identity = Identity::from(bob());
+		let public_identity = MemberIdentity::Public(identity.clone());
+		let identity_hash =
+			H256::from(blake2_256(&Identity::from(bob()).to_did().unwrap().encode()));
+
+		assert_noop!(
+			OmniAccount::make_identity_public(
+				RuntimeOrigin::signed(tee_signer.clone()),
+				who.clone(),
+				identity_hash,
+				public_identity.clone()
+			),
+			Error::<TestRuntime>::UnknownIDGraph
+		);
+
+		assert_ok!(OmniAccount::link_identity(
+			RuntimeOrigin::signed(tee_signer.clone()),
+			who.clone(),
+			IDGraphMember { id: private_identity.clone(), hash: identity_hash },
+			None
+		));
+
+		let charlie_identity = Identity::from(charlie());
+		let other_identity = MemberIdentity::Public(charlie_identity.clone());
+		let other_identity_hash =
+			H256::from(blake2_256(&charlie_identity.to_did().unwrap().encode()));
+
+		assert_noop!(
+			OmniAccount::make_identity_public(
+				RuntimeOrigin::signed(tee_signer),
+				who,
+				other_identity_hash,
+				other_identity,
+			),
+			Error::<TestRuntime>::IdentityNotFound
+		);
+	});
+}
+
+#[test]
+fn make_identity_public_identity_is_private_check_works() {
+	new_test_ext().execute_with(|| {
+		let tee_signer = get_tee_signer();
+		let who = Identity::from(alice());
+
+		let private_identity = MemberIdentity::Private(vec![1, 2, 3]);
+		let identity_hash = Identity::from(bob()).hash().unwrap();
+
+		assert_ok!(OmniAccount::link_identity(
+			RuntimeOrigin::signed(tee_signer.clone()),
+			who.clone(),
+			IDGraphMember { id: private_identity.clone(), hash: identity_hash },
+			None
+		));
+
+		assert_noop!(
+			OmniAccount::make_identity_public(
+				RuntimeOrigin::signed(tee_signer),
+				who,
+				identity_hash,
+				private_identity,
+			),
+			Error::<TestRuntime>::IdentityIsPrivate
+		);
+	});
+}
