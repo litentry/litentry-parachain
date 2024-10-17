a04fa8bf128556117da2e6c9782336ce14bb1e9b
Francisco Silva <francisco@kawagarbo-tech.io>
2024-10-17 15:16:02 +0000

    refactoring get_all, getting entries with pagination

--- tee-worker/identity/litentry/core/omni-account/src/repository.rs
+++ tee-worker/identity/litentry/core/omni-account/src/repository.rs
@@ -15,7 +15,7 @@
 // along with Litentry.  If not, see <https://www.gnu.org/licenses/>.
 
 use crate::{AccountId, Error, Header, MemberAccount, OmniAccounts, ParentchainId};
-use alloc::{sync::Arc, vec::Vec};
+use alloc::{collections::btree_map::BTreeMap, sync::Arc, vec::Vec};
 use frame_support::storage::storage_prefix;
 use itp_ocall_api::EnclaveOnChainOCallApi;
 use itp_storage::{decode_storage_key, extract_blake2_128concat_key};
@@ -44,30 +44,50 @@ impl<OCallApi: EnclaveOnChainOCallApi> GetAccountStoresRepository
 {
 	fn get_all(&self) -> Result<OmniAccounts, Error> {
 		let account_store_key_prefix = storage_prefix(b"OmniAccount", b"AccountStore");
-		let account_store_storage_keys_response = self
-			.ocall_api
-			.get_storage_keys(account_store_key_prefix.into(), Some(&self.header))
-			.map_err(|_| Error::OCallApiError("Failed to get storage keys"))?;
-		let account_store_storage_keys = account_store_storage_keys_response
-			.into_iter()
-			.filter_map(decode_storage_key)
-			.collect::<Vec<Vec<u8>>>();
-		let omni_accounts: OmniAccounts = self
-			.ocall_api
-			.get_multiple_storages_verified(
-				account_store_storage_keys,
-				&self.header,
-				&ParentchainId::Litentry,
-			)
-			.map_err(|_| Error::OCallApiError("Failed to get multiple storages"))?
-			.into_iter()
-			.filter_map(|entry| {
-				let storage_key = decode_storage_key(entry.key)?;
-				let account_id: AccountId = extract_blake2_128concat_key(&storage_key)?;
-				let member_accounts: Vec<MemberAccount> = entry.value?;
-				Some((account_id, member_accounts))
-			})
-			.collect();
+		let page_size = 300;
+		let mut start_key: Option<Vec<u8>> = None;
+		let mut omni_accounts: OmniAccounts = BTreeMap::new();
+
+		loop {
+			let storage_keys_paged = self
+				.ocall_api
+				.get_storage_keys_paged(
+					account_store_key_prefix.into(),
+					page_size,
+					start_key.clone(),
+					Some(&self.header),
+				)
+				.map_err(|_| Error::OCallApiError("Failed to get storage keys"))?;
+
+			if storage_keys_paged.is_empty() {
+				break
+			}
+
+			let account_store_storage_keys = storage_keys_paged
+				.into_iter()
+				.filter_map(decode_storage_key)
+				.collect::<Vec<Vec<u8>>>();
+
+			start_key = account_store_storage_keys.last().cloned();
+
+			self.ocall_api
+				.get_multiple_storages_verified(
+					account_store_storage_keys,
+					&self.header,
+					&ParentchainId::Litentry,
+				)
+				.map_err(|_| Error::OCallApiError("Failed to get multiple storages"))?
+				.into_iter()
+				.filter_map(|entry| {
+					let storage_key = decode_storage_key(entry.key)?;
+					let account_id: AccountId = extract_blake2_128concat_key(&storage_key)?;
+					let member_accounts: Vec<MemberAccount> = entry.value?;
+					Some((account_id, member_accounts))
+				})
+				.for_each(|(account_id, member_accounts)| {
+					omni_accounts.insert(account_id, member_accounts);
+				});
+		}
 
 		Ok(omni_accounts)
 	}
