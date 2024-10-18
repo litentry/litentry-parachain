2fd63073f4e56bf6c7546fe51401468eca08869e
Kai <7630809+Kailai-Wang@users.noreply.github.com>
2024-10-18 15:04:00 +0200

    Try to remove duplicated test code (#3135)
    * Try to remove duplicated test code
    
    * remove storage

--- parachain/pallets/omni-account/src/lib.rs
+++ parachain/pallets/omni-account/src/lib.rs
@@ -21,9 +21,7 @@ mod mock;
 #[cfg(test)]
 mod tests;
 
-pub use core_primitives::{
-	GetAccountStoreHash, Identity, Intention, MemberAccount, OmniAccountConverter,
-};
+pub use core_primitives::{Identity, Intention, MemberAccount, OmniAccountConverter};
 pub use frame_system::pallet_prelude::BlockNumberFor;
 pub use pallet::*;
 
@@ -118,17 +116,11 @@ pub mod pallet {
 	pub type MemberAccountHash<T: Config> =
 		StorageMap<Hasher = Blake2_128Concat, Key = H256, Value = T::AccountId>;
 
-	/// A map between OmniAccount and hash of its AccountStore
-	#[pallet::storage]
-	#[pallet::getter(fn account_store_hash)]
-	pub type AccountStoreHash<T: Config> =
-		StorageMap<Hasher = Blake2_128Concat, Key = T::AccountId, Value = H256>;
-
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
 		/// An account store is created
-		AccountStoreCreated { who: T::AccountId, account_store_hash: H256 },
+		AccountStoreCreated { who: T::AccountId },
 		/// Some member account is added
 		AccountAdded { who: T::AccountId, member_account_hash: H256 },
 		/// Some member accounts are removed
@@ -213,13 +205,9 @@ pub mod pallet {
 				.map_err(|_| Error::<T>::AccountStoreLenLimitReached)?;
 
 			MemberAccountHash::<T>::insert(hash, omni_account.clone());
-			AccountStore::<T>::insert(omni_account.clone(), member_accounts.clone());
-			AccountStoreHash::<T>::insert(omni_account.clone(), member_accounts.hash());
+			AccountStore::<T>::insert(omni_account.clone(), member_accounts);
 
-			Self::deposit_event(Event::AccountStoreCreated {
-				who: omni_account,
-				account_store_hash: member_accounts.hash(),
-			});
+			Self::deposit_event(Event::AccountStoreCreated { who: omni_account });
 
 			Ok(())
 		}
@@ -247,7 +235,6 @@ pub mod pallet {
 
 			MemberAccountHash::<T>::insert(hash, who.clone());
 			AccountStore::<T>::insert(who.clone(), member_accounts.clone());
-			AccountStoreHash::<T>::insert(who.clone(), member_accounts.hash());
 
 			Self::deposit_event(Event::AccountAdded { who, member_account_hash: hash });
 
