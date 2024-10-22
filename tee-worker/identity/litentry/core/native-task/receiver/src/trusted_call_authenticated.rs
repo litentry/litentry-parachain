3da4354e6c14d90f9afe4667373885855a26f00a
Francisco Silva <francisco@kawagarbo-tech.io>
2024-10-22 17:07:54 +0000

    refactoring TrustedCallAuthenticated and verification methods

--- tee-worker/identity/litentry/core/native-task/receiver/src/trusted_call_authenticated.rs
+++ tee-worker/identity/litentry/core/native-task/receiver/src/trusted_call_authenticated.rs
@@ -1,11 +1,12 @@
 use alloc::string::String;
 use codec::{Decode, Encode};
 use ita_stf::{LitentryMultiSignature, TrustedCall};
+use itp_stf_primitives::traits::TrustedCallVerification;
 use itp_types::parentchain::Index as ParentchainIndex;
 use lc_identity_verification::VerificationCodeStore;
 use lc_omni_account::InMemoryStore as OmniAccountStore;
 use litentry_hex_utils::hex_encode;
-use litentry_primitives::{MemberAccount, ShardIdentifier};
+use litentry_primitives::{Identity, MemberAccount, ShardIdentifier};
 use sp_core::{
 	blake2_256,
 	crypto::{AccountId32, UncheckedFrom},
@@ -49,49 +50,58 @@ impl Default for TrustedCallAuthenticated {
 	}
 }
 
-pub fn verify_tca_web3_authentication(
-	tca: TrustedCallAuthenticated,
-	mrenclave: &[u8; 32],
-	shard: &ShardIdentifier,
-) -> bool {
-	match tca.authentication {
-		TCAuthentication::Web3(signature) => {
-			let mut payload = tca.call.encode();
-			payload.append(&mut tca.nonce.encode());
-			payload.append(&mut mrenclave.encode());
-			payload.append(&mut shard.encode());
+// TODO: we should refactor this as verify_signature should not be part of TrustedCallAuthenticated
+impl TrustedCallVerification for TrustedCallAuthenticated {
+	fn sender_identity(&self) -> &Identity {
+		self.call.sender_identity()
+	}
 
-			// The signature should be valid in either case:
-			// 1. blake2_256(payload)
-			// 2. Signature Prefix + blake2_256(payload)
+	fn nonce(&self) -> ita_stf::Index {
+		self.nonce
+	}
 
-			let hashed = blake2_256(&payload);
+	fn verify_signature(&self, mrenclave: &[u8; 32], shard: &ShardIdentifier) -> bool {
+		false
+	}
 
-			let prettified_msg_hash = tca.call.signature_message_prefix() + &hex_encode(&hashed);
-			let prettified_msg_hash = prettified_msg_hash.as_bytes();
-
-			// Most common signatures variants by clients are verified first (4 and 2).
-			signature.verify(prettified_msg_hash, tca.call.sender_identity())
-				|| signature.verify(&hashed, tca.call.sender_identity())
-		},
-		TCAuthentication::Email(_) => false,
+	fn metric_name(&self) -> &'static str {
+		self.call.metric_name()
 	}
 }
 
-pub fn verify_tca_email_authentication(tca: TrustedCallAuthenticated) -> bool {
-	match tca.authentication {
-		TCAuthentication::Web3(_) => false,
-		TCAuthentication::Email(verification_code) => {
-			match OmniAccountStore::get_omni_account(tca.call.sender_identity().hash()) {
-				Ok(Some(account_id)) => {
-					let identity_hash = tca.call.sender_identity().hash();
-					match VerificationCodeStore::get(&account_id, identity_hash) {
-						Ok(Some(code)) => code == verification_code,
-						_ => false,
-					}
-				},
-				_ => false,
-			}
+pub fn verify_tca_web3_authentication(
+	signature: &LitentryMultiSignature,
+	call: &TrustedCall,
+	nonce: ParentchainIndex,
+	mrenclave: &[u8; 32],
+	shard: &ShardIdentifier,
+) -> bool {
+	let mut payload = call.encode();
+	payload.append(&mut nonce.encode());
+	payload.append(&mut mrenclave.encode());
+	payload.append(&mut shard.encode());
+
+	// The signature should be valid in either case:
+	// 1. blake2_256(payload)
+	// 2. Signature Prefix + blake2_256(payload)
+
+	let hashed = blake2_256(&payload);
+
+	let prettified_msg_hash = call.signature_message_prefix() + &hex_encode(&hashed);
+	let prettified_msg_hash = prettified_msg_hash.as_bytes();
+
+	// Most common signatures variants by clients are verified first (4 and 2).
+	signature.verify(prettified_msg_hash, call.sender_identity())
+		|| signature.verify(&hashed, call.sender_identity())
+}
+
+pub fn verify_tca_email_authentication(call: &TrustedCall, verification_code: String) -> bool {
+	let identity_hash = call.sender_identity().hash();
+	match OmniAccountStore::get_omni_account(identity_hash) {
+		Ok(Some(account_id)) => match VerificationCodeStore::get(&account_id, identity_hash) {
+			Ok(Some(code)) => code == verification_code,
+			_ => false,
 		},
+		_ => false,
 	}
 }
